1a6f3137eb75c8019049b7124d979bc8ac15c193
Clement Michaud <clement.michaud34@gmail.com>
2017-06-16 22:21:12 +0200

    npm install breaks because it does not find entrypoint file

--- scripts/npm-deployment-test.sh
+++ scripts/npm-deployment-test.sh
@@ -1,33 +1,37 @@
 #!/bin/bash
 
-NPM_UNPACK_DIR=/tmp/npm-unpack/test
+NPM_UNPACK_DIR=/tmp/npm-unpack
 
-echo "Packing npm package into a tarball"
+echo "--- Packing npm package into a tarball"
 npm pack
 
 AUTHELIA_PACKAGE=`ls | grep "authelia-\([0-9]\+.\)\{2\}[0-9]\+.tgz"`
-echo "Authelia package is ${AUTHELIA_PACKAGE}"
+echo "--- Authelia package is ${AUTHELIA_PACKAGE}"
 
-echo "Copy package into "${NPM_UNPACK_DIR}" to test unpacking"
+echo "--- Copy package into "${NPM_UNPACK_DIR}" to test unpacking"
 mkdir -p ${NPM_UNPACK_DIR}
 cp ${AUTHELIA_PACKAGE} ${NPM_UNPACK_DIR}
 
 pushd ${NPM_UNPACK_DIR}
 
-echo "Test unpacking..."
+echo "--- Test unpacking..."
 npm install ${AUTHELIA_PACKAGE}
 
-RET_CODE=$?
+RET_CODE_INSTALL=$?
 # echo ${RET_CODE}
+
+# The binary must start and display the help menu
+./node_modules/.bin/authelia | grep "No config file has been provided."
+RET_CODE_RUN=$?
 
 popd
 
-if [ "$RET_CODE" != "0" ]
+if [ "$RET_CODE_INSTALL" != "0" ] || [ "$RET_CODE_RUN" != "0" ]
 then
-    echo "Unpacking failed..."
+    echo "--- Unpacking failed..."
     exit 1
 else
-    echo "Unpacking succeeded"
+    echo "+++ Unpacking succeeded"
     exit 0
 fi
 
