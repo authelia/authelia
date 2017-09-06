ffd4cfb871632e455a2912499beec1844c3976da
Clement Michaud <clement.michaud34@gmail.com>
2017-09-06 21:45:11 +0200

    Display the content of the tarball generated by npm packing

--- scripts/npm-deployment-test.sh
+++ scripts/npm-deployment-test.sh
@@ -5,9 +5,14 @@ set -e
 NPM_UNPACK_DIR=/tmp/npm-unpack
 
 pwd
+echo "--- Packing npm package into a tarball"
+npm pack
 
 AUTHELIA_PACKAGE=`ls | grep "authelia-\([0-9]\+.\)\{2\}[0-9]\+.tgz"`
 echo "--- Authelia package is ${AUTHELIA_PACKAGE}"
+
+echo "Files in tarball are:"
+tar -ztvf ${AUTHELIA_PACKAGE}
 
 echo "--- Copy package into "${NPM_UNPACK_DIR}" to test unpacking"
 mkdir -p ${NPM_UNPACK_DIR}
