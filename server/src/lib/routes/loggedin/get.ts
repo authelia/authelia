daee0423685ffd7d4c6741f7d5ab812a6a0741b5
Clement Michaud <clement.michaud34@gmail.com>
2017-10-15 22:15:54 +0200

    Add username to the 'already logged in' page

--- server/src/lib/routes/loggedin/get.ts
+++ server/src/lib/routes/loggedin/get.ts
@@ -2,12 +2,16 @@ import Express = require("express");
 import Endpoints = require("../../../../../shared/api");
 import FirstFactorBlocker from "../FirstFactorBlocker";
 import BluebirdPromise = require("bluebird");
+import AuthenticationSession = require("../../AuthenticationSession");
 
 export default FirstFactorBlocker(handler);
 
 function handler(req: Express.Request, res: Express.Response): BluebirdPromise<void> {
-  res.render("already-logged-in", {
-    logout_endpoint: Endpoints.LOGOUT_GET
-  });
-  return BluebirdPromise.resolve();
+  return AuthenticationSession.get(req)
+    .then(function (authSession) {
+      res.render("already-logged-in", {
+        logout_endpoint: Endpoints.LOGOUT_GET,
+        username: authSession.userid
+      });
+    });
 }
