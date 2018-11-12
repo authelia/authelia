f5c653551ac54e51b736caa2a2be77e4eaec87b1
Clement Michaud <c.michaud@criteo.com>
2018-11-11 20:39:20 -0800

    First design of first factor page.

--- client-react/src/first-factor/index.tsx
+++ client-react/src/first-factor/index.tsx
@@ -1,20 +1,90 @@
 import React, { Component } from "react";
 
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
-export class FirstFactor extends Component {
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+
+import logo from '../logo.svg';
+import styles from "./index.module.css"
+
+interface State {
+  rememberMe: boolean;
+}
+
+export class FirstFactor extends Component<any, State> {
+  constructor(props: any) {
+    super(props)
+    this.state = {
+      rememberMe: false
+    }
+  }
+
+  toggleRememberMe = () => {
+    this.setState({
+      rememberMe: !(this.state.rememberMe)
+    })
+  }
+
   render() {
     return (
-      <div className="frame">
-        <TextField
-          id="username"
-          label="Username">
-        </TextField>
-        <TextField
-          id="password"
-          label="Password"
-          type="password">
-        </TextField>
+      <div className={styles.mainContent}>
+        <div className={styles.header}>
+          <h1>Sign in</h1>
+        </div>
+        <div className={styles.frame}>
+          <div className={styles.innerFrame}>
+            <div className={styles.fields}>
+              <div className={styles.field}>
+                <TextField
+                  className={styles.input}
+                  id="username"
+                  label="Username">
+                </TextField>
+              </div>
+              <div className={styles.field}>
+                <TextField
+                  className={styles.input}
+                  id="password"
+                  label="Password"
+                  type="password">
+                </TextField>
+              </div>
+            </div>
+            <div className={styles.controlArea}>
+              <div className={styles.controls}>
+                <div className={styles.rememberMe}>
+                  <FormControlLabel
+                    control={
+                      <Checkbox
+                        checked={this.state.rememberMe}
+                        onChange={this.toggleRememberMe}
+                        color="primary"
+                      />
+                    }
+                    label="Remember me"
+                  />
+                </div>
+                <div className={styles.resetPassword}>
+                  <a href="/">Forgot password?</a>
+                </div>
+              </div>
+              <div className={styles.buttons}>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  className={styles.button}>
+                  Login
+                </Button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.footer}>
+          <img src={logo} alt="logo"></img>
+          <div>Powered by <a href="#">Authelia</a></div>
+        </div>
       </div>
     )
   }
