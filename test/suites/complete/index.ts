e37cca5d454812a517f51303cdb2f816f2b729d0
Clement Michaud <c.michaud@criteo.com>
2019-01-30 23:36:58 +0100

    Add config file in complete suite and remove useless files.

--- test/suites/complete/index.ts
+++ test/suites/complete/index.ts
@@ -2,7 +2,7 @@ import AutheliaSuite from "../../helpers/context/AutheliaSuite";
 import MongoConnectionRecovery from "./scenarii/MongoConnectionRecovery";
 import EnforceInternalRedirectionsOnly from "./scenarii/EnforceInternalRedirectionsOnly";
 
-AutheliaSuite('Complete configuration', 'config.template.yml', function() {
+AutheliaSuite('Complete configuration', __dirname + 'config.yml', function() {
   this.timeout(10000);
 
   describe('Mongo broken connection recovery', MongoConnectionRecovery);
