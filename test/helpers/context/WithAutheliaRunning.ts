4adb0569acf3076dfc985ffac11b131376e85714
Clement Michaud <c.michaud@criteo.com>
2019-02-24 16:42:29 +0100

    Display Authelia server logs when tests fail.

--- test/helpers/context/WithAutheliaRunning.ts
+++ test/helpers/context/WithAutheliaRunning.ts
@@ -1,5 +1,6 @@
 
 import ChildProcess from 'child_process';
+import fs from 'fs';
 
 export default function WithAutheliaRunning(configPath: string, waitTimeout: number = 5000) {
   before(function() {
@@ -11,8 +12,17 @@ export default function WithAutheliaRunning(configPath: string, waitTimeout: num
       ['serve', '--no-watch', '--config', configPath],
       {detached: true});
 
-    authelia.on('exit', function(status) {
-      console.log('Server terminated with status ' + status);
+    authelia.on('exit', function(statusCode) {
+      console.log('Server terminated with status ' + statusCode);
+      if (statusCode != 0) {
+        fs.readFile('/tmp/authelia-server.log', function(err, data) {
+          if (err) {
+            console.error(err);
+            return;
+          }
+          console.log(data.toString('utf-8'));
+        })
+      }
     });
     this.authelia = authelia;
   
