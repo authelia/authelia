21d52df9950e4382773e0b1f30b2386888d745eb
Clement Michaud <clement.michaud34@gmail.com>
2019-11-06 23:41:21 +0100

    Make docker build and push commands parameterized by an architecture flag.

--- cmd/authelia-scripts/cmd_docker.go
+++ cmd/authelia-scripts/cmd_docker.go
@@ -5,18 +5,67 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
 
-func DockerBuildOfficialImage() error {
+var arch string
+var supportedArch = []string{"amd64", "arm32v7", "arm64v8"}
+var defaultArch = "amd64"
+
+func init() {
+	DockerBuildCmd.PersistentFlags().StringVar(&arch, "arch", defaultArch, "target architecture among: "+strings.Join(supportedArch, ", "))
+	DockerPushCmd.PersistentFlags().StringVar(&arch, "arch", defaultArch, "target architecture among: "+strings.Join(supportedArch, ", "))
+
+}
+
+func checkArchIsSupported(arch string) {
+	for _, a := range supportedArch {
+		if arch == a {
+			return
+		}
+	}
+	log.Fatal("Architecture is not supported. Please select one of " + strings.Join(supportedArch, ", ") + ".")
+}
+
+func dockerBuildOfficialImage(arch string) error {
 	docker := &Docker{}
 	// Set default Architecture Dockerfile to amd64
-	Dockerfile := "Dockerfile"
-	if dockerfile := os.Getenv("DOCKERFILE"); dockerfile != "" {
-		Dockerfile = dockerfile
+	dockerfile := "Dockerfile"
+
+	// If not the default value
+	if arch != defaultArch {
+		dockerfile = fmt.Sprintf("%s.%s", dockerfile, arch)
 	}
-	return docker.Build(IntermediateDockerImageName, Dockerfile, ".")
+
+	if arch == "arm32v7" {
+		err := CommandWithStdout("docker", "run", "--rm", "--privileged", "multiarch/qemu-user-static", "--reset", "-p", "yes").Run()
+
+		if err != nil {
+			panic(err)
+		}
+
+		err = CommandWithStdout("bash", "-c", "wget https://github.com/multiarch/qemu-user-static/releases/download/v4.1.0-1/qemu-arm-static -O ./qemu-arm-static && chmod +x ./qemu-arm-static").Run()
+
+		if err != nil {
+			panic(err)
+		}
+	} else if arch == "arm64v8" {
+		err := CommandWithStdout("docker", "run", "--rm", "--privileged", "multiarch/qemu-user-static", "--reset", "-p", "yes").Run()
+
+		if err != nil {
+			panic(err)
+		}
+
+		err = CommandWithStdout("bash", "-c", "wget https://github.com/multiarch/qemu-user-static/releases/download/v4.1.0-1/qemu-aarch64-static -O ./qemu-aarch64-static && chmod +x ./qemu-aarch64-static").Run()
+
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	return docker.Build(IntermediateDockerImageName, dockerfile, ".")
 }
 
 // DockerBuildCmd Command for building docker image of Authelia.
@@ -24,7 +73,8 @@ var DockerBuildCmd = &cobra.Command{
 	Use:   "build",
 	Short: "Build the docker image of Authelia",
 	Run: func(cmd *cobra.Command, args []string) {
-		err := DockerBuildOfficialImage()
+		checkArchIsSupported(arch)
+		err := dockerBuildOfficialImage(arch)
 
 		if err != nil {
 			log.Fatal(err)
@@ -41,16 +91,17 @@ var DockerBuildCmd = &cobra.Command{
 
 // DockerPushCmd Command for pushing Authelia docker image to Dockerhub
 var DockerPushCmd = &cobra.Command{
-	Use:   "publish",
+	Use:   "push-image",
 	Short: "Publish Authelia docker image to Dockerhub",
 	Run: func(cmd *cobra.Command, args []string) {
-		publishDockerImage()
+		checkArchIsSupported(arch)
+		publishDockerImage(arch)
 	},
 }
 
 // DockerManifestCmd Command for pushing Authelia docker manifest to Dockerhub
 var DockerManifestCmd = &cobra.Command{
-	Use:   "manifest",
+	Use:   "push-manifest",
 	Short: "Publish Authelia docker manifest to Dockerhub",
 	Run: func(cmd *cobra.Command, args []string) {
 		publishDockerManifest()
@@ -116,21 +167,20 @@ func deployManifest(docker *Docker, tag string, amd64tag string, arm32v7tag stri
 	}
 }
 
-func publishDockerImage() {
+func publishDockerImage(arch string) {
 	docker := &Docker{}
 
-	ARCH := os.Getenv("ARCH")
 	travisBranch := os.Getenv("TRAVIS_BRANCH")
 	travisPullRequest := os.Getenv("TRAVIS_PULL_REQUEST")
 	travisTag := os.Getenv("TRAVIS_TAG")
 
 	if travisBranch == "master" && travisPullRequest == "false" {
 		login(docker)
-		deploy(docker, "master-" + ARCH)
+		deploy(docker, "master-"+arch)
 	} else if travisTag != "" {
 		login(docker)
-		deploy(docker, travisTag + "-" + ARCH)
-		deploy(docker, "latest-" + ARCH)
+		deploy(docker, travisTag+"-"+arch)
+		deploy(docker, "latest-"+arch)
 	} else {
 		fmt.Println("Docker image will not be published")
 	}
@@ -148,7 +198,7 @@ func publishDockerManifest() {
 		deployManifest(docker, "master", "master-amd64", "master-arm32v7", "master-arm64v8")
 	} else if travisTag != "" {
 		login(docker)
-		deployManifest(docker, travisTag, travisTag + "-amd64", travisTag + "-arm32v7", travisTag + "-arm64v8")
+		deployManifest(docker, travisTag, travisTag+"-amd64", travisTag+"-arm32v7", travisTag+"-arm64v8")
 		deployManifest(docker, "latest", "latest-amd64", "latest-arm32v7", "latest-arm64v8")
 	} else {
 		fmt.Println("Docker manifest will not be published")
