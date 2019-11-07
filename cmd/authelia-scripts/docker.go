c5c79f54dbfbd3af0e473284dbe973f5d724a90e
Amir Zarrinkafsh <nightah@me.com>
2019-11-07 11:59:24 +1100

    Enable Multiarch docker builds

--- cmd/authelia-scripts/docker.go
+++ cmd/authelia-scripts/docker.go
@@ -4,8 +4,8 @@ package main
 type Docker struct{}
 
 // Build build a docker image
-func (d *Docker) Build(tag string, target string) error {
-	return CommandWithStdout("docker", "build", "-t", tag, target).Run()
+func (d *Docker) Build(tag string, dockerfile string, target string) error {
+	return CommandWithStdout("docker", "build", "-t", tag, "-f", dockerfile, target).Run()
 }
 
 // Tag tag a docker image.
@@ -21,4 +21,27 @@ func (d *Docker) Login(username, password string) error {
 // Push push a docker image to dockerhub.
 func (d *Docker) Push(tag string) error {
 	return CommandWithStdout("docker", "push", tag).Run()
+}
+
+// Manifest push a docker manifest to dockerhub.
+func (d *Docker) Manifest(tag string, amd64tag string, arm32v7tag string, arm64v8tag string) error {
+	err := CommandWithStdout("docker", "manifest", "create", tag, amd64tag, arm32v7tag, arm64v8tag).Run()
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = CommandWithStdout("docker", "manifest", "annotate", tag, arm32v7tag, "--os", "linux", "--arch", "arm").Run()
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = CommandWithStdout("docker", "manifest", "annotate", tag, arm64v8tag, "--os", "linux", "--arch", "arm64", "--variant", "v8").Run()
+
+	if err != nil {
+		panic(err)
+	}
+
+	return CommandWithStdout("docker", "manifest", "push", "--purge", tag).Run()
 }
