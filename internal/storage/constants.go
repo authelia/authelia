b5e9cba98cefefb79b1d3dacb06c5d6877118dd0
Clement Michaud <clement.michaud34@gmail.com>
2019-12-08 00:22:02 +0100

    Use lower case database table names.

--- internal/storage/constants.go
+++ internal/storage/constants.go
@@ -1,7 +1,8 @@
 package storage
 
-var preferencesTableName = "Preferences"
-var identityVerificationTokensTableName = "IdentityVerificationTokens"
-var totpSecretsTableName = "TOTPSecrets"
-var u2fDeviceHandlesTableName = "U2FDeviceHandles"
-var authenticationLogsTableName = "AuthenticationLogs"
+// Keep table names in lower case because some DB does not support upper case.
+var preferencesTableName = "user_preferences"
+var identityVerificationTokensTableName = "identity_verification_tokens"
+var totpSecretsTableName = "totp_secrets"
+var u2fDeviceHandlesTableName = "u2f_devices"
+var authenticationLogsTableName = "authentication_logs"
