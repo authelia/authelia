b4a8c4f0ec69773e4667dbdabecb07d71f6dc6fe
Clement Michaud <clement.michaud34@gmail.com>
2019-12-08 16:51:12 +0100

    Introduce version command to Authelia to check the version
    The version command displays the tag and the commit hash of the
    built commit along with the time when the build was done.

--- example/compose/authelia/resources/run.sh
+++ example/compose/authelia/resources/run.sh
@@ -3,10 +3,10 @@
 set -e
 
 # Build the binary
-go build -o /tmp/authelia/authelia-tmp cmd/authelia/main.go
+go build -o /tmp/authelia/authelia-tmp cmd/authelia/*.go
 
 while true;
 do
-    /tmp/authelia/authelia-tmp -config /etc/authelia/configuration.yml
+    /tmp/authelia/authelia-tmp --config /etc/authelia/configuration.yml
     sleep 10
 done
