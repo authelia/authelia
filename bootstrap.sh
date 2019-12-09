a5d706e29dd130ab6475be8c118232cea2d057e6
Clement Michaud <clement.michaud34@gmail.com>
2019-12-09 07:38:20 +0100

    Use static version of Authelia in suites when in CI.
    This is to avoid the multiple reloads due to the frontend modules
    being installed at the startup of the suite and randomly preventing
    Authelia from starting.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
 
-export PATH=$PATH:./cmd/authelia-scripts/:./node_modules/.bin:/tmp
+export PATH=./cmd/authelia-scripts/:/tmp:$PATH:./node_modules/.bin
 
 if [ -z "$OLD_PS1" ]; then
   OLD_PS1="$PS1"
   export PS1="(authelia) $PS1"
 fi
 
-if [ $(id -u) = 0 ]; then
-  export USER_ID=1000
-else
-  export USER_ID=$(id -u)
-fi
-if [ $(id -g) = 0 ]; then
-  export GROUP_ID=1000
-else
-  export GROUP_ID=$(id -g)
-fi
+export USER_ID=$(id -u)
+export GROUP_ID=$(id -g)
+
 
 echo "[BOOTSTRAP] Checking if Go is installed..."
 if [ ! -x "$(command -v go)" ];
