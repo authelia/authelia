1582710f73a39c873733e98b3fa9cee88cd08368
Amir Zarrinkafsh <nightah@me.com>
2019-12-12 21:58:50 +1100

    Break out Kubernetes e2e suite

--- .buildkite/steps/e2etests.sh
+++ .buildkite/steps/e2etests.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 set -eu
 
-for SUITE_NAME in BypassAll Docker DuoPush HighAvailability Kubernetes LDAP Mariadb NetworkACL Postgres ShortTimeouts Standalone Traefik; do
-  echo "  - commands:"
-  echo "    - \"authelia-scripts --log-level debug suites test ${SUITE_NAME} --headless\""
+echo "  - command: \"authelia-scripts --log-level debug suites test Kubernetes --headless\""
+echo "    label: \":selenium: Kubernetes Suite\""
+echo "    agents:"
+echo "      "kubernetes: true""
+echo "    env:"
+echo "      "CI: true""
+
+for SUITE_NAME in BypassAll Docker DuoPush HighAvailability  LDAP Mariadb NetworkACL Postgres ShortTimeouts Standalone Traefik; do
+  echo "  - command: \"authelia-scripts --log-level debug suites test ${SUITE_NAME} --headless\""
   echo "    label: \":selenium: ${SUITE_NAME} Suite\""
   echo "    env:"
   echo "      "CI: true""
 done
-
-
