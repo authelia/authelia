3f7d6f25fc252cc49fa989b35248fe973159c1ef
Amir Zarrinkafsh <nightah@me.com>
2019-12-12 23:15:30 +1100

    Fix executable bit for build artifact

--- .buildkite/steps/e2etests.sh
+++ .buildkite/steps/e2etests.sh
@@ -3,6 +3,7 @@ set -eu
 
 echo "  - commands:"
 echo "    - \"buildkite-agent artifact download "dist/*" .\""
+echo "    - \"chmod +x dist/authelia\""
 echo "    - \"authelia-scripts --log-level debug suites test Kubernetes --headless\""
 echo "    label: \":selenium: Kubernetes Suite\""
 echo "    agents:"
@@ -13,6 +14,7 @@ echo "      "CI: true""
 for SUITE_NAME in BypassAll Docker DuoPush HighAvailability  LDAP Mariadb NetworkACL Postgres ShortTimeouts Standalone Traefik; do
   echo "  - commands:"
   echo "    - \"buildkite-agent artifact download "dist/*" .\""
+  echo "    - \"chmod +x dist/authelia\""
   echo "    - \"authelia-scripts --log-level debug suites test ${SUITE_NAME} --headless\""
   echo "    label: \":selenium: ${SUITE_NAME} Suite\""
   echo "    env:"
