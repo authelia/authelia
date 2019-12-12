49afc82db744c44695e4e8534e61eac7eadfaa34
Amir Zarrinkafsh <nightah@me.com>
2019-12-12 23:22:41 +1100

    Optimise e2etests.sh

--- .buildkite/steps/e2etests.sh
+++ .buildkite/steps/e2etests.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
 set -eu
 
-echo "  - commands:"
-echo "    - \"buildkite-agent artifact download "dist/*" .\""
-echo "    - \"chmod +x dist/authelia\""
-echo "    - \"authelia-scripts --log-level debug suites test Kubernetes --headless\""
-echo "    label: \":selenium: Kubernetes Suite\""
-echo "    agents:"
-echo "      "kubernetes: true""
-echo "    env:"
-echo "      "CI: true""
-
-for SUITE_NAME in BypassAll Docker DuoPush HighAvailability  LDAP Mariadb NetworkACL Postgres ShortTimeouts Standalone Traefik; do
+for SUITE_NAME in BypassAll Docker DuoPush HighAvailability Kubernetes LDAP Mariadb NetworkACL Postgres ShortTimeouts Standalone Traefik; do
   echo "  - commands:"
   echo "    - \"buildkite-agent artifact download "dist/*" .\""
   echo "    - \"chmod +x dist/authelia\""
   echo "    - \"authelia-scripts --log-level debug suites test ${SUITE_NAME} --headless\""
   echo "    label: \":selenium: ${SUITE_NAME} Suite\""
+  if "${SUITE_NAME}" == "Kubernetes"; then
+    echo "    agents:"
+    echo "      "kubernetes: true""
+  fi
   echo "    env:"
   echo "      "CI: true""
 done
