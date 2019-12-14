b7ddeb5238db5237de3259391073442adb18cc18
Amir Zarrinkafsh <nightah@me.com>
2019-12-15 00:37:31 +1100

    Build docker images and prep artifacts for GH releases

---
+++ .buildkite/steps/buildimages.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -eu
+
+for BUILD_ARCH in amd64 arm32v7 arm64v8;
+do
+  echo "  - commands:"
+  echo "    - \"authelia-scripts docker build --arch=${BUILD_ARCH}\""
+  echo "    label: \":docker: Build ${BUILD_ARCH} image\""
+  echo "    artifact_paths:"
+  echo "      - \"authelia-linux-${BUILD_ARCH}.tar.gz\""
+  echo "      - \"authelia-linux-${BUILD_ARCH}.tar.gz.sha256\""
+  echo "    env:"
+  echo "      "ARCH: ${BUILD_ARCH}""
+done
--- .buildkite/steps/e2etests.sh
+++ .buildkite/steps/e2etests.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -eu
 
-for SUITE_NAME in BypassAll Docker DuoPush HighAvailability Kubernetes LDAP Mariadb NetworkACL Postgres ShortTimeouts Standalone Traefik; do
+for SUITE_NAME in BypassAll Docker DuoPush HighAvailability Kubernetes LDAP Mariadb NetworkACL Postgres ShortTimeouts Standalone Traefik;
+do
   echo "  - commands:"
   echo "    - \"authelia-scripts --log-level debug suites test ${SUITE_NAME} --headless\""
-  echo "    label: \":selenium: ${SUITE_NAME} Suite\""
+  echo "    label: \":selenium: ${SUITE_NAME} suite\""
   echo "    env:"
   echo "      "CI: true""
   if [[ "${SUITE_NAME}" == "Kubernetes" ]]; then
