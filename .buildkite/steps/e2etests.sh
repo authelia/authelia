3e618ef1fdeb94de917c645b0ff2af43eda0cdfe
Amir Zarrinkafsh <nightah@me.com>
2019-12-14 15:27:31 +1100

    Prepare artifacts in post-checkout step

--- .buildkite/steps/e2etests.sh
+++ .buildkite/steps/e2etests.sh
@@ -3,8 +3,6 @@ set -eu
 
 for SUITE_NAME in BypassAll Docker DuoPush HighAvailability Kubernetes LDAP Mariadb NetworkACL Postgres ShortTimeouts Standalone Traefik; do
   echo "  - commands:"
-  echo "    - \"buildkite-agent artifact download "dist/*" .\""
-  echo "    - \"chmod +x dist/authelia\""
   echo "    - \"authelia-scripts --log-level debug suites test ${SUITE_NAME} --headless\""
   echo "    label: \":selenium: ${SUITE_NAME} Suite\""
   if "${SUITE_NAME}" == "Kubernetes"; then
