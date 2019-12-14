36f9e5ffe751715444db77638e98a67e0024e7da
Amir Zarrinkafsh <nightah@me.com>
2019-12-15 01:44:58 +1100

    Specify builder nodes

--- .buildkite/steps/buildimages.sh
+++ .buildkite/steps/buildimages.sh
@@ -11,4 +11,6 @@ do
   echo "      - \"authelia-linux-${BUILD_ARCH}.tar.gz.sha256\""
   echo "    env:"
   echo "      "ARCH: ${BUILD_ARCH}""
+  echo "    agents:"
+  echo "      "docker: true""
 done
