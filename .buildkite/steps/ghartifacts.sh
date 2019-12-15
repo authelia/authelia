cbecda557286973a4aa447c9b3964133f69037c9
Amir Zarrinkafsh <nightah@me.com>
2019-12-16 09:35:41 +1100

    Attach all assets to GH release

--- .buildkite/steps/ghartifacts.sh
+++ .buildkite/steps/ghartifacts.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -eu
 
+artifacts=()
 for FILES in authelia-linux-amd64.tar.gz authelia-linux-arm32v7.tar.gz authelia-linux-arm64v8.tar.gz authelia-linux-amd64.tar.gz.sha256 authelia-linux-arm32v7.tar.gz.sha256 authelia-linux-arm64v8.tar.gz.sha256;
 do
-  hub release create -a ${FILES} $BUILDKITE_TAG
+  artifacts+=(-a ${FILES})
 done
+
+hub release create ${artifacts[@]} -m $BUILDKITE_TAG $BUILDKITE_TAG
