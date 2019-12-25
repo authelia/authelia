7f0bca46dfb8ecabfe1b31b4976dd3a2e24beff2
Amir Zarrinkafsh <nightah@me.com>
2019-12-26 01:00:23 +1100

    Warn user when running bootstrap as root

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,12 +8,14 @@ if [ -z "$OLD_PS1" ]; then
 fi
 
 if [ $(id -u) = 0 ]; then
+  echo "Cannot run as root, defaulting to UID 1000"
   export USER_ID=1000
 else
   export USER_ID=$(id -u)
 fi
 
 if [ $(id -g) = 0 ]; then
+  echo "Cannot run as root, defaulting to GID 1000"
   export GROUP_ID=1000
 else
   export GROUP_ID=$(id -g)
