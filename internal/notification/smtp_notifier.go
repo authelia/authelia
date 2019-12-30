329dfb9ab16213f18ab02166b9e225441870b81a
James Elliott <james-d-elliott@users.noreply.github.com>
2019-12-30 13:03:51 +1100

    Force TLS and valid x509 certs in SMTP Notifier by default
    - Adjust AUTH LOGIN functionality to be closer to AUTH PLAIN
    - Removed: secure (notifier smtp conf) boolean string
    - Added: disable_verify_cert (notifier smtp conf) boolean
        - disables X509 validation of certificates
    - Added: disable_require_tls (notifier smtp conf) boolean
        - allows emails to be sent over plain text (for non-authenticated only)
    - Added: trusted_cert (notifier smtp conf) string (path)
        - allows specifying the path of a PEM format cert to add to trusted cert pool
    - Make SMTP notifier return errors on connection over plain text
    - Make SMTP notifier return errors on TLS connection with invalid certs
    - Implemented various debug logging for the SMTP notifier
    - Implemented explicit SMTP closes on errors (previously left con open)
    - Split SMTPNotifier Send func to seperate funcs for:
        - writing future test suites and startup checks more easily
        - organization and readability
    - Add details of changes to docs/security.yml
    - Adjust config.yml's (template and test) for the changes

--- internal/notification/smtp_notifier.go
+++ internal/notification/smtp_notifier.go
@@ -2,8 +2,10 @@ package notification
 
 import (
 	"crypto/tls"
+	"crypto/x509"
 	"errors"
 	"fmt"
+	"io/ioutil"
 	"net/smtp"
 	"strings"
 
@@ -12,130 +14,244 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-// SMTPNotifier a notifier to send emails to SMTP servers.
+// SMTPNotifier a notifier to send emails to SMTP servers
 type SMTPNotifier struct {
-	username string
-	password string
-	sender   string
-	host     string
-	port     int
-	secure   bool
-	address  string
+	username          string
+	password          string
+	sender            string
+	host              string
+	port              int
+	trustedCert       string
+	disableVerifyCert bool
+	disableRequireTLS bool
+	address           string
+	client            *smtp.Client
+	tlsConfig         *tls.Config
 }
 
-// NewSMTPNotifier create an SMTPNotifier targeting a given address.
+// NewSMTPNotifier create an SMTPNotifier targeting a given address
 func NewSMTPNotifier(configuration schema.SMTPNotifierConfiguration) *SMTPNotifier {
-	return &SMTPNotifier{
-		username: configuration.Username,
-		password: configuration.Password,
-		sender:   configuration.Sender,
-		host:     configuration.Host,
-		port:     configuration.Port,
-		secure:   configuration.Secure,
-		address:  fmt.Sprintf("%s:%d", configuration.Host, configuration.Port),
+	notifier := &SMTPNotifier{
+		username:          configuration.Username,
+		password:          configuration.Password,
+		sender:            configuration.Sender,
+		host:              configuration.Host,
+		port:              configuration.Port,
+		trustedCert:       configuration.TrustedCert,
+		disableVerifyCert: configuration.DisableVerifyCert,
+		disableRequireTLS: configuration.DisableRequireTLS,
+		address:           fmt.Sprintf("%s:%d", configuration.Host, configuration.Port),
+	}
+	notifier.initializeTLSConfig()
+	return notifier
+}
+
+func (n *SMTPNotifier) initializeTLSConfig() {
+	// Do not allow users to disable verification of certs if they have also set a trusted cert that was loaded
+	// The second part of this check happens in the Configure Cert Pool code block
+	log.Debug("Notifier SMTP client initializing TLS configuration")
+	insecureSkipVerify := false
+	if n.disableVerifyCert {
+		insecureSkipVerify = true
+	}
+
+	//Configure Cert Pool
+	certPool, err := x509.SystemCertPool()
+	if err != nil || certPool == nil {
+		certPool = x509.NewCertPool()
+	}
+
+	if n.trustedCert != "" {
+		log.Debugf("Notifier SMTP client attempting to load certificate from %s", n.trustedCert)
+		if exists, err := utils.FileExists(n.trustedCert); exists {
+			pem, err := ioutil.ReadFile(n.trustedCert)
+			if err != nil {
+				log.Warnf("Notifier SMTP failed to load cert from file with error: %s", err)
+			} else {
+				if ok := certPool.AppendCertsFromPEM(pem); !ok {
+					log.Warn("Notifier SMTP failed to import cert loaded from file")
+				} else {
+					log.Debug("Notifier SMTP successfully loaded certificate")
+					if n.disableVerifyCert {
+						log.Warn("Notifier SMTP when trusted_cert is specified we force disable_verify_cert to false, if you want to disable certificate validation please comment/delete trusted_cert from your config")
+						insecureSkipVerify = false
+					}
+				}
+			}
+		} else {
+			log.Warnf("Notifier SMTP failed to load cert from file (file does not exist) with error: %s", err)
+		}
+	}
+	n.tlsConfig = &tls.Config{
+		InsecureSkipVerify: insecureSkipVerify,
+		ServerName:         n.host,
+		RootCAs:            certPool,
 	}
 }
 
-// Send send a identity verification link to a user.
-func (n *SMTPNotifier) Send(recipient string, subject string, body string) error {
-	msg := "From: " + n.sender + "\n" +
-		"To: " + recipient + "\n" +
-		"Subject: " + subject + "\n" +
-		"MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n" +
-		body
-
-	c, err := smtp.Dial(n.address)
-
-	if err != nil {
-		return err
+// Do startTLS if available (some servers only provide the auth extension after, and encryption is preferred)
+func (n *SMTPNotifier) startTLS() (bool, error) {
+	// Only start if not already encrypted
+	if _, ok := n.client.TLSConnectionState(); ok {
+		log.Debugf("Notifier SMTP connection is already encrypted, skipping STARTTLS")
+		return ok, nil
 	}
 
-	// Do StartTLS if available (some servers only provide the auth extnesion after, and encryption is preferred)
-	starttls, _ := c.Extension("STARTTLS")
-	if starttls {
-		tlsconfig := &tls.Config{
-			InsecureSkipVerify: !n.secure,
-			ServerName:         n.host,
-		}
-		log.Debugf("SMTP server supports STARTTLS (InsecureSkipVerify: %t, ServerName: %s), attempting", tlsconfig.InsecureSkipVerify, tlsconfig.ServerName)
-		err := c.StartTLS(tlsconfig)
+	ok, _ := n.client.Extension("STARTTLS")
+	if ok {
+		log.Debugf("Notifier SMTP server supports STARTTLS (disableVerifyCert: %t, ServerName: %s), attempting", n.tlsConfig.InsecureSkipVerify, n.tlsConfig.ServerName)
+
+		err := n.client.StartTLS(n.tlsConfig)
 		if err != nil {
-			return err
+			return ok, err
 		} else {
-			log.Debug("SMTP STARTTLS completed without error")
+			log.Debug("Notifier SMTP STARTTLS completed without error")
 		}
+	} else if n.disableRequireTLS {
+		log.Warn("Notifier SMTP server does not support STARTTLS and SMTP configuration is set to disable the TLS requirement (only useful for unauthenticated emails over plain text)")
 	} else {
-		log.Debug("SMTP server does not support STARTTLS, skipping")
+		return ok, errors.New("Notifier SMTP server does not support TLS and it is required by default (see documentation if you want to disable this highly recommended requirement)")
 	}
+	return ok, nil
+}
 
+// Attempt Authentication
+func (n *SMTPNotifier) auth() (bool, error) {
 	// Attempt AUTH if password is specified only
 	if n.password != "" {
-		if !starttls {
-			log.Warn("Authentication is being attempted over an insecure connection. Using a SMTP server that supports STARTTLS is recommended, especially if the server is not on your local network (username and pasword are being transmitted in plain-text).")
+		_, ok := n.client.TLSConnectionState()
+		if !ok {
+			return false, errors.New("Notifier SMTP client does not support authentication over plain text and the connection is currently plain text")
 		}
 
 		// Check the server supports AUTH, and get the mechanisms
-		authExtension, m := c.Extension("AUTH")
-		if authExtension {
-			log.Debugf("Config has SMTP password and server supports AUTH with the following mechanisms: %s.", m)
+		ok, m := n.client.Extension("AUTH")
+		if ok {
+			log.Debugf("Notifier SMTP server supports authentication with the following mechanisms: %s", m)
 			mechanisms := strings.Split(m, " ")
 			var auth smtp.Auth
 
 			// Adaptively select the AUTH mechanism to use based on what the server advertised
 			if utils.IsStringInSlice("PLAIN", mechanisms) {
 				auth = smtp.PlainAuth("", n.username, n.password, n.host)
-				log.Debug("SMTP server supports AUTH PLAIN, attempting...")
+				log.Debug("Notifier SMTP client attempting AUTH PLAIN with server")
 			} else if utils.IsStringInSlice("LOGIN", mechanisms) {
-				auth = LoginAuth(n.username, n.password)
-				log.Debug("SMTP server supports AUTH LOGIN, attempting...")
+				auth = newLoginAuth(n.username, n.password, n.host)
+				log.Debug("Notifier SMTP client attempting AUTH LOGIN with server")
 			}
 
 			// Throw error since AUTH extension is not supported
 			if auth == nil {
-				return fmt.Errorf("SMTP server does not advertise a AUTH mechanism that Authelia supports (PLAIN or LOGIN). Advertised mechanisms: %s.", m)
+				return false, fmt.Errorf("notifier SMTP server does not advertise a AUTH mechanism that are supported by Authelia (PLAIN or LOGIN are supported, but server advertised %s mechanisms)", m)
 			}
 
 			// Authenticate
-			err := c.Auth(auth)
+			err := n.client.Auth(auth)
 			if err != nil {
-				return err
+				return false, err
 			} else {
-				log.Debug("SMTP AUTH completed successfully.")
+				log.Debug("Notifier SMTP client authenticated successfully with the server")
+				return true, nil
 			}
 		} else {
-			return errors.New("SMTP server does not advertise the AUTH extension but a password was specified. Either disable auth (don't specify a password/comment the password), or specify an SMTP host and port that supports AUTH PLAIN or AUTH LOGIN.")
+			return false, errors.New("Notifier SMTP server does not advertise the AUTH extension but config requires AUTH (password specified), either disable AUTH, or use an SMTP host that supports AUTH PLAIN or AUTH LOGIN")
 		}
 	} else {
-		log.Debug("SMTP config has no password specified for use with AUTH, skipping.")
+		log.Debug("Notifier SMTP config has no password specified so authentication is being skipped")
+		return false, nil
+	}
+}
+
+func (n *SMTPNotifier) compose(recipient, subject, body string) error {
+	log.Debugf("Notifier SMTP client attempting to send email body to %s", recipient)
+	if !n.disableRequireTLS {
+		_, ok := n.client.TLSConnectionState()
+		if !ok {
+			return errors.New("Notifier SMTP client can't send an email over plain text connection")
+		}
+	}
+	wc, err := n.client.Data()
+	if err != nil {
+		log.Debugf("Notifier SMTP client error while obtaining WriteCloser: %s", err)
+		return err
+	}
+
+	msg := "From: " + n.sender + "\n" +
+		"To: " + recipient + "\n" +
+		"Subject: " + subject + "\n" +
+		"MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n" +
+		body
+
+	_, err = fmt.Fprintf(wc, msg)
+	if err != nil {
+		log.Debugf("Notifier SMTP client error while sending email body over WriteCloser: %s", err)
+		return err
+	}
+
+	err = wc.Close()
+	if err != nil {
+		log.Debugf("Notifier SMTP client error while closing the WriteCloser: %s", err)
+		return err
+	}
+	return nil
+}
+
+// Dial the SMTP server with the SMTPNotifier config
+func (n *SMTPNotifier) dial() error {
+	log.Debugf("Notifier SMTP client attempting connection to %s", n.address)
+	client, err := smtp.Dial(n.address)
+	if err != nil {
+		return err
+	}
+	log.Debug("Notifier SMTP client connected successfully")
+	n.client = client
+	return nil
+}
+
+// Send an email
+func (n *SMTPNotifier) Send(recipient, subject, body string) error {
+	err := n.dial()
+	if err != nil {
+		_ = n.client.Close()
+		return err
+	}
+
+	_, err = n.startTLS()
+	if err != nil {
+		_ = n.client.Close()
+		return err
+	}
+
+	_, err = n.auth()
+	if err != nil {
+		_ = n.client.Close()
+		return err
 	}
 
 	// Set the sender and recipient first
-	if err := c.Mail(n.sender); err != nil {
+	if err := n.client.Mail(n.sender); err != nil {
+		log.Debugf("Notifier SMTP failed while sending MAIL FROM (using sender) with error: %s", err)
+		_ = n.client.Close()
+		return err
+	}
+	if err := n.client.Rcpt(recipient); err != nil {
+		log.Debugf("Notifier SMTP failed while sending RCPT TO (using recipient) with error: %s", err)
+		_ = n.client.Close()
 		return err
 	}
 
-	if err := c.Rcpt(recipient); err != nil {
+	// compose and send the email body
+	if err := n.compose(recipient, subject, body); err != nil {
+		_ = n.client.Close()
 		return err
 	}
 
-	// Send the email body.
-	wc, err := c.Data()
+	// Send the QUIT command and close the connection
+	err = n.client.Quit()
 	if err != nil {
+		_ = n.client.Close()
 		return err
 	}
-	_, err = fmt.Fprintf(wc, msg)
-	if err != nil {
-		return err
-	}
-	err = wc.Close()
-	if err != nil {
-		return err
-	}
-
-	// Send the QUIT command and close the connection.
-	err = c.Quit()
-	if err != nil {
-		return err
-	}
+	log.Debug("Notifier SMTP client successfully sent email")
 	return nil
 }
