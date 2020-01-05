4559b954e632dc046f7c9f83ece58dfbb1ad6ee7
Amir Zarrinkafsh <nightah@me.com>
2020-01-06 01:31:33 +1100

    Shellcheck Buildkite pipeline

--- .buildkite/steps/ghartifacts.sh
+++ .buildkite/steps/ghartifacts.sh
@@ -3,9 +3,12 @@ set -eu
 
 artifacts=()
 
-for FILES in authelia-linux-amd64.tar.gz authelia-linux-arm32v7.tar.gz authelia-linux-arm64v8.tar.gz authelia-linux-amd64.tar.gz.sha256 authelia-linux-arm32v7.tar.gz.sha256 authelia-linux-arm64v8.tar.gz.sha256;
+for FILES in \
+  authelia-linux-amd64.tar.gz authelia-linux-amd64.tar.gz.sha256 \
+  authelia-linux-arm32v7.tar.gz authelia-linux-arm32v7.tar.gz.sha256 \
+  authelia-linux-arm64v8.tar.gz authelia-linux-arm64v8.tar.gz.sha256;
 do
-  artifacts+=(-a ${FILES})
+  artifacts+=(-a "${FILES}")
 done
 
-hub release create ${artifacts[@]} -m $BUILDKITE_TAG $BUILDKITE_TAG
+hub release create "${artifacts[@]}" -m "${BUILDKITE_TAG}" "${BUILDKITE_TAG}"
