1059551133ca66cce185d72aabc569111f9500e1
Amir Zarrinkafsh <nightah@me.com>
2020-01-20 10:53:55 +1100

    Optimise deploy artifacts step (#564)
    * Optimise deploy artifacts step
    authelia-scripts is not required to publish GitHub artifacts as we utilise [Hub](https://hub.github.com/), this should save ~10 seconds in this step.
    
    * Specify release number in pipeline
    
    * Change buildkite and github published artifacts back to gzip
    
    * Update README.md

--- .buildkite/steps/ghartifacts.sh
+++ .buildkite/steps/ghartifacts.sh
@@ -4,11 +4,12 @@ set -eu
 artifacts=()
 
 for FILES in \
-  authelia-linux-amd64.tar.zst authelia-linux-amd64.tar.zst.sha256 \
-  authelia-linux-arm32v7.tar.zst authelia-linux-arm32v7.tar.zst.sha256 \
-  authelia-linux-arm64v8.tar.zst authelia-linux-arm64v8.tar.zst.sha256;
+  authelia-linux-amd64.tar.gz authelia-linux-amd64.tar.gz.sha256 \
+  authelia-linux-arm32v7.tar.gz authelia-linux-arm32v7.tar.gz.sha256 \
+  authelia-linux-arm64v8.tar.gz authelia-linux-arm64v8.tar.gz.sha256;
 do
   artifacts+=(-a "${FILES}")
 done
 
+echo "--- :github: Deploy artifacts for release: ${BUILDKITE_TAG}"
 hub release create "${artifacts[@]}" -m "${BUILDKITE_TAG}" "${BUILDKITE_TAG}"
