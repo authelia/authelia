49e739d0091d3d6e733ee1a1b13e5eac581db486
Amir Zarrinkafsh <nightah@me.com>
2020-01-30 18:37:11 +1100

    [Buildkite] Add automatic deployment and removal of Docker images for Branches and PRs (#592)

--- .buildkite/steps/buildimages.sh
+++ .buildkite/steps/buildimages.sh
@@ -10,10 +10,6 @@ do
   echo "      - \"authelia-image-${BUILD_ARCH}.tar.zst\""
   echo "      - \"authelia-linux-${BUILD_ARCH}.tar.gz\""
   echo "      - \"authelia-linux-${BUILD_ARCH}.tar.gz.sha256\""
-  if [[ "${BUILD_ARCH}" != "amd64" ]];
-  then
-    echo "    branches: \"master v*\""
-  fi
   echo "    env:"
   echo "      "ARCH: ${BUILD_ARCH}""
   echo "    key: \"build-docker-${BUILD_ARCH}\""
