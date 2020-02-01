d70cdb085e9d6bbaf33d622f8db189f4a881f642
Clement Michaud <clement.michaud34@gmail.com>
2020-02-01 11:57:48 +0100

    Fix integration tests.

--- internal/suites/suite_duo_push_test.go
+++ internal/suites/suite_duo_push_test.go
@@ -36,14 +36,14 @@ func (s *DuoPushWebDriverSuite) TearDownSuite() {
 }
 
 func (s *DuoPushWebDriverSuite) SetupTest() {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
 	s.doLogout(ctx, s.T())
 }
 
 func (s *DuoPushWebDriverSuite) TearDownTest() {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
 	s.doChangeMethod(ctx, s.T(), "one-time-password")
@@ -72,6 +72,48 @@ func (s *DuoPushWebDriverSuite) TestShouldFailAuthentication() {
 	s.WaitElementLocatedByClassName(ctx, s.T(), "failure-icon")
 }
 
+type DuoPushDefaultRedirectionSuite struct {
+	*SeleniumSuite
+}
+
+func NewDuoPushDefaultRedirectionSuite() *DuoPushDefaultRedirectionSuite {
+	return &DuoPushDefaultRedirectionSuite{SeleniumSuite: new(SeleniumSuite)}
+}
+
+func (s *DuoPushDefaultRedirectionSuite) SetupSuite() {
+	wds, err := StartWebDriver()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s.WebDriverSession = wds
+}
+
+func (s *DuoPushDefaultRedirectionSuite) TearDownSuite() {
+	err := s.WebDriverSession.Stop()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (s *DuoPushDefaultRedirectionSuite) SetupTest() {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	s.doLogout(ctx, s.T())
+}
+
+func (s *DuoPushDefaultRedirectionSuite) TestUserIsRedirectedToDefaultURL() {
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	s.doLoginOneFactor(ctx, s.T(), "john", "password", false, "")
+	s.doChangeMethod(ctx, s.T(), "push-notification")
+	s.verifyURLIs(ctx, s.T(), HomeBaseURL+"/")
+}
+
 type DuoPushSuite struct {
 	suite.Suite
 }
@@ -84,6 +126,10 @@ func (s *DuoPushSuite) TestDuoPushWebDriverSuite() {
 	suite.Run(s.T(), NewDuoPushWebDriverSuite())
 }
 
+func (s *DuoPushSuite) TestDuoPushRedirectionURLSuite() {
+	suite.Run(s.T(), NewDuoPushDefaultRedirectionSuite())
+}
+
 func (s *DuoPushSuite) TestAvailableMethodsScenario() {
 	suite.Run(s.T(), NewAvailableMethodsScenario([]string{
 		"ONE-TIME PASSWORD",
@@ -93,10 +139,6 @@ func (s *DuoPushSuite) TestAvailableMethodsScenario() {
 
 func (s *DuoPushSuite) TestUserPreferencesScenario() {
 	suite.Run(s.T(), NewUserPreferencesScenario())
-}
-
-func (s *DuoPushSuite) TestDefaultRedirectionURLScenario() {
-	suite.Run(s.T(), NewDefaultRedirectionURLScenario())
 }
 
 func TestDuoPushSuite(t *testing.T) {
