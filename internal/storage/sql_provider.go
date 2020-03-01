4088075227126ae291849ce8b800af0e6100a412
Clement Michaud <clement.michaud34@gmail.com>
2020-03-01 16:13:09 +0100

    [FEATURE] Support MySQL as a storage backend.
    Fixes #512.

--- internal/storage/sql_provider.go
+++ internal/storage/sql_provider.go
@@ -34,40 +34,51 @@ type SQLProvider struct {
 func (p *SQLProvider) initialize(db *sql.DB) error {
 	p.db = db
 
-	_, err := db.Exec(fmt.Sprintf("CREATE TABLE IF NOT EXISTS %s (username VARCHAR(100) PRIMARY KEY, second_factor_method VARCHAR(11))", preferencesTableName))
+	_, err := db.Exec(fmt.Sprintf(`
+CREATE TABLE IF NOT EXISTS %s (
+	username VARCHAR(100) PRIMARY KEY,
+	second_factor_method VARCHAR(11)
+)`, preferencesTableName))
 	if err != nil {
-		return err
+		return fmt.Errorf("Unable to create table %s: %v", preferencesTableName, err)
 	}
 
-	_, err = db.Exec(fmt.Sprintf("CREATE TABLE IF NOT EXISTS %s (token VARCHAR(512))", identityVerificationTokensTableName))
+	_, err = db.Exec(fmt.Sprintf(`
+CREATE TABLE IF NOT EXISTS %s (token VARCHAR(512))
+`, identityVerificationTokensTableName))
 	if err != nil {
-		return err
+		return fmt.Errorf("Unable to create table %s: %v", identityVerificationTokensTableName, err)
 	}
 
-	_, err = db.Exec(fmt.Sprintf("CREATE TABLE IF NOT EXISTS %s (username VARCHAR(100) PRIMARY KEY, secret VARCHAR(64))", totpSecretsTableName))
+	_, err = db.Exec(fmt.Sprintf(`
+CREATE TABLE IF NOT EXISTS %s (username VARCHAR(100) PRIMARY KEY, secret VARCHAR(64))
+`, totpSecretsTableName))
 	if err != nil {
-		return err
+		return fmt.Errorf("Unable to create table %s: %v", totpSecretsTableName, err)
 	}
 
 	// keyHandle and publicKey are stored in base64 format
-	_, err = db.Exec(fmt.Sprintf("CREATE TABLE IF NOT EXISTS %s (username VARCHAR(100) PRIMARY KEY, keyHandle TEXT, publicKey TEXT)", u2fDeviceHandlesTableName))
+	_, err = db.Exec(fmt.Sprintf(`
+CREATE TABLE IF NOT EXISTS %s (
+	username VARCHAR(100) PRIMARY KEY,
+	keyHandle TEXT,
+	publicKey TEXT
+)`, u2fDeviceHandlesTableName))
 	if err != nil {
-		return err
+		return fmt.Errorf("Unable to create table %s: %v", u2fDeviceHandlesTableName, err)
 	}
 
-	_, err = db.Exec(fmt.Sprintf("CREATE TABLE IF NOT EXISTS %s (username VARCHAR(100), successful BOOL, time INTEGER)", authenticationLogsTableName))
+	// Create an index on (username, time) because this couple is highly used by the regulation module
+	// to check whether a user is banned.
+	_, err = db.Exec(fmt.Sprintf(`
+CREATE TABLE IF NOT EXISTS %s (
+	username VARCHAR(100),
+	successful BOOL,
+	time INTEGER,
+	INDEX usr_time_idx (username, time)
+)`, authenticationLogsTableName))
 	if err != nil {
-		return err
-	}
-
-	_, err = db.Exec(fmt.Sprintf("CREATE INDEX IF NOT EXISTS time ON %s (time);", authenticationLogsTableName))
-	if err != nil {
-		return err
-	}
-
-	_, err = db.Exec(fmt.Sprintf("CREATE INDEX IF NOT EXISTS username ON %s (username);", authenticationLogsTableName))
-	if err != nil {
-		return err
+		return fmt.Errorf("Unable to create table %s: %v", authenticationLogsTableName, err)
 	}
 	return nil
 }
