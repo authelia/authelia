4c563262c1fba6474788615437129783e2644186
Amir Zarrinkafsh <nightah@me.com>
2020-03-09 00:25:54 +1100

    [Buildkite] Convert static pipelines into dynamic pipelines

---
+++ .buildkite/pipeline.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -u
+
+cat << EOF
+env:
+  BUILDKITE_DOCS_BYPASS: $(git diff --name-only `git merge-base --fork-point master` | sed -rn '/^docs\/.*/!{q1}' && echo true || echo false)
+
+steps:
+  - label: ":hammer_and_wrench: Unit Test"
+    command: "authelia-scripts --log-level debug ci"
+    if: build.env("BUILDKITE_DOCS_BYPASS") != "true"
+
+  - wait:
+    if: build.env("BUILDKITE_DOCS_BYPASS") != "true"
+
+  - label: ":docker: Image Builds"
+    command: ".buildkite/steps/buildimages.sh | buildkite-agent pipeline upload"
+    depends_on: ~
+    if: build.env("BUILDKITE_DOCS_BYPASS") != "true"
+
+  - wait:
+    if: build.env("BUILDKITE_DOCS_BYPASS") != "true"
+
+  - label: ":chrome: Integration Tests"
+    command: ".buildkite/steps/e2etests.sh | buildkite-agent pipeline upload"
+    depends_on:
+      - "build-docker-amd64"
+    if: build.env("BUILDKITE_DOCS_BYPASS") != "true"
+EOF
