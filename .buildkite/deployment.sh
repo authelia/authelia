9acc94cb5c5513c352dd484ecf3295e1d95c50a7
Amir Zarrinkafsh <nightah@me.com>
2020-03-15 21:40:21 +1100

    [Buildkite] Fix unbound variable for docs bypass

--- .buildkite/deployment.sh
+++ .buildkite/deployment.sh
@@ -7,6 +7,8 @@ if [[ $BUILDKITE_TAG == "" ]]; then
   else
     CI_DOCS_BYPASS=$(git diff --name-only `git merge-base --fork-point origin/master` | sed -rn '/^docs\/.*/!{q1}' && echo true || echo false)
   fi
+else
+  CI_DOCS_BYPASS="false"
 fi
 
 cat << EOF
