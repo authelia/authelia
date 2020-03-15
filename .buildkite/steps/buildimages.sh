da9edbc8fcc6966912261511f9e6ef1d1c9c5290
Amir Zarrinkafsh <nightah@me.com>
2020-03-15 14:45:52 +1100

    [FEATURE] Include darwin based binaries for OSX

--- .buildkite/steps/buildimages.sh
+++ .buildkite/steps/buildimages.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -eu
 
-for BUILD_ARCH in amd64 arm32v7 arm64v8; do
+for BUILD_OS in linux darwin; do
+  if [[ $BUILD_OS == "linux" ]]; then
+    for BUILD_ARCH in amd64 arm32v7 arm64v8; do
 cat << EOF
   - label: ":docker: Build Image [${BUILD_ARCH}]"
     command: "authelia-scripts docker build --arch=${BUILD_ARCH}"
@@ -9,10 +11,29 @@ cat << EOF
       build: "true"
     artifact_paths:
       - "authelia-image-${BUILD_ARCH}.tar.zst"
-      - "authelia-linux-${BUILD_ARCH}.tar.gz"
-      - "authelia-linux-${BUILD_ARCH}.tar.gz.sha256"
+      - "authelia-${BUILD_OS}-${BUILD_ARCH}.tar.gz"
+      - "authelia-${BUILD_OS}-${BUILD_ARCH}.tar.gz.sha256"
     env:
       ARCH: "${BUILD_ARCH}"
-    key: "build-docker-${BUILD_ARCH}"
+      OS: "${BUILD_OS}"
+    key: "build-docker-${BUILD_OS}-${BUILD_ARCH}"
 EOF
+    done
+  else
+    for BUILD_ARCH in amd64; do
+cat << EOF
+  - label: ":docker: Build Image [${BUILD_OS}]"
+    command: "authelia-scripts docker build --arch=${BUILD_OS}"
+    agents:
+      build: "true"
+    artifact_paths:
+      - "authelia-${BUILD_OS}-${BUILD_ARCH}.tar.gz"
+      - "authelia-${BUILD_OS}-${BUILD_ARCH}.tar.gz.sha256"
+    env:
+      ARCH: "${BUILD_ARCH}"
+      OS: "${BUILD_OS}"
+    key: "build-docker-${BUILD_OS}-${BUILD_ARCH}"
+EOF
+    done
+  fi
 done
