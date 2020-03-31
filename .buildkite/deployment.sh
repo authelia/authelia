6128081e1fb7c0afdbe40b8327aa2ff3eacd0852
Amir Zarrinkafsh <nightah@me.com>
2020-04-01 09:44:57 +1100

    [Buildkite] Adjust CI bypasses (#804)

--- .buildkite/deployment.sh
+++ .buildkite/deployment.sh
@@ -6,34 +6,34 @@ DIVERGED=$(git merge-base --fork-point origin/master > /dev/null; echo $?)
 if [[ $DIVERGED -eq 0 ]]; then
   if [[ $BUILDKITE_TAG == "" ]]; then
     if [[ $BUILDKITE_BRANCH == "master" ]]; then
-      CI_DOCS_BYPASS=$(git diff --name-only HEAD~1 | sed -rn '/^docs\/.*/!{q1}' && echo true || echo false)
+      CI_BYPASS=$(git diff --name-only HEAD~1 | sed -rn '/^(BREAKING.md|CONTRIBUTING.md|README.md|docs\/.*)/!{q1}' && echo true || echo false)
     else
-      CI_DOCS_BYPASS=$(git diff --name-only `git merge-base --fork-point origin/master` | sed -rn '/^docs\/.*/!{q1}' && echo true || echo false)
+      CI_BYPASS=$(git diff --name-only `git merge-base --fork-point origin/master` | sed -rn '/^(BREAKING.md|CONTRIBUTING.md|README.md|docs\/.*)/!{q1}' && echo true || echo false)
     fi
   else
-    CI_DOCS_BYPASS="false"
+    CI_BYPASS="false"
   fi
 else
-  CI_DOCS_BYPASS="false"
+  CI_BYPASS="false"
 fi
 
 cat << EOF
 env:
-  CI_DOCS_BYPASS: ${CI_DOCS_BYPASS}
+  CI_BYPASS: ${CI_BYPASS}
 
 steps:
   - label: ":docker: Image Deployments"
     command: ".buildkite/steps/deployimages.sh | buildkite-agent pipeline upload"
     concurrency: 1
     concurrency_group: "deployments"
-    if: build.branch == "master" && build.env("CI_DOCS_BYPASS") != "true"
+    if: build.branch == "master" && build.env("CI_BYPASS") != "true"
 
   - label: ":docker: Image Deployments"
     command: ".buildkite/steps/deployimages.sh | buildkite-agent pipeline upload"
-    if: build.branch != "master" && build.env("CI_DOCS_BYPASS") != "true"
+    if: build.branch != "master" && build.env("CI_BYPASS") != "true"
 
   - wait:
-    if: build.env("CI_DOCS_BYPASS") != "true"
+    if: build.env("CI_BYPASS") != "true"
 
   - label: ":docker: Deploy Manifests"
     command: "authelia-scripts docker push-manifest"
@@ -41,13 +41,13 @@ steps:
     concurrency_group: "deployments"
     env:
       DOCKER_CLI_EXPERIMENTAL: "enabled"
-    if: build.branch == "master" && build.env("CI_DOCS_BYPASS") != "true"
+    if: build.branch == "master" && build.env("CI_BYPASS") != "true"
 
   - label: ":docker: Deploy Manifests"
     command: "authelia-scripts docker push-manifest"
     env:
       DOCKER_CLI_EXPERIMENTAL: "enabled"
-    if: build.branch != "master" && build.env("CI_DOCS_BYPASS") != "true"
+    if: build.branch != "master" && build.env("CI_BYPASS") != "true"
 
   - label: ":github: Deploy Artifacts"
     command: "ghartifacts.sh"
@@ -62,7 +62,7 @@ steps:
   - label: ":linux: Deploy AUR"
     command: ".buildkite/steps/aurpackages.sh | buildkite-agent pipeline upload"
     depends_on: ~
-    if: build.tag != null || build.branch == "master" && build.env("CI_DOCS_BYPASS") != "true"
+    if: build.tag != null || build.branch == "master" && build.env("CI_BYPASS") != "true"
 
   - label: ":book: Deploy Documentation"
     command: "syncdoc.sh"
