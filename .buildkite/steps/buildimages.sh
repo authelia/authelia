45278ac590edfb57e43ef4ac37d606f32317c7be
Amir Zarrinkafsh <nightah@me.com>
2020-04-03 10:03:59 +1100

    Utilise hash table for image builds

--- .buildkite/steps/buildimages.sh
+++ .buildkite/steps/buildimages.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 set -eu
 
-for BUILD_OS in linux darwin; do
-  if [[ $BUILD_OS == "linux" ]]; then
-    for BUILD_ARCH in amd64 arm32v7 arm64v8; do
-cat << EOF
-  - label: ":docker: Build Image [${BUILD_ARCH}]"
-    command: "authelia-scripts docker build --arch=${BUILD_ARCH}"
-    agents:
-      build: "true"
-    artifact_paths:
-      - "authelia-image-${BUILD_ARCH}.tar.zst"
-      - "authelia-${BUILD_OS}-${BUILD_ARCH}.tar.gz"
-      - "authelia-${BUILD_OS}-${BUILD_ARCH}.tar.gz.sha256"
-    env:
-      ARCH: "${BUILD_ARCH}"
-      OS: "${BUILD_OS}"
-    key: "build-docker-${BUILD_OS}-${BUILD_ARCH}"
-EOF
-    done
-  else
-    for BUILD_ARCH in amd64; do
+declare -A BUILDS=(["linux"]="amd64 arm32v7 arm64v8" ["darwin"]="amd64")
+
+for BUILD_OS in "${!BUILDS[@]}"; do
+  for BUILD_ARCH in ${BUILDS[$BUILD_OS]}; do
+if [[ "${BUILD_OS}" == "darwin" ]]; then
 cat << EOF
   - label: ":docker: Build Image [${BUILD_OS}]"
     command: "authelia-scripts docker build --arch=${BUILD_OS}"
+EOF
+else
+cat << EOF
+  - label: ":docker: Build Image [${BUILD_ARCH}]"
+    command: "authelia-scripts docker build --arch=${BUILD_ARCH}"
+EOF
+fi
+cat << EOF
     agents:
       build: "true"
     artifact_paths:
+EOF
+if [[ "${BUILD_OS}" == "linux" ]]; then
+cat << EOF
+      - "authelia-image-${BUILD_ARCH}.tar.zst"
+EOF
+fi
+cat << EOF
       - "authelia-${BUILD_OS}-${BUILD_ARCH}.tar.gz"
       - "authelia-${BUILD_OS}-${BUILD_ARCH}.tar.gz.sha256"
     env:
@@ -34,6 +34,5 @@ cat << EOF
       OS: "${BUILD_OS}"
     key: "build-docker-${BUILD_OS}-${BUILD_ARCH}"
 EOF
-    done
-  fi
+  done
 done
