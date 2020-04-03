580152b40b8d8758d5d1c911d854b770f1a0d0c6
Amir Zarrinkafsh <nightah@me.com>
2020-04-03 16:13:24 +1100

    [FEATURE] Include darwin based binaries for OSX (#814)
    Build and publish binary artifacts for Authelia which can be run directly from OSX.

--- .buildkite/steps/ghartifacts.sh
+++ .buildkite/steps/ghartifacts.sh
@@ -6,7 +6,8 @@ artifacts=()
 for FILES in \
   authelia-linux-amd64.tar.gz authelia-linux-amd64.tar.gz.sha256 \
   authelia-linux-arm32v7.tar.gz authelia-linux-arm32v7.tar.gz.sha256 \
-  authelia-linux-arm64v8.tar.gz authelia-linux-arm64v8.tar.gz.sha256;
+  authelia-linux-arm64v8.tar.gz authelia-linux-arm64v8.tar.gz.sha256 \
+  authelia-darwin-amd64.tar.gz authelia-darwin-amd64.tar.gz.sha256;
 do
   artifacts+=(-a "${FILES}")
 done
