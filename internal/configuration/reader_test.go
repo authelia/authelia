60aa02d43be7e17bdcdaf8c2a962414269ed801d
James Elliott <james-d-elliott@users.noreply.github.com>
2020-04-21 19:08:17 +1000

    fix unit tests

--- internal/configuration/reader_test.go
+++ internal/configuration/reader_test.go
@@ -16,7 +16,6 @@ func TestShouldParseConfigFile(t *testing.T) {
 	require.NoError(t, os.Setenv("AUTHELIA_NOTIFIER_SMTP_PASSWORD", "smtp_secret_from_env"))
 	require.NoError(t, os.Setenv("AUTHELIA_SESSION_REDIS_PASSWORD", "redis_secret_from_env"))
 	require.NoError(t, os.Setenv("AUTHELIA_STORAGE_MYSQL_PASSWORD", "mysql_secret_from_env"))
-	require.NoError(t, os.Setenv("AUTHELIA_STORAGE_POSTGRES_PASSWORD", "postgres_secret_from_env"))
 
 	config, errors := Read("./test_resources/config.yml")
 
@@ -37,6 +36,24 @@ func TestShouldParseConfigFile(t *testing.T) {
 	assert.Equal(t, "smtp_secret_from_env", config.Notifier.SMTP.Password)
 	assert.Equal(t, "redis_secret_from_env", config.Session.Redis.Password)
 	assert.Equal(t, "mysql_secret_from_env", config.Storage.MySQL.Password)
+
+	assert.Equal(t, "deny", config.AccessControl.DefaultPolicy)
+	assert.Len(t, config.AccessControl.Rules, 12)
+}
+
+func TestShouldParseAltConfigFile(t *testing.T) {
+	require.NoError(t, os.Setenv("AUTHELIA_STORAGE_POSTGRES_PASSWORD", "postgres_secret_from_env"))
+	config, errors := Read("./test_resources/config_alt.yml")
+	require.Len(t, errors, 0)
+
+	assert.Equal(t, 9091, config.Port)
+	assert.Equal(t, "debug", config.LogLevel)
+	assert.Equal(t, "https://home.example.com:8080/", config.DefaultRedirectionURL)
+	assert.Equal(t, "authelia.com", config.TOTP.Issuer)
+	assert.Equal(t, "secret_from_env", config.JWTSecret)
+
+	assert.Equal(t, "api-123456789.example.com", config.DuoAPI.Hostname)
+	assert.Equal(t, "ABCDEF", config.DuoAPI.IntegrationKey)
 	assert.Equal(t, "postgres_secret_from_env", config.Storage.PostgreSQL.Password)
 
 	assert.Equal(t, "deny", config.AccessControl.DefaultPolicy)
