43de5ce05130f93cc4305d5afc248476ba8c9d51
Amir Zarrinkafsh <nightah@me.com>
2020-05-20 12:28:35 +1000

    Fix unit test Also remove check for body as this will never get triggered, react itself is loaded inside the body so this has to always be successful.

--- web/src/hooks/BasePath.ts
+++ web/src/hooks/BasePath.ts
@@ -1,13 +1,5 @@
-
 export function useBasePath() {
-    const bodyElements = document.getElementsByTagName("body");
-    if (bodyElements.length !== 1) {
-        throw new Error("No body detected");
-    }
-
-    const body = bodyElements[0];
-
-    const basePath = body.getAttribute("data-basepath");
+    const basePath = document.body.getAttribute("data-basepath");
     if (basePath === null) {
         throw new Error("No base path detected");
     }
