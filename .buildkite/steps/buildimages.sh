1409cecf6ed27241ff79804e2d4c6e608b4b19fd
Amir Zarrinkafsh <nightah@me.com>
2020-06-03 23:49:11 +1000

    [CI] Remove unnecessary artifacts for coverage build

--- .buildkite/steps/buildimages.sh
+++ .buildkite/steps/buildimages.sh
@@ -12,8 +12,14 @@ cat << EOF
       build: "${BUILD_OS}-${BUILD_ARCH}"
     artifact_paths:
       - "authelia-image-${BUILD_ARCH}.tar.zst"
+EOF
+if [[ "${BUILD_ARCH}" != "coverage" ]]; then
+cat << EOF
       - "authelia-${BUILD_OS}-${BUILD_ARCH}.tar.gz"
       - "authelia-${BUILD_OS}-${BUILD_ARCH}.tar.gz.sha256"
+EOF
+fi
+cat << EOF
     env:
       ARCH: "${BUILD_ARCH}"
       OS: "${BUILD_OS}"
