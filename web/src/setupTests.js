9cae78740926098bba79709bd1c5946eb10e9c03
Clement Michaud <clement.michaud34@gmail.com>
2020-06-21 14:28:25 +0200

    Fix unit tests
    Signed-off-by: Clement Michaud <clement.michaud34@gmail.com>

--- web/src/setupTests.js
+++ web/src/setupTests.js
@@ -1,4 +1,6 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 document.body.setAttribute("data-basepath", "");
+document.body.setAttribute("data-rememberme", "false");
+document.body.setAttribute("data-disable-resetpassword", "false");
 configure({ adapter: new Adapter() });
