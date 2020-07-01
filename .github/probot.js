73a7d0465a678a44cb2f73ca0999fd43332d9105
Amir Zarrinkafsh <nightah@me.com>
2020-07-01 16:28:12 +1000

    [CI] Attempt to fix PR commentary (#1168)

--- .github/probot.js
+++ .github/probot.js
@@ -2,11 +2,11 @@
 on('pull_request.opened')
     .filter(
         context =>
-            context.payload.pull_request.head.label.startsWith('authelia:')
+            context.payload.pull_request.head.label.slice(0, 9) === 'authelia:'
     )
     .filter(
         context =>
-            !context.payload.pull_request.head.ref.startsWith('dependabot/')
+            context.payload.pull_request.head.ref.slice(0, 11) !== 'dependabot/'
     )
     .comment(`## Artifacts
 These changes are published for testing on Buildkite and DockerHub.
@@ -18,11 +18,11 @@ These changes are published for testing on Buildkite and DockerHub.
 on('pull_request.opened')
     .filter(
         context =>
-            context.payload.pull_request.head.label.startsWith('authelia:')
+            context.payload.pull_request.head.label.slice(0, 9) !== 'authelia:'
     )
     .filter(
         context =>
-            !context.payload.pull_request.head.label.endsWith(':master')
+            !context.payload.pull_request.head.label.includes(':master')
     )
     .comment(`Thanks for choosing to contribute. We lint all PR's with golangci-lint, I may add a review to your PR with some suggestions.
     
@@ -38,11 +38,11 @@ These changes once approved by a team member will be published for testing on Bu
 on('pull_request.opened')
     .filter(
         context =>
-            context.payload.pull_request.head.label.startsWith('authelia:')
+            context.payload.pull_request.head.label.slice(0, 9) !== 'authelia:'
     )
     .filter(
         context =>
-            context.payload.pull_request.head.label.endsWith(':master')
+            context.payload.pull_request.head.label.includes(':master')
     )
     .comment(`Thanks for choosing to contribute. It appears that you're submitting a PR from a forked master branch.
     
