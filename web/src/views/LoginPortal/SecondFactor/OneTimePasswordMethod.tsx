authelia/authelia

commit b34b10322bee7193b9ff6ba05001bcba7fc0745e

[MISC] (deps): Bump react-scripts from 3.4.4 to 4.0.0 in /web
Bumps [react-scripts](https://github.com/facebook/create-react-app/tree/HEAD/packages/react-scripts) from 3.4.4 to 4.0.0.
- [Release notes](https://github.com/facebook/create-react-app/releases)
- [Changelog](https://github.com/facebook/create-react-app/blob/master/CHANGELOG-3.x.md)
- [Commits](https://github.com/facebook/create-react-app/commits/react-scripts@4.0.0/packages/react-scripts)

Signed-off-by: dependabot-preview[bot] <support@dependabot.com>

Address CRA breaking changes

This is related to [breaking changes](https://github.com/facebook/create-react-app/blob/master/CHANGELOG.md#breaking-changes) in CRA specific to ESLint.

web/src/views/LoginPortal/SecondFactor/OneTimePasswordMethod.tsx
@@ -23,7 +23,7 @@ export interface Props {
     onSignInSuccess: (redirectURL: string | undefined) => void;
 }
 
-export default function (props: Props) {
+const OneTimePasswordMethod = function (props: Props) {
     const [passcode, setPasscode] = useState("");
     const [state, setState] = useState(props.authenticationLevel === AuthenticationLevel.TwoFactor
         ? State.Success
@@ -31,8 +31,8 @@ export default function (props: Props) {
     const redirectionURL = useRedirectionURL();
 
     const { onSignInSuccess, onSignInError } = props;
-    const onSignInErrorCallback = useCallback(onSignInError, []);
-    const onSignInSuccessCallback = useCallback(onSignInSuccess, []);
+    const onSignInErrorCallback = useCallback(onSignInError, [onSignInError]);
+    const onSignInSuccessCallback = useCallback(onSignInSuccess, [onSignInSuccess]);
 
     const signInFunc = useCallback(async () => {
         if (props.authenticationLevel === AuthenticationLevel.TwoFactor) {
@@ -89,3 +89,5 @@ export default function (props: Props) {
         </MethodContainer>
     )
 }
+
+export default OneTimePasswordMethod

web/src/views/LoginPortal/SecondFactor/PushNotificationMethod.tsx
@@ -23,15 +23,15 @@ export interface Props {
     onSignInSuccess: (redirectURL: string | undefined) => void;
 }
 
-export default function (props: Props) {
+const PushNotificationMethod = function (props: Props) {
     const style = useStyles();
     const [state, setState] = useState(State.SignInInProgress);
     const redirectionURL = useRedirectionURL();
     const mounted = useIsMountedRef();
 
     const { onSignInSuccess, onSignInError } = props;
-    const onSignInErrorCallback = useCallback(onSignInError, []);
-    const onSignInSuccessCallback = useCallback(onSignInSuccess, []);
+    const onSignInErrorCallback = useCallback(onSignInError, [onSignInError]);
+    const onSignInSuccessCallback = useCallback(onSignInSuccess, [onSignInSuccess]);
 
     const signInFunc = useCallback(async () => {
         if (props.authenticationLevel === AuthenticationLevel.TwoFactor) {
@@ -102,6 +102,8 @@ export default function (props: Props) {
         </MethodContainer>
     )
 }
+
+export default PushNotificationMethod
 
 const useStyles = makeStyles(theme => ({
     icon: {

web/src/views/LoginPortal/SecondFactor/SecurityKeyMethod.tsx
@@ -29,7 +29,7 @@ export interface Props {
     onSignInSuccess: (redirectURL: string | undefined) => void;
 }
 
-export default function (props: Props) {
+const SecurityKeyMethod = function (props: Props) {
     const signInTimeout = 30;
     const [state, setState] = useState(State.WaitTouch);
     const style = useStyles();
@@ -38,8 +38,8 @@ export default function (props: Props) {
     const [timerPercent, triggerTimer,] = useTimer(signInTimeout * 1000 - 500);
 
     const { onSignInSuccess, onSignInError } = props;
-    const onSignInErrorCallback = useCallback(onSignInError, []);
-    const onSignInSuccessCallback = useCallback(onSignInSuccess, []);
+    const onSignInErrorCallback = useCallback(onSignInError, [onSignInError]);
+    const onSignInSuccessCallback = useCallback(onSignInSuccess, [onSignInSuccess]);
 
     const doInitiateSignIn = useCallback(async () => {
         // If user is already authenticated, we don't initiate sign in process.
@@ -101,6 +101,8 @@ export default function (props: Props) {
         </MethodContainer>
     )
 }
+
+export default SecurityKeyMethod
 
 const useStyles = makeStyles(theme => ({
     icon: {

web/src/components/AppStoreBadges.tsx
@@ -12,7 +12,7 @@ export interface Props {
     className?: string;
 }
 
-export default function (props: Props) {
+const AppStoreBadges = function (props: Props) {
     const target = props.targetBlank ? "_blank" : undefined;
 
     const width = props.iconSize;
@@ -28,3 +28,5 @@ export default function (props: Props) {
         </div >
     )
 }
+
+export default AppStoreBadges

web/src/components/ColoredSnackbarContent.test.tsx
@@ -16,6 +16,7 @@ it('should contain the message', () => {
     expect(el.text()).to.contain("this is a success");
 });
 
+/* eslint-disable @typescript-eslint/no-unused-expressions */
 it('should have correct color', () => {
     let el = shallow(<ColoredSnackbarContent level="success" message="this is a success" />);
     expect(el.find(SnackbarContent).props().className!.indexOf("success") > -1).to.be.true;
@@ -29,3 +30,4 @@ it('should have correct color', () => {
     el = shallow(<ColoredSnackbarContent level="warning" message="this is an warning" />);
     expect(el.find(SnackbarContent).props().className!.indexOf("warning") > -1).to.be.true;
 });
+/* eslint-enable @typescript-eslint/no-unused-expressions */

web/src/components/ColoredSnackbarContent.tsx
@@ -24,7 +24,7 @@ export interface Props extends SnackbarContentProps {
     message: string;
 }
 
-export default function (props: Props) {
+const ColoredSnackbarContent = function (props: Props) {
     const classes = useStyles();
     const Icon = variantIcon[props.level];
 
@@ -42,6 +42,8 @@ export default function (props: Props) {
             {...others} />
     )
 }
+
+export default ColoredSnackbarContent
 
 const useStyles = makeStyles(theme => ({
     success: {

web/src/components/FailureIcon.tsx
@@ -4,8 +4,10 @@ import { faTimesCircle } from "@fortawesome/free-regular-svg-icons";
 
 export interface Props { }
 
-export default function (props: Props) {
+const FailureIcon = function (props: Props) {
     return (
         <FontAwesomeIcon icon={faTimesCircle} size="4x" color="red" className="failure-icon" />
     )
 }
+
+export default FailureIcon

web/src/components/FingerTouchIcon.tsx
@@ -9,7 +9,7 @@ export interface Props {
 	strong?: boolean;
 }
 
-export default function (props: Props) {
+const FingerTouchIcon = function (props: Props) {
 	const shakingClass = (props.animated) ? style.shaking : undefined;
 	const strong = (props.strong) ? style.strong : undefined;
 
@@ -40,3 +40,5 @@ export default function (props: Props) {
         </svg>
     )
 }
+
+export default FingerTouchIcon

commit c703318d82ff246f3f5248cb73e86518c7b2f04c

[BUGFIX] Fix re-rendering callbacks
b34b10322bee7193b9ff6ba05001bcba7fc0745e introduced a regression where including deps in the associated useCallback functions React would re-render components.
This resulted in unexpected symptoms like multiple Duo push requests, even if a successful or errored request had already been received.
Empty deps/no re-rendering for the respective callbacks is an expected result therefore we can safely ignore these issues the linter is suggesting needs to be fixed.

web/src/views/LoginPortal/SecondFactor/OneTimePasswordMethod.tsx
@@ -31,8 +31,10 @@ const OneTimePasswordMethod = function (props: Props) {
     const redirectionURL = useRedirectionURL();
 
     const { onSignInSuccess, onSignInError } = props;
-    const onSignInErrorCallback = useCallback(onSignInError, [onSignInError]);
-    const onSignInSuccessCallback = useCallback(onSignInSuccess, [onSignInSuccess]);
+    /* eslint-disable react-hooks/exhaustive-deps */
+    const onSignInErrorCallback = useCallback(onSignInError, []);
+    const onSignInSuccessCallback = useCallback(onSignInSuccess, []);
+    /* eslint-enable react-hooks/exhaustive-deps */
 
     const signInFunc = useCallback(async () => {
         if (props.authenticationLevel === AuthenticationLevel.TwoFactor) {

web/src/views/LoginPortal/SecondFactor/PushNotificationMethod.tsx
@@ -30,8 +30,10 @@ const PushNotificationMethod = function (props: Props) {
     const mounted = useIsMountedRef();
 
     const { onSignInSuccess, onSignInError } = props;
-    const onSignInErrorCallback = useCallback(onSignInError, [onSignInError]);
-    const onSignInSuccessCallback = useCallback(onSignInSuccess, [onSignInSuccess]);
+    /* eslint-disable react-hooks/exhaustive-deps */
+    const onSignInErrorCallback = useCallback(onSignInError, []);
+    const onSignInSuccessCallback = useCallback(onSignInSuccess, []);
+    /* eslint-enable react-hooks/exhaustive-deps */
 
     const signInFunc = useCallback(async () => {
         if (props.authenticationLevel === AuthenticationLevel.TwoFactor) {

web/src/views/LoginPortal/SecondFactor/SecurityKeyMethod.tsx
@@ -38,8 +38,10 @@ const SecurityKeyMethod = function (props: Props) {
     const [timerPercent, triggerTimer,] = useTimer(signInTimeout * 1000 - 500);
 
     const { onSignInSuccess, onSignInError } = props;
-    const onSignInErrorCallback = useCallback(onSignInError, [onSignInError]);
-    const onSignInSuccessCallback = useCallback(onSignInSuccess, [onSignInSuccess]);
+    /* eslint-disable react-hooks/exhaustive-deps */
+    const onSignInErrorCallback = useCallback(onSignInError, []);
+    const onSignInSuccessCallback = useCallback(onSignInSuccess, []);
+    /* eslint-enable react-hooks/exhaustive-deps */
 
     const doInitiateSignIn = useCallback(async () => {
         // If user is already authenticated, we don't initiate sign in process.