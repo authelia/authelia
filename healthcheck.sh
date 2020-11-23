26d09413643a4c88366d6779f957c1fa14b38f5c
Amir Zarrinkafsh <nightah@me.com>
2020-11-23 17:21:31 +1100

    [MISC] Update Docker healthcheck script
    This change now determines the host for Docker healthcheck from the `configuration.yml` that Authelia is started with.
    
    The script has also been run through shellcheck and terminated with a newline in hopes to resolve a number of unreproducible issues.

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,17 +1,22 @@
 #!/bin/sh
 
-AUTHELIA_CONFIG=$(ps | grep authelia | awk '{print $6}' | head -1)
-AUTHELIA_SCHEME=$(cat "${AUTHELIA_CONFIG}" | grep ^tls)
-AUTHELIA_PORT=$(cat "${AUTHELIA_CONFIG}" | grep ^port | sed -e 's/port: //')
+AUTHELIA_CONFIG=$(pgrep -af authelia | awk '{print $4}')
+AUTHELIA_SCHEME=$(grep ^tls "${AUTHELIA_CONFIG}")
+AUTHELIA_HOST=$(grep ^host "${AUTHELIA_CONFIG}" | sed -e 's/host: //')
+AUTHELIA_PORT=$(grep ^port "${AUTHELIA_CONFIG}" | sed -e 's/port: //')
 
-if [[ -z ${AUTHELIA_PORT} ]]; then
-  AUTHELIA_PORT=9091
-fi
-
-if [[ -z ${AUTHELIA_SCHEME} ]]; then
+if [ -z "${AUTHELIA_SCHEME}" ]; then
   AUTHELIA_SCHEME=http
 else
   AUTHELIA_SCHEME=https
 fi
 
-wget --quiet --tries=1 --spider ${AUTHELIA_SCHEME}://localhost:${AUTHELIA_PORT}/api/state || exit 1
+if [ -z "${AUTHELIA_HOST}" ] || [ "${AUTHELIA_HOST}" = "0.0.0.0" ]; then
+  AUTHELIA_HOST=localhost
+fi
+
+if [ -z "${AUTHELIA_PORT}" ]; then
+  AUTHELIA_PORT=9091
+fi
+
+wget --quiet --tries=1 --spider "${AUTHELIA_SCHEME}://${AUTHELIA_HOST}:${AUTHELIA_PORT}/api/state" || exit 1
