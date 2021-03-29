4a4dff9736f7b73ebdd5bf86445d1f02fffbdb2e
Clément Michaud <clement.michaud34@gmail.com>
2021-03-29 23:51:25 +0200

    fix: fix arguments for running oidc-tester-app.

--- internal/suites/example/compose/oidc-client/entrypoint.sh
+++ internal/suites/example/compose/oidc-client/entrypoint.sh
@@ -2,6 +2,6 @@
 
 while true;
 do
-    oidc-tester-app --oidc-provider-url https://login.example.com:8080 --client-id oidc-tester-app --client-secret foobar --scopes openid,profile,email --redirect-uri https://oidc.example.com:8080/oauth2/callback
+    oidc-tester-app --issuer https://login.example.com:8080 --id oidc-tester-app --secret foobar --scopes openid,profile,email --redirect-domain oidc.example.com
     sleep 5
 done
