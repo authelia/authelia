8ca3e34069e373e74b1f6ebc4953a33882706e0f
James Elliott <james-d-elliott@users.noreply.github.com>
2021-04-13 13:13:14 +1000

    test: add logging to debug test

--- internal/suites/action_logout.go
+++ internal/suites/action_logout.go
@@ -13,12 +13,21 @@ func (wds *WebDriverSession) doLogout(ctx context.Context, t *testing.T) {
 }
 
 func (wds *WebDriverSession) doLogoutWithRedirect(ctx context.Context, t *testing.T, redirectURL string, firstFactor bool) {
+	// TODO: REMOVE.
+	fmt.Printf("DEBUG: Attempting Logout With redirect to: %s\n", redirectURL)
+
 	wds.doVisit(t, fmt.Sprintf("%s%s%s", GetLoginBaseURL(), "/logout?rd=", url.QueryEscape(redirectURL)))
 
 	if firstFactor {
+		// TODO: REMOVE.
+		fmt.Printf("DEBUG: Attempting Logout Expected Page is First Factor\n")
+
 		wds.verifyIsFirstFactorPage(ctx, t)
 		return
 	}
+
+	// TODO: REMOVE.
+	fmt.Printf("DEBUG: Attempting Logout Expected Page is %s\n", redirectURL)
 
 	wds.verifyURLIs(ctx, t, redirectURL)
 }
