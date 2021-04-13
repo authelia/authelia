e7563330cb37fbfa2b0e9e026fdab89585ac4932
James Elliott <james-d-elliott@users.noreply.github.com>
2021-04-13 13:52:55 +1000

    ci: remove debug logs

--- internal/suites/verify_url_is.go
+++ internal/suites/verify_url_is.go
@@ -2,7 +2,6 @@ package suites
 
 import (
 	"context"
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -10,19 +9,10 @@ import (
 )
 
 func (wds *WebDriverSession) verifyURLIs(ctx context.Context, t *testing.T, url string) {
-	// TODO: REMOVE.
-	fmt.Printf("DEBUG: Verifying URL is: %s\n", url)
-
 	err := wds.Wait(ctx, func(driver selenium.WebDriver) (bool, error) {
 		currentURL, err := driver.CurrentURL()
 
-		// TODO: REMOVE.
-		fmt.Printf("DEBUG: Current URL is: %s\n", currentURL)
-
 		if err != nil {
-			// TODO: REMOVE.
-			fmt.Printf("DEBUG: Err for Current URL is: %v\n", err)
-
 			return false, err
 		}
 		return currentURL == url, nil
