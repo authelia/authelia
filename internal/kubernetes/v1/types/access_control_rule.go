dad14b59dcaf61a89d03873a62ecb1c3244d82b6
Alex Gustafsson <alex-gustafsson@live.se>
2021-05-29 13:44:32 +0200

    Add schema support for all access control rule values

--- internal/kubernetes/v1/types/access_control_rule.go
+++ internal/kubernetes/v1/types/access_control_rule.go
@@ -17,18 +17,57 @@ type AccessControlRule struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 	Spec              struct {
-		Domain  string `json:"domain"`
-		Policy  string `json:"policy"`
-		Subject string `json:"subject"`
+		Domains   []string   `json:"domains"`
+		Policy    string     `json:"policy"`
+		Subjects  [][]string `json:"subjects"`
+		Networks  []string   `json:"networks"`
+		Resources []string   `json:"resources"`
+		Methods   []string   `json:"methods"`
 	} `json:"spec"`
 }
 
 func (in *AccessControlRule) DeepCopyInto(out *AccessControlRule) {
 	out.TypeMeta = in.TypeMeta
 	out.ObjectMeta = in.ObjectMeta
-	out.Spec.Domain = in.Spec.Domain
 	out.Spec.Policy = in.Spec.Policy
-	out.Spec.Subject = in.Spec.Subject
+
+	if in.Spec.Domains != nil {
+		out.Spec.Domains = make([]string, len(in.Spec.Domains))
+		for i := range in.Spec.Domains {
+			out.Spec.Domains[i] = in.Spec.Domains[i]
+		}
+	}
+
+	if in.Spec.Subjects != nil {
+		out.Spec.Subjects = make([][]string, len(in.Spec.Subjects))
+		for i := range in.Spec.Subjects {
+			out.Spec.Subjects[i] = make([]string, len(in.Spec.Subjects[i]))
+			for j := range in.Spec.Subjects[i] {
+				out.Spec.Subjects[i][j] = in.Spec.Subjects[i][j]
+			}
+		}
+	}
+
+	if in.Spec.Networks != nil {
+		out.Spec.Networks = make([]string, len(in.Spec.Networks))
+		for i := range in.Spec.Networks {
+			out.Spec.Networks[i] = in.Spec.Networks[i]
+		}
+	}
+
+	if in.Spec.Resources != nil {
+		out.Spec.Resources = make([]string, len(in.Spec.Resources))
+		for i := range in.Spec.Resources {
+			out.Spec.Resources[i] = in.Spec.Resources[i]
+		}
+	}
+
+	if in.Spec.Methods != nil {
+		out.Spec.Methods = make([]string, len(in.Spec.Methods))
+		for i := range in.Spec.Methods {
+			out.Spec.Methods[i] = in.Spec.Methods[i]
+		}
+	}
 }
 
 func (in *AccessControlRule) DeepCopyObject() runtime.Object {
