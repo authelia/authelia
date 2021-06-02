8838e81264ef03d4096aefe8c8de9fe57f1ef76b
Freebien <freebien@gmail.com>
2021-06-02 18:40:41 +0200

    feat(authentication): add ssha hash for ldap
    LDAP was storing passwords in cleartext. This commit add a SSHA hash on
    the password for secure storage.

---
+++ internal/authentication/ldap_ssha.go
@@ -0,0 +1,53 @@
+package authentication
+
+import (
+	"bytes"
+	"crypto/rand"
+	"crypto/sha1"
+	"encoding/base64"
+	"fmt"
+)
+
+type SSHA []byte
+
+// Encode encodes the []byte of raw password
+func (pass SSHA) Encode() ([]byte, error) {
+	hash := makeSSHAHash(pass, makeSalt())
+	b64 := base64.StdEncoding.EncodeToString(hash)
+	return []byte(fmt.Sprintf("{SSHA}%s", b64)), nil
+}
+
+// Matches matches the encoded password and the raw password
+func (pass SSHA) Matches(encodedPassPhrase []byte) bool {
+	//strip the {SSHA}
+	eppS := string(encodedPassPhrase)[6:]
+	hash, err := base64.StdEncoding.DecodeString(eppS)
+	if err != nil {
+		return false
+	}
+	salt := hash[len(hash)-4:]
+
+	sha := sha1.New()
+	sha.Write(pass)
+	sha.Write(salt)
+	sum := sha.Sum(nil)
+
+	return bytes.Compare(sum, hash[:len(hash)-4]) == 0
+}
+
+// makeSalt make a 4 byte array containing random bytes.
+func makeSalt() []byte {
+	sbytes := make([]byte, 4)
+	rand.Read(sbytes)
+	return sbytes
+}
+
+// makeSSHAHash make hasing using SHA-1 with salt. This is not the final output though. You need to append {SSHA} string with base64 of this hash.
+func makeSSHAHash(passphrase, salt []byte) []byte {
+	sha := sha1.New()
+	sha.Write(passphrase)
+	sha.Write(salt)
+
+	h := sha.Sum(nil)
+	return append(h, salt...)
+}
--- internal/authentication/ldap_user_provider.go
+++ internal/authentication/ldap_user_provider.go
@@ -290,16 +290,19 @@ func (p *LDAPUserProvider) UpdatePassword(inputUsername string, newPassword stri
 	}
 
 	modifyRequest := ldap.NewModifyRequest(profile.DN, nil)
-
+	newPassBytes, err := SSHA([]byte(newPassword)).Encode()
+	if err != nil {
+		return fmt.Errorf("Unable to convert to SSHA: %s", err)
+	}
 	switch p.configuration.Implementation {
 	case schema.LDAPImplementationActiveDirectory:
 		utf16 := unicode.UTF16(unicode.LittleEndian, unicode.IgnoreBOM)
 		// The password needs to be enclosed in quotes
 		// https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-adts/6e803168-f140-4d23-b2d3-c3a8ab5917d2
-		pwdEncoded, _ := utf16.NewEncoder().String(fmt.Sprintf("\"%s\"", newPassword))
+		pwdEncoded, _ := utf16.NewEncoder().String(fmt.Sprintf("\"%s\"", newPassBytes))
 		modifyRequest.Replace("unicodePwd", []string{pwdEncoded})
 	default:
-		modifyRequest.Replace("userPassword", []string{newPassword})
+		modifyRequest.Replace("userPassword", []string{string(newPassBytes)})
 	}
 
 	err = conn.Modify(modifyRequest)
