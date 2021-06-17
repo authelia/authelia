54da1ad464abdb35fed59db374ce8dfb5520fdfb
James Elliott <james-d-elliott@users.noreply.github.com>
2021-06-17 20:54:10 +1000

    build(deps): replace jwt-go
    This replaces github.com/dgrijalva/jwt-go and github.com/form3tech-oss/jwt-go with github.com/golang-jwt/jwt which will be the maintained package going forward.

--- internal/handlers/handler_register_u2f_step1_test.go
+++ internal/handlers/handler_register_u2f_step1_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/dgrijalva/jwt-go"
+	"github.com/golang-jwt/jwt"
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
--- internal/middlewares/types.go
+++ internal/middlewares/types.go
@@ -1,7 +1,7 @@
 package middlewares
 
 import (
-	"github.com/dgrijalva/jwt-go"
+	"github.com/golang-jwt/jwt"
 	"github.com/sirupsen/logrus"
 	"github.com/valyala/fasthttp"
 
