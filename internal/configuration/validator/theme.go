519fa7beaf77ecdab3d656bc0f1cc29151b2fef5
Alex Gustafsson <alex-gustafsson@live.se>
2021-06-17 08:05:32 +0200

    fix(configuration): resolve merge conflicts

--- internal/configuration/validator/theme.go
+++ internal/configuration/validator/theme.go
@@ -9,7 +9,12 @@ import (
 
 // ValidateTheme validates and update Theme configuration.
 func ValidateTheme(configuration *schema.Configuration, validator *schema.StructValidator) {
+	if configuration.Theme == "" {
+		configuration.Theme = "light"
+	}
+
 	validThemes := regexp.MustCompile("light|dark|grey|auto")
+
 	if !validThemes.MatchString(configuration.Theme) {
 		validator.Push(fmt.Errorf("Theme: %s is not valid, valid themes are: \"light\", \"dark\", \"grey\" or \"auto\"", configuration.Theme))
 	}
