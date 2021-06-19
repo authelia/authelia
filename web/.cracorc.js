43182d6efd916868250c36d6a1b2028452a996eb
Amir Zarrinkafsh <nightah@me.com>
2021-06-19 14:51:25 +1000

    refactor(web): rename extended tsconfig.json

--- web/.cracorc.js
+++ web/.cracorc.js
@@ -11,7 +11,7 @@ module.exports = {
             options: {
                 source: "tsconfig",
                 baseUrl: "./src",
-                tsConfigPath: "./tsconfig.extend.json",
+                tsConfigPath: "./tsconfig.aliases.json",
             }
         }
     ]
