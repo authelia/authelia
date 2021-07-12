d5c806327c23ca2a5e2e206820407a8e6719fe8c
Clément Michaud <clement.michaud34@gmail.com>
2021-07-12 18:41:58 +0200

    Apply suggestions from code review
    Co-authored-by: Amir Zarrinkafsh <nightah@me.com>

--- internal/server/error_handler.go
+++ internal/server/error_handler.go
@@ -14,15 +14,15 @@ func autheliaErrorHandler(ctx *fasthttp.RequestCtx, err error) {
 
 	if _, ok := err.(*fasthttp.ErrSmallBuffer); ok {
 		// Note: Getting X-Forwarded-For or Request URI is impossible for ths error.
-		logger.Tracef("request was too large to handle from client %s. Response Code %d.", ctx.RemoteIP().String(), fasthttp.StatusRequestHeaderFieldsTooLarge)
+		logger.Tracef("request was too large to handle from client %s. response code %d.", ctx.RemoteIP().String(), fasthttp.StatusRequestHeaderFieldsTooLarge)
 		ctx.Error("request header too large", fasthttp.StatusRequestHeaderFieldsTooLarge)
 	} else if netErr, ok := err.(*net.OpError); ok && netErr.Timeout() {
 		// TODO: Add X-Forwarded-For Check here.
-		logger.Tracef("request timeout occurred while handling from client %s: %s. Response Code %d.", ctx.RemoteIP().String(), ctx.RequestURI(), fasthttp.StatusRequestTimeout)
+		logger.Tracef("request timeout occurred while handling from client %s: %s. response code %d.", ctx.RemoteIP().String(), ctx.RequestURI(), fasthttp.StatusRequestTimeout)
 		ctx.Error("request timeout", fasthttp.StatusRequestTimeout)
 	} else {
 		// TODO: Add X-Forwarded-For Check here.
-		logger.Tracef("an unknown error occurred while handling a request from client %s: %s. Response Code %d.", ctx.RemoteIP().String(), ctx.RequestURI(), fasthttp.StatusBadRequest)
+		logger.Tracef("an unknown error occurred while handling a request from client %s: %s. response code %d.", ctx.RemoteIP().String(), ctx.RequestURI(), fasthttp.StatusBadRequest)
 		ctx.Error("error when parsing request", fasthttp.StatusBadRequest)
 	}
 }
