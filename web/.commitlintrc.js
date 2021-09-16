11032bdf9328e5d4b13fa6dc324293dd718d7912
Amir Zarrinkafsh <nightah@me.com>
2021-09-16 12:26:34 +1000

    fix(server): remove obselete memory check warning (#2380)
    Given the fact that many Linux OSes are defaulting to CGroups v2 and also Authelia changing the default memory config for argon2id this warning is now obselete.

--- web/.commitlintrc.js
+++ web/.commitlintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
     extends: ["@commitlint/config-conventional"],
     rules: {
+        "body-max-line-length": [2, "always", "Infinity"],
         "body-min-length": [2, "always", 20],
         "header-case": [2, "always", "lower-case"],
         "header-max-length": [2, "always", 72],
