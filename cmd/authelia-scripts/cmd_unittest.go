3d312cf3b94c98f5c17b6388fb257187f705dbbf
Amir Zarrinkafsh <nightah@me.com>
2021-09-29 17:24:21 +1000

    refactor: replace yarn with pnpm (#2424)
    * Check for pnpm in authelia-scripts
    * Improve husky hooks to check for required apps
    * Use pnpm in coverage dockerfile
    * Use pnpm in dev workflow
    * Stop buildx log truncation
    * Ignore pnpm lockfile in yamllint
    * Update versions required for docker and docker-compose in contributing docs

--- cmd/authelia-scripts/cmd_unittest.go
+++ cmd/authelia-scripts/cmd_unittest.go
@@ -17,7 +17,7 @@ func RunUnitTest(cobraCmd *cobra.Command, args []string) {
 		log.Fatal(err)
 	}
 
-	cmd := utils.Shell("yarn test")
+	cmd := utils.Shell("pnpm test")
 	cmd.Dir = webDirectory
 
 	cmd.Env = append(os.Environ(), "CI=true")
