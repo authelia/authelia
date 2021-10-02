898c58f1010114ad7db08438106c9a7d7a8a1e77
James Elliott <james-d-elliott@users.noreply.github.com>
2021-10-02 14:45:22 +1000

    fix: nil ptr

--- internal/suites/suite_standalone_test.go
+++ internal/suites/suite_standalone_test.go
@@ -106,6 +106,8 @@ func (s *StandaloneWebDriverSuite) TestShouldCheckUserIsAskedToRegisterDevice() 
 
 	// Clean up any TOTP secret already in DB.
 	provider := storage.NewSQLiteProvider("/tmp/db.sqlite3")
+
+	require.NoError(s.T(), provider.StartupCheck(nil))
 	require.NoError(s.T(), provider.DeleteTOTPSecret(ctx, username))
 
 	// Login one factor.
