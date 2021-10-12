9b12117ed6aa4ae716b1ffba94657c51e2a7efb3
Amir Zarrinkafsh <nightah@me.com>
2021-10-12 15:11:33 +1100

    build(deps): update swagger-ui to v3.52.4

--- cmd/authelia-scripts/cmd_build.go
+++ cmd/authelia-scripts/cmd_build.go
@@ -86,7 +86,7 @@ func buildFrontend(branch string) {
 }
 
 func buildSwagger() {
-	swaggerVer := "3.52.3"
+	swaggerVer := "3.52.4"
 	cmd := utils.CommandWithStdout("bash", "-c", "wget -q https://github.com/swagger-api/swagger-ui/archive/v"+swaggerVer+".tar.gz -O ./v"+swaggerVer+".tar.gz")
 
 	err := cmd.Run()
