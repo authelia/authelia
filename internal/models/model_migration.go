aa88cd3b112b54faf120cc5333d677d0805d0210
James Elliott <james-d-elliott@users.noreply.github.com>
2021-10-26 15:44:23 +1100

    fix: model not using time.Time

--- internal/models/model_migration.go
+++ internal/models/model_migration.go
@@ -1,9 +1,13 @@
 package models
 
+import (
+	"time"
+)
+
 type Migration struct {
-	ID      int    `db:"id"`
-	Time    Time   `db:"time"`
-	Prior   int    `db:"prior"`
-	Current int    `db:"current"`
-	Version string `db:"version"`
+	ID      int       `db:"id"`
+	Time    time.Time `db:"time"`
+	Prior   int       `db:"prior"`
+	Current int       `db:"current"`
+	Version string    `db:"version"`
 }
