a24d43c7fadba1a6ddba78a72eb407698303326e
James Elliott <james-d-elliott@users.noreply.github.com>
2021-11-10 08:49:30 +1100

    fix: use crlf

--- internal/notification/const.go
+++ internal/notification/const.go
@@ -4,16 +4,16 @@ const fileNotifierMode = 0600
 const rfc5322DateTimeLayout = "Mon, 2 Jan 2006 15:04:05 -0700"
 
 var (
-	newline           = []byte("\n")
-	newlineDouble     = []byte("\n\n")
+	newline           = []byte("\r\n")
+	newlineDouble     = []byte("\r\n\r\n")
 	boundarySeparator = []byte("--")
 
 	headerDate        = []byte("Date: ")
-	headerFrom        = []byte("\nFrom: ")
-	headerTo          = []byte("\nTo: ")
-	headerSubject     = []byte("\nSubject: ")
-	headerMIMEVersion = []byte("\nMIME-Version: 1.0\nContent-Type: multipart/alternative; boundary=")
+	headerFrom        = []byte("\r\nFrom: ")
+	headerTo          = []byte("\r\nTo: ")
+	headerSubject     = []byte("\r\nSubject: ")
+	headerMIMEVersion = []byte("\r\nMIME-Version: 1.0\r\nContent-Type: multipart/alternative; boundary=")
 
-	headerTextPlain       = []byte("\nContent-Type: text/plain; charset=\"UTF-8\"\nContent-Transfer-Encoding: quoted-printable\nContent-Disposition: inline")
-	headerContentTypeHTML = []byte("\nContent-Type: text/html; charset=\"UTF-8\"")
+	headerTextPlain       = []byte("\r\nContent-Type: text/plain; charset=\"UTF-8\"\r\nContent-Transfer-Encoding: quoted-printable\r\nContent-Disposition: inline")
+	headerContentTypeHTML = []byte("\r\nContent-Type: text/html; charset=\"UTF-8\"")
 )
