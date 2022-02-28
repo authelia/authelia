16362825ab0ba0dad9b140a31e2ec34e266690da
James Elliott <james-d-elliott@users.noreply.github.com>
2022-02-28 20:01:51 +1100

    refactor: remove unused consts

--- internal/configuration/schema/const.go
+++ internal/configuration/schema/const.go
@@ -1,22 +1,6 @@
 package schema
 
-import (
-	"time"
-)
-
 const argon2id = "argon2id"
-
-// ProfileRefreshDisabled represents a value for refresh_interval that disables the check entirely.
-const ProfileRefreshDisabled = "disable"
-
-// ProfileRefreshAlways represents a value for refresh_interval that's the same as 0ms.
-const ProfileRefreshAlways = "always"
-
-// RefreshIntervalDefault represents the default value of refresh_interval.
-const RefreshIntervalDefault = "5m"
-
-// RefreshIntervalAlways represents the duration value refresh interval should have if set to always.
-const RefreshIntervalAlways = 0 * time.Millisecond
 
 // LDAPImplementationCustom is the string for the custom LDAP implementation.
 const LDAPImplementationCustom = "custom"
