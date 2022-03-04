406aa8c0e2f3bcc698234020941bf2f7bdf1825d
James Elliott <james-d-elliott@users.noreply.github.com>
2022-03-04 12:37:06 +1100

    build(deps): update font awesome to v6 (major)

--- web/src/components/FailureIcon.tsx
+++ web/src/components/FailureIcon.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 
+import { IconLookup } from "@fortawesome/fontawesome-svg-core";
 import { faTimesCircle } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export interface Props {}
 
 const FailureIcon = function (props: Props) {
-    return <FontAwesomeIcon icon={faTimesCircle} size="4x" color="red" className="failure-icon" />;
+    return <FontAwesomeIcon icon={faTimesCircle as IconLookup} size="4x" color="red" className="failure-icon" />;
 };
 
 export default FailureIcon;
