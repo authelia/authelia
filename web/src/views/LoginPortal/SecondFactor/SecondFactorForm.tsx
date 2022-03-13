fb5d5b232e39258fd774aa3154a18c39c9634117
James Elliott <james-d-elliott@users.noreply.github.com>
2022-03-13 14:48:45 +1100

    fix: remove methods button

--- web/src/views/LoginPortal/SecondFactor/SecondFactorForm.tsx
+++ web/src/views/LoginPortal/SecondFactor/SecondFactorForm.tsx
@@ -99,10 +99,12 @@ const SecondFactorForm = function (props: Props) {
                     <Button color="secondary" onClick={handleLogoutClick} id="logout-button">
                         {translate("Logout")}
                     </Button>
-                    {" | "}
-                    <Button color="secondary" onClick={handleMethodSelectionClick} id="methods-button">
-                        {translate("Methods")}
-                    </Button>
+                    {props.configuration.available_methods.size > 1 ? " | " : null}
+                    {props.configuration.available_methods.size > 1 ? (
+                        <Button color="secondary" onClick={handleMethodSelectionClick} id="methods-button">
+                            {translate("Methods")}
+                        </Button>
+                    ) : null}
                 </Grid>
                 <Grid item xs={12} className={style.methodContainer}>
                     <Routes>
