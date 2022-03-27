54946a047f5302c9326927ee8929fd5bbf8fd179
James Elliott <james-d-elliott@users.noreply.github.com>
2022-03-27 11:27:07 +1100

    test: use subtests

--- internal/oidc/amr_test.go
+++ internal/oidc/amr_test.go
@@ -1,137 +1,189 @@
 package oidc
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
+type testAMRWant struct {
+	FactorKnowledge, FactorPossession, MultiFactorAuthentication bool
+	ChannelBrowser, ChannelService, MultiChannelAuthentication   bool
+
+	RFC8176 []string
+}
+
 func TestAuthenticationMethodsReferences(t *testing.T) {
-	var amr AuthenticationMethodsReferences
+	testCases := []struct {
+		desc string
+		is   AuthenticationMethodsReferences
+		want testAMRWant
+	}{
+		{
+			desc: "Username and Password",
 
-	amr = AuthenticationMethodsReferences{
-		UsernameAndPassword: true,
+			is: AuthenticationMethodsReferences{UsernameAndPassword: true},
+			want: testAMRWant{
+				FactorKnowledge:            true,
+				FactorPossession:           false,
+				MultiFactorAuthentication:  false,
+				ChannelBrowser:             true,
+				ChannelService:             false,
+				MultiChannelAuthentication: false,
+				RFC8176:                    []string{"pwd"},
+			},
+		},
+		{
+			desc: "TOTP",
+
+			is: AuthenticationMethodsReferences{TOTP: true},
+			want: testAMRWant{
+				FactorKnowledge:            false,
+				FactorPossession:           true,
+				MultiFactorAuthentication:  false,
+				ChannelBrowser:             true,
+				ChannelService:             false,
+				MultiChannelAuthentication: false,
+				RFC8176:                    []string{"otp"},
+			},
+		},
+		{
+			desc: "Webauthn",
+
+			is: AuthenticationMethodsReferences{Webauthn: true},
+			want: testAMRWant{
+				FactorKnowledge:            false,
+				FactorPossession:           true,
+				MultiFactorAuthentication:  false,
+				ChannelBrowser:             true,
+				ChannelService:             false,
+				MultiChannelAuthentication: false,
+				RFC8176:                    []string{"hwk"},
+			},
+		},
+		{
+			desc: "Webauthn User Presence",
+
+			is: AuthenticationMethodsReferences{WebauthnUserPresence: true},
+			want: testAMRWant{
+				FactorKnowledge:            false,
+				FactorPossession:           false,
+				MultiFactorAuthentication:  false,
+				ChannelBrowser:             false,
+				ChannelService:             false,
+				MultiChannelAuthentication: false,
+				RFC8176:                    []string{"user"},
+			},
+		},
+		{
+			desc: "Webauthn User Verified",
+
+			is: AuthenticationMethodsReferences{WebauthnUserVerified: true},
+			want: testAMRWant{
+				FactorKnowledge:            false,
+				FactorPossession:           false,
+				MultiFactorAuthentication:  false,
+				ChannelBrowser:             false,
+				ChannelService:             false,
+				MultiChannelAuthentication: false,
+				RFC8176:                    []string{"pin"},
+			},
+		},
+		{
+			desc: "Webauthn with User Presence and Verified",
+
+			is: AuthenticationMethodsReferences{Webauthn: true, WebauthnUserVerified: true, WebauthnUserPresence: true},
+			want: testAMRWant{
+				FactorKnowledge:            false,
+				FactorPossession:           true,
+				MultiFactorAuthentication:  false,
+				ChannelBrowser:             true,
+				ChannelService:             false,
+				MultiChannelAuthentication: false,
+				RFC8176:                    []string{"hwk", "user", "pin"},
+			},
+		},
+		{
+			desc: "Duo",
+
+			is: AuthenticationMethodsReferences{Duo: true},
+			want: testAMRWant{
+				FactorKnowledge:            false,
+				FactorPossession:           true,
+				MultiFactorAuthentication:  false,
+				ChannelBrowser:             false,
+				ChannelService:             true,
+				MultiChannelAuthentication: false,
+				RFC8176:                    []string{"sms"},
+			},
+		},
+		{
+			desc: "Duo Webauthn TOTP",
+
+			is: AuthenticationMethodsReferences{Duo: true, Webauthn: true, TOTP: true},
+			want: testAMRWant{
+				FactorKnowledge:            false,
+				FactorPossession:           true,
+				MultiFactorAuthentication:  false,
+				ChannelBrowser:             true,
+				ChannelService:             true,
+				MultiChannelAuthentication: true,
+				RFC8176:                    []string{"sms", "hwk", "otp", "mca"},
+			},
+		},
+		{
+			desc: "Duo TOTP",
+
+			is: AuthenticationMethodsReferences{Duo: true, TOTP: true},
+			want: testAMRWant{
+				FactorKnowledge:            false,
+				FactorPossession:           true,
+				MultiFactorAuthentication:  false,
+				ChannelBrowser:             true,
+				ChannelService:             true,
+				MultiChannelAuthentication: true,
+				RFC8176:                    []string{"sms", "otp", "mca"},
+			},
+		},
+		{
+			desc: "Username and Password with Duo",
+
+			is: AuthenticationMethodsReferences{Duo: true, UsernameAndPassword: true},
+			want: testAMRWant{
+				FactorKnowledge:            true,
+				FactorPossession:           true,
+				MultiFactorAuthentication:  true,
+				ChannelBrowser:             true,
+				ChannelService:             true,
+				MultiChannelAuthentication: true,
+				RFC8176:                    []string{"pwd", "sms", "mfa", "mca"},
+			},
+		},
 	}
 
-	assert.True(t, amr.FactorKnowledge())
-	assert.False(t, amr.FactorPossession())
-	assert.False(t, amr.MultiFactorAuthentication())
-	assert.True(t, amr.ChannelBrowser())
-	assert.False(t, amr.ChannelService())
-	assert.False(t, amr.MultiChannelAuthentication())
-	assert.Equal(t, []string{"pwd"}, amr.MarshalRFC8176())
+	for _, tc := range testCases {
+		t.Run(tc.desc, func(t *testing.T) {
+			assert.Equal(t, tc.want.FactorKnowledge, tc.is.FactorKnowledge())
+			assert.Equal(t, tc.want.FactorPossession, tc.is.FactorPossession())
+			assert.Equal(t, tc.want.MultiFactorAuthentication, tc.is.MultiFactorAuthentication())
+			assert.Equal(t, tc.want.ChannelBrowser, tc.is.ChannelBrowser())
+			assert.Equal(t, tc.want.ChannelService, tc.is.ChannelService())
+			assert.Equal(t, tc.want.MultiChannelAuthentication, tc.is.MultiChannelAuthentication())
 
-	amr = AuthenticationMethodsReferences{
-		TOTP: true,
+			isRFC8176 := tc.is.MarshalRFC8176()
+
+			for _, amr := range tc.want.RFC8176 {
+				t.Run(fmt.Sprintf("has all wanted/%s", amr), func(t *testing.T) {
+					assert.Contains(t, isRFC8176, amr)
+				})
+			}
+
+			for _, amr := range isRFC8176 {
+				t.Run(fmt.Sprintf("only has wanted/%s", amr), func(t *testing.T) {
+					assert.Contains(t, tc.want.RFC8176, amr)
+				})
+			}
+		})
 	}
-
-	assert.False(t, amr.FactorKnowledge())
-	assert.True(t, amr.FactorPossession())
-	assert.False(t, amr.MultiFactorAuthentication())
-	assert.True(t, amr.ChannelBrowser())
-	assert.False(t, amr.ChannelService())
-	assert.False(t, amr.MultiChannelAuthentication())
-	assert.Equal(t, []string{"otp"}, amr.MarshalRFC8176())
-
-	amr = AuthenticationMethodsReferences{
-		Webauthn: true,
-	}
-
-	assert.False(t, amr.FactorKnowledge())
-	assert.True(t, amr.FactorPossession())
-	assert.False(t, amr.MultiFactorAuthentication())
-	assert.True(t, amr.ChannelBrowser())
-	assert.False(t, amr.ChannelService())
-	assert.False(t, amr.MultiChannelAuthentication())
-	assert.Equal(t, []string{"hwk"}, amr.MarshalRFC8176())
-
-	amr = AuthenticationMethodsReferences{
-		WebauthnUserPresence: true,
-	}
-
-	assert.False(t, amr.FactorKnowledge())
-	assert.False(t, amr.FactorPossession())
-	assert.False(t, amr.MultiFactorAuthentication())
-	assert.False(t, amr.ChannelBrowser())
-	assert.False(t, amr.ChannelService())
-	assert.False(t, amr.MultiChannelAuthentication())
-	assert.Equal(t, []string{"user"}, amr.MarshalRFC8176())
-
-	amr = AuthenticationMethodsReferences{
-		WebauthnUserVerified: true,
-	}
-
-	assert.False(t, amr.FactorKnowledge())
-	assert.False(t, amr.FactorPossession())
-	assert.False(t, amr.MultiFactorAuthentication())
-	assert.False(t, amr.ChannelBrowser())
-	assert.False(t, amr.ChannelService())
-	assert.False(t, amr.MultiChannelAuthentication())
-	assert.Equal(t, []string{"pin"}, amr.MarshalRFC8176())
-
-	amr = AuthenticationMethodsReferences{
-		Webauthn:             true,
-		WebauthnUserPresence: true,
-		WebauthnUserVerified: true,
-	}
-
-	assert.False(t, amr.FactorKnowledge())
-	assert.True(t, amr.FactorPossession())
-	assert.False(t, amr.MultiFactorAuthentication())
-	assert.True(t, amr.ChannelBrowser())
-	assert.False(t, amr.ChannelService())
-	assert.False(t, amr.MultiChannelAuthentication())
-	assert.Equal(t, []string{"hwk", "user", "pin"}, amr.MarshalRFC8176())
-
-	amr = AuthenticationMethodsReferences{
-		Duo: true,
-	}
-
-	assert.False(t, amr.FactorKnowledge())
-	assert.True(t, amr.FactorPossession())
-	assert.False(t, amr.MultiFactorAuthentication())
-	assert.False(t, amr.ChannelBrowser())
-	assert.True(t, amr.ChannelService())
-	assert.False(t, amr.MultiChannelAuthentication())
-	assert.Equal(t, []string{"sms"}, amr.MarshalRFC8176())
-
-	amr = AuthenticationMethodsReferences{
-		Duo:      true,
-		Webauthn: true,
-		TOTP:     true,
-	}
-
-	assert.False(t, amr.FactorKnowledge())
-	assert.True(t, amr.FactorPossession())
-	assert.False(t, amr.MultiFactorAuthentication())
-	assert.True(t, amr.ChannelBrowser())
-	assert.True(t, amr.ChannelService())
-	assert.True(t, amr.MultiChannelAuthentication())
-	assert.Equal(t, []string{"otp", "sms", "hwk", "mca"}, amr.MarshalRFC8176())
-
-	amr = AuthenticationMethodsReferences{
-		Duo:  true,
-		TOTP: true,
-	}
-
-	assert.False(t, amr.FactorKnowledge())
-	assert.True(t, amr.FactorPossession())
-	assert.False(t, amr.MultiFactorAuthentication())
-	assert.True(t, amr.ChannelBrowser())
-	assert.True(t, amr.ChannelService())
-	assert.True(t, amr.MultiChannelAuthentication())
-	assert.Equal(t, []string{"otp", "sms", "mca"}, amr.MarshalRFC8176())
-
-	amr = AuthenticationMethodsReferences{
-		Duo:                 true,
-		UsernameAndPassword: true,
-	}
-
-	assert.True(t, amr.FactorKnowledge())
-	assert.True(t, amr.FactorPossession())
-	assert.True(t, amr.MultiFactorAuthentication())
-	assert.True(t, amr.ChannelBrowser())
-	assert.True(t, amr.ChannelService())
-	assert.True(t, amr.MultiChannelAuthentication())
-	assert.Equal(t, []string{"pwd", "sms", "mfa", "mca"}, amr.MarshalRFC8176())
 }
