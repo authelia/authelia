c7a8438183e004d3176d0f6a3cec9ba5be97f122
James Elliott <james-d-elliott@users.noreply.github.com>
2022-03-28 10:18:30 +1100

    test: use subtests

--- internal/model/user_info_test.go
+++ internal/model/user_info_test.go
@@ -1,131 +1,220 @@
 package model
 
 import (
+	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestUserInfo_SetDefaultMethod_ShouldConfigureConfigDefault(t *testing.T) {
-	var (
-		info    UserInfo
-		changed bool
-	)
+	testName := func(i int, have UserInfo, availableMethods []string) string {
+		method := have.Method
 
-	info = UserInfo{
-		Method:      SecondFactorMethodTOTP,
-		HasDuo:      true,
-		HasTOTP:     true,
-		HasWebauthn: true,
+		if method == "" {
+			method = "none"
+		}
+
+		has := ""
+
+		if have.HasTOTP || have.HasDuo || have.HasWebauthn {
+			has += " has"
+
+			if have.HasTOTP {
+				has += " " + SecondFactorMethodTOTP
+			}
+
+			if have.HasDuo {
+				has += " " + SecondFactorMethodDuo
+			}
+
+			if have.HasWebauthn {
+				has += " " + SecondFactorMethodWebauthn
+			}
+		}
+
+		available := "none"
+		if len(availableMethods) != 0 {
+			available = strings.Join(availableMethods, " ")
+		}
+
+		return fmt.Sprintf("%d/method %s%s/available methods %s", i+1, method, has, available)
 	}
 
-	changed = info.SetDefaultPreferred2FAMethod([]string{SecondFactorMethodWebauthn, SecondFactorMethodDuo})
-
-	assert.True(t, changed)
-	assert.Equal(t, SecondFactorMethodWebauthn, info.Method)
-
-	info = UserInfo{
-		Method:      "",
-		HasDuo:      true,
-		HasTOTP:     true,
-		HasWebauthn: true,
+	testCases := []struct {
+		have             UserInfo
+		availableMethods []string
+		changed          bool
+		want             UserInfo
+	}{
+		{
+			have: UserInfo{
+				Method:      SecondFactorMethodTOTP,
+				HasDuo:      true,
+				HasTOTP:     true,
+				HasWebauthn: true,
+			},
+			availableMethods: []string{SecondFactorMethodWebauthn, SecondFactorMethodDuo},
+			changed:          true,
+			want: UserInfo{
+				Method:      SecondFactorMethodWebauthn,
+				HasDuo:      true,
+				HasTOTP:     true,
+				HasWebauthn: true,
+			},
+		},
+		{
+			have: UserInfo{
+				HasDuo:      true,
+				HasTOTP:     true,
+				HasWebauthn: true,
+			},
+			availableMethods: []string{SecondFactorMethodTOTP, SecondFactorMethodWebauthn, SecondFactorMethodDuo},
+			changed:          true,
+			want: UserInfo{
+				Method:      SecondFactorMethodTOTP,
+				HasDuo:      true,
+				HasTOTP:     true,
+				HasWebauthn: true,
+			},
+		},
+		{
+			have: UserInfo{
+				Method:      SecondFactorMethodWebauthn,
+				HasDuo:      true,
+				HasTOTP:     false,
+				HasWebauthn: false,
+			},
+			availableMethods: []string{SecondFactorMethodTOTP},
+			changed:          true,
+			want: UserInfo{
+				Method:      SecondFactorMethodTOTP,
+				HasDuo:      true,
+				HasTOTP:     false,
+				HasWebauthn: false,
+			},
+		},
+		{
+			have: UserInfo{
+				Method:      SecondFactorMethodWebauthn,
+				HasDuo:      false,
+				HasTOTP:     false,
+				HasWebauthn: false,
+			},
+			availableMethods: []string{SecondFactorMethodTOTP},
+			changed:          true,
+			want: UserInfo{
+				Method:      SecondFactorMethodTOTP,
+				HasDuo:      false,
+				HasTOTP:     false,
+				HasWebauthn: false,
+			},
+		},
+		{
+			have: UserInfo{
+				Method:      SecondFactorMethodTOTP,
+				HasDuo:      false,
+				HasTOTP:     false,
+				HasWebauthn: false,
+			},
+			availableMethods: []string{SecondFactorMethodWebauthn},
+			changed:          true,
+			want: UserInfo{
+				Method:      SecondFactorMethodWebauthn,
+				HasDuo:      false,
+				HasTOTP:     false,
+				HasWebauthn: false,
+			},
+		},
+		{
+			have: UserInfo{
+				Method:      SecondFactorMethodTOTP,
+				HasDuo:      false,
+				HasTOTP:     false,
+				HasWebauthn: false,
+			},
+			availableMethods: []string{SecondFactorMethodDuo},
+			changed:          true,
+			want: UserInfo{
+				Method:      SecondFactorMethodDuo,
+				HasDuo:      false,
+				HasTOTP:     false,
+				HasWebauthn: false,
+			},
+		},
+		{
+			have: UserInfo{
+				Method:      SecondFactorMethodWebauthn,
+				HasDuo:      false,
+				HasTOTP:     true,
+				HasWebauthn: true,
+			},
+			availableMethods: []string{SecondFactorMethodTOTP, SecondFactorMethodWebauthn, SecondFactorMethodDuo},
+			changed:          false,
+			want: UserInfo{
+				Method:      SecondFactorMethodWebauthn,
+				HasDuo:      false,
+				HasTOTP:     true,
+				HasWebauthn: true,
+			},
+		},
+		{
+			have: UserInfo{
+				Method:      "",
+				HasDuo:      false,
+				HasTOTP:     true,
+				HasWebauthn: true,
+			},
+			availableMethods: []string{SecondFactorMethodWebauthn, SecondFactorMethodDuo},
+			changed:          true,
+			want: UserInfo{
+				Method:      SecondFactorMethodWebauthn,
+				HasDuo:      false,
+				HasTOTP:     true,
+				HasWebauthn: true,
+			},
+		},
+		{
+			have: UserInfo{
+				Method:      "",
+				HasDuo:      false,
+				HasTOTP:     true,
+				HasWebauthn: true,
+			},
+			availableMethods: []string{SecondFactorMethodDuo},
+			changed:          true,
+			want: UserInfo{
+				Method:      SecondFactorMethodDuo,
+				HasDuo:      false,
+				HasTOTP:     true,
+				HasWebauthn: true,
+			},
+		},
+		{
+			have: UserInfo{
+				Method:      "",
+				HasDuo:      false,
+				HasTOTP:     true,
+				HasWebauthn: true,
+			},
+			availableMethods: nil,
+			changed:          false,
+			want: UserInfo{
+				Method:      "",
+				HasDuo:      false,
+				HasTOTP:     true,
+				HasWebauthn: true,
+			},
+		},
 	}
 
-	changed = info.SetDefaultPreferred2FAMethod([]string{SecondFactorMethodTOTP, SecondFactorMethodWebauthn, SecondFactorMethodDuo})
+	for i, tc := range testCases {
+		t.Run(testName(i, tc.have, tc.availableMethods), func(t *testing.T) {
+			changed := tc.have.SetDefaultPreferred2FAMethod(tc.availableMethods)
 
-	assert.True(t, changed)
-	assert.Equal(t, SecondFactorMethodTOTP, info.Method)
-
-	info = UserInfo{
-		Method:      "webauthn",
-		HasDuo:      true,
-		HasTOTP:     false,
-		HasWebauthn: false,
+			assert.Equal(t, tc.changed, changed)
+			assert.Equal(t, tc.want, tc.have)
+		})
 	}
-
-	changed = info.SetDefaultPreferred2FAMethod([]string{SecondFactorMethodTOTP, SecondFactorMethodDuo})
-
-	assert.True(t, changed)
-	assert.Equal(t, SecondFactorMethodDuo, info.Method)
-
-	info = UserInfo{
-		Method:      "webauthn",
-		HasDuo:      false,
-		HasTOTP:     false,
-		HasWebauthn: false,
-	}
-
-	changed = info.SetDefaultPreferred2FAMethod([]string{SecondFactorMethodTOTP})
-
-	assert.True(t, changed)
-	assert.Equal(t, SecondFactorMethodTOTP, info.Method)
-
-	info = UserInfo{
-		Method:      "totp",
-		HasDuo:      false,
-		HasTOTP:     false,
-		HasWebauthn: false,
-	}
-
-	changed = info.SetDefaultPreferred2FAMethod([]string{SecondFactorMethodWebauthn})
-
-	assert.True(t, changed)
-	assert.Equal(t, SecondFactorMethodWebauthn, info.Method)
-
-	info = UserInfo{
-		Method:      "totp",
-		HasDuo:      false,
-		HasTOTP:     false,
-		HasWebauthn: false,
-	}
-
-	changed = info.SetDefaultPreferred2FAMethod([]string{SecondFactorMethodDuo})
-
-	assert.True(t, changed)
-	assert.Equal(t, SecondFactorMethodDuo, info.Method)
-
-	info = UserInfo{
-		Method:      "",
-		HasDuo:      false,
-		HasTOTP:     false,
-		HasWebauthn: false,
-	}
-
-	changed = info.SetDefaultPreferred2FAMethod([]string{SecondFactorMethodTOTP, SecondFactorMethodWebauthn, SecondFactorMethodDuo})
-
-	assert.True(t, changed)
-	assert.Equal(t, SecondFactorMethodTOTP, info.Method)
-
-	info = UserInfo{
-		Method:      "webauthn",
-		HasDuo:      false,
-		HasTOTP:     true,
-		HasWebauthn: true,
-	}
-
-	changed = info.SetDefaultPreferred2FAMethod([]string{SecondFactorMethodTOTP})
-
-	assert.True(t, changed)
-	assert.Equal(t, SecondFactorMethodTOTP, info.Method)
-
-	info.Method = ""
-
-	changed = info.SetDefaultPreferred2FAMethod([]string{SecondFactorMethodWebauthn, SecondFactorMethodDuo})
-
-	assert.True(t, changed)
-	assert.Equal(t, SecondFactorMethodWebauthn, info.Method)
-
-	info.Method = ""
-
-	changed = info.SetDefaultPreferred2FAMethod([]string{SecondFactorMethodDuo})
-
-	assert.True(t, changed)
-	assert.Equal(t, SecondFactorMethodDuo, info.Method)
-
-	info.Method = ""
-
-	changed = info.SetDefaultPreferred2FAMethod(nil)
-
-	assert.False(t, changed)
-	assert.Equal(t, "", info.Method)
 }
