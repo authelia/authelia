48350f09355c10b828c398aab3d5f60ba2828533
James Elliott <james-d-elliott@users.noreply.github.com>
2022-04-20 17:52:17 +1000

    refactor: use v4 theme colors

--- web/src/themes/Dark.ts
+++ web/src/themes/Dark.ts
@@ -8,8 +8,47 @@ const Dark = createTheme({
     palette: {
         mode: "dark",
         primary: {
+            light: "#a6d4fa",
             main: "#1976d2",
+            dark: "#648dae",
         },
+        secondary: {
+            light: "#f6a5c0",
+            main: "#f48fb1",
+            dark: "#aa647b",
+        },
+        error: {
+            light: "#e57373",
+            main: "#f44336",
+            dark: "#d32f2f",
+        },
+        warning: {
+            light: "#ffb74d",
+            main: "#ff9800",
+            dark: "#f57c00",
+        },
+        info: {
+            light: "#81c784",
+            main: "#4caf50",
+            dark: "#388e3c",
+        },
+        text: {
+            primary: "#fff",
+            secondary: "rgba(255, 255, 255, 0.7)",
+            disabled: "rgba(255, 255, 255, 0.5)",
+        },
+        action: {
+            active: "#fff",
+            hover: "rgba(255, 255, 255, 0.08)",
+            selected: "rgba(255, 255, 255, 0.16)",
+            disabled: "rgba(255, 255, 255, 0.3)",
+            disabledBackground: "rgba(255, 255, 255, 0.12)",
+        },
+        background: {
+            default: "#303030",
+            paper: "#424242",
+        },
+        divider: "rgba(255, 255, 255, 0.12)",
     },
 });
 
--- web/src/themes/Light.ts
+++ web/src/themes/Light.ts
@@ -6,13 +6,49 @@ const Light = createTheme({
         loadingBar: "#000",
     },
     palette: {
+        mode: "dark",
         primary: {
+            light: "#a6d4fa",
             main: "#1976d2",
+            dark: "#648dae",
+        },
+        secondary: {
+            light: "#f6a5c0",
+            main: "#f48fb1",
+            dark: "#aa647b",
+        },
+        error: {
+            light: "#e57373",
+            main: "#f44336",
+            dark: "#d32f2f",
+        },
+        warning: {
+            light: "#ffb74d",
+            main: "#ff9800",
+            dark: "#f57c00",
+        },
+        info: {
+            light: "#81c784",
+            main: "#4caf50",
+            dark: "#388e3c",
+        },
+        text: {
+            primary: "rgba(0, 0, 0, 0.87)",
+            secondary: "rgba(0, 0, 0, 0.54)",
+            disabled: "rgba(0, 0, 0, 0.38)",
+        },
+        action: {
+            active: "rgba(0, 0, 0, 0.54)",
+            hover: "rgba(0, 0, 0, 0.04)",
+            selected: "rgba(0, 0, 0, 0.08)",
+            disabled: "rgba(0, 0, 0, 0.26)",
+            disabledBackground: "rgba(0, 0, 0, 0.12)",
         },
         background: {
-            default: "#fff",
+            default: "#fafafa",
             paper: "#fff",
         },
+        divider: "rgba(0, 0, 0, 0.12)",
     },
 });
 
