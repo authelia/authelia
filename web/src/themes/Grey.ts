986bed9615737b3a49dccf887d3234bac5346a2b
James Elliott <james-d-elliott@users.noreply.github.com>
2022-04-20 22:31:00 +1000

    refactor: update grey theme

--- web/src/themes/Grey.ts
+++ web/src/themes/Grey.ts
@@ -1,22 +1,58 @@
-import { createTheme, adaptV4Theme } from "@mui/material/styles";
+import { createTheme } from "@mui/material/styles";
 
-const Grey = createTheme(
-    adaptV4Theme({
-        custom: {
-            icon: "#929aa5",
-            loadingBar: "#929aa5",
+const Grey = createTheme({
+    custom: {
+        icon: "#929aa5",
+        loadingBar: "#929aa5",
+    },
+    palette: {
+        mode: "dark",
+        primary: {
+            light: "#a6d4fa",
+            main: "#929aa5",
+            dark: "#648dae",
         },
-        palette: {
-            primary: {
-                main: "#929aa5",
-            },
-            background: {
-                default: "#2f343e",
-                paper: "#2f343e",
-            },
+        secondary: {
+            light: "#f6a5c0",
+            main: "#f48fb1",
+            dark: "#aa647b",
         },
-        overrides: {
-            MuiCssBaseline: {
+        error: {
+            light: "#e57373",
+            main: "#f44336",
+            dark: "#d32f2f",
+        },
+        warning: {
+            light: "#ffb74d",
+            main: "#ff9800",
+            dark: "#f57c00",
+        },
+        info: {
+            light: "#81c784",
+            main: "#4caf50",
+            dark: "#388e3c",
+        },
+        text: {
+            primary: "rgba(0, 0, 0, 0.87)",
+            secondary: "rgba(0, 0, 0, 0.54)",
+            disabled: "rgba(0, 0, 0, 0.38)",
+        },
+        action: {
+            active: "rgba(0, 0, 0, 0.54)",
+            hover: "rgba(0, 0, 0, 0.04)",
+            selected: "rgba(0, 0, 0, 0.08)",
+            disabled: "rgba(0, 0, 0, 0.26)",
+            disabledBackground: "rgba(0, 0, 0, 0.12)",
+        },
+        background: {
+            default: "#2f343e",
+            paper: "#2f343e",
+        },
+        divider: "rgba(0, 0, 0, 0.12)",
+    },
+    components: {
+        MuiCssBaseline: {
+            styleOverrides: {
                 "@global": {
                     body: {
                         backgroundColor: "#2f343e",
@@ -24,7 +60,9 @@ const Grey = createTheme(
                     },
                 },
             },
-            MuiOutlinedInput: {
+        },
+        MuiOutlinedInput: {
+            styleOverrides: {
                 root: {
                     "& $notchedOutline": {
                         borderColor: "#929aa5",
@@ -39,23 +77,29 @@ const Grey = createTheme(
                 },
                 notchedOutline: {},
             },
-            MuiCheckbox: {
-                root: {
-                    color: "#929aa5",
-                },
-            },
-            MuiInputBase: {
-                input: {
-                    color: "#929aa5",
-                },
-            },
-            MuiInputLabel: {
+        },
+        MuiCheckbox: {
+            styleOverrides: {
                 root: {
                     color: "#929aa5",
                 },
             },
         },
-    }),
-);
+        MuiInputBase: {
+            styleOverrides: {
+                input: {
+                    color: "#929aa5",
+                },
+            },
+        },
+        MuiInputLabel: {
+            styleOverrides: {
+                root: {
+                    color: "#929aa5",
+                },
+            },
+        },
+    },
+});
 
 export default Grey;
