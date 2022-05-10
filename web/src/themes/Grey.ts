8c15aa9c4fe2b5e16ed58e847cd3442ad5911b8c
James Elliott <james-d-elliott@users.noreply.github.com>
2022-05-10 14:36:26 +1000

    fix: grey colors

--- web/src/themes/Grey.ts
+++ web/src/themes/Grey.ts
@@ -33,14 +33,14 @@ const Grey = createTheme({
             dark: "#388e3c",
         },
         text: {
-            primary: "rgba(0, 0, 0, 0.87)",
+            primary: "#929aa5",
             secondary: "rgba(0, 0, 0, 0.54)",
             disabled: "rgba(0, 0, 0, 0.38)",
         },
         action: {
-            active: "rgba(0, 0, 0, 0.54)",
-            hover: "rgba(0, 0, 0, 0.04)",
-            selected: "rgba(0, 0, 0, 0.08)",
+            active: "#929aa5",
+            hover: "#929aa5",
+            selected: "#929aa5",
             disabled: "rgba(0, 0, 0, 0.26)",
             disabledBackground: "rgba(0, 0, 0, 0.12)",
         },
