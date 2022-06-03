bbeab861b742b2b3e86493862a4f7622853b8c2d
Amir Zarrinkafsh <nightah@me.com>
2022-06-03 10:17:00 +1000

    test(web): fix tests to conform to react18 (#3466)
    This change drops the use of ReactDOM which is not supported in React 18. We also fix any test warnings related to i18next.

--- web/src/App.test.tsx
+++ web/src/App.test.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { render } from "@testing-library/react";
 
 import App from "@root/App";
+import "@i18n/index.ts";
 
 it("renders without crashing", () => {
     render(<App />);
