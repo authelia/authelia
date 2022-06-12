a76a1a5e78b2deb0b29ec7b19e533301bce2a0a3
James Elliott <james-d-elliott@users.noreply.github.com>
2022-06-12 19:41:50 +1000

    refactor: adjust authelia-suites examples

--- cmd/authelia-scripts/cmd/const.go
+++ cmd/authelia-scripts/cmd/const.go
@@ -26,61 +26,61 @@ testing suites and various other aspects of the environment.
 
 It can be used to automate or manually run unit testing, integration testing, etc.`
 
-	cmdRootExample = `  authelia-scripts help`
+	cmdRootExample = `authelia-scripts help`
 
 	cmdBootstrapShort = "Prepare environment for development and testing"
 
 	cmdBootstrapLong = `Prepare environment for development and testing.`
 
-	cmdBootstrapExample = `  authelia-scripts bootstrap`
+	cmdBootstrapExample = `authelia-scripts bootstrap`
 
 	cmdBuildShort = "Build Authelia binary and static assets"
 
 	cmdBuildLong = `Build Authelia binary and static assets.`
 
-	cmdBuildExample = `  authelia-scripts build`
+	cmdBuildExample = `authelia-scripts build`
 
 	cmdCleanShort = "Clean build artifacts"
 
 	cmdCleanLong = `Clean build artifacts.`
 
-	cmdCleanExample = `  authelia-scripts clean`
+	cmdCleanExample = `authelia-scripts clean`
 
 	cmdCIShort = "Run the continuous integration script"
 
 	cmdCILong = `Run the continuous integration script.`
 
-	cmdCIExample = `  authelia-scripts ci`
+	cmdCIExample = `authelia-scripts ci`
 
 	cmdDockerShort = "Commands related to building and publishing docker image"
 
 	cmdDockerLong = `Commands related to building and publishing docker image.`
 
-	cmdDockerExample = `  authelia-scripts docker`
+	cmdDockerExample = `authelia-scripts docker`
 
 	cmdDockerBuildShort = "Build the docker image of Authelia"
 
 	cmdDockerBuildLong = `Build the docker image of Authelia.`
 
-	cmdDockerBuildExample = `  authelia-scripts docker build`
+	cmdDockerBuildExample = `authelia-scripts docker build`
 
 	cmdDockerPushManifestShort = "Push Authelia docker manifest to the Docker registries"
 
 	cmdDockerPushManifestLong = `Push Authelia docker manifest to the Docker registries.`
 
-	cmdDockerPushManifestExample = `  authelia-scripts docker push-manifest`
+	cmdDockerPushManifestExample = `authelia-scripts docker push-manifest`
 
 	cmdServeShort = "Serve compiled version of Authelia"
 
 	cmdServeLong = `Serve compiled version of Authelia.`
 
-	cmdServeExample = `  authelia-scripts serve test.yml`
+	cmdServeExample = `authelia-scripts serve test.yml`
 
 	cmdSuitesShort = "Commands related to suites management"
 
 	cmdSuitesLong = `Commands related to suites management.`
 
-	cmdSuitesExample = `  authelia-scripts suites`
+	cmdSuitesExample = `authelia-scripts suites`
 
 	cmdSuitesListShort = "List available suites"
 
@@ -88,7 +88,7 @@ It can be used to automate or manually run unit testing, integration testing, et
 
 Suites can be ran with the authelia-scripts suites test [suite] command.`
 
-	cmdSuitesListExample = `  authelia-scripts suites list`
+	cmdSuitesListExample = `authelia-scripts suites list`
 
 	cmdSuitesTestShort = "Run a test suite"
 
@@ -96,7 +96,7 @@ Suites can be ran with the authelia-scripts suites test [suite] command.`
 
 Suites can be listed with the authelia-scripts suites list command.`
 
-	cmdSuitesTestExample = `  authelia-scripts suites test Standalone`
+	cmdSuitesTestExample = `authelia-scripts suites test Standalone`
 
 	cmdSuitesSetupShort = "Setup a test suite environment"
 
@@ -104,7 +104,7 @@ Suites can be listed with the authelia-scripts suites list command.`
 
 Suites can be listed with the authelia-scripts suites list command.`
 
-	cmdSuitesSetupExample = `  authelia-scripts suites setup Standalone`
+	cmdSuitesSetupExample = `authelia-scripts suites setup Standalone`
 
 	cmdSuitesTeardownShort = "Teardown a test suite environment"
 
@@ -112,17 +112,17 @@ Suites can be listed with the authelia-scripts suites list command.`
 
 Suites can be listed with the authelia-scripts suites list command.`
 
-	cmdSuitesTeardownExample = `  authelia-scripts suites setup Standalone`
+	cmdSuitesTeardownExample = `authelia-scripts suites setup Standalone`
 
 	cmdUnitTestShort = "Run unit tests"
 
 	cmdUnitTestLong = `Run unit tests.`
 
-	cmdUnitTestExample = `  authelia-scripts unittest`
+	cmdUnitTestExample = `authelia-scripts unittest`
 
 	cmdXFlagsShort = "Generate X LDFlags for building Authelia"
 
 	cmdXFlagsLong = `Generate X LDFlags for building Authelia.`
 
-	cmdXFlagsExample = `  authelia-scripts xflags`
+	cmdXFlagsExample = `authelia-scripts xflags`
 )
