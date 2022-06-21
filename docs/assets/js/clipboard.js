dc1e3c432968e7ab457d0d727adabc7cc72117c5
James Elliott <james-d-elliott@users.noreply.github.com>
2022-06-22 07:55:41 +1000

    docs: comment logging

--- docs/assets/js/clipboard.js
+++ docs/assets/js/clipboard.js
@@ -21,16 +21,16 @@ var clipboard = new Clipboard('.btn-copy', {
 });
 
 clipboard.on('success', function(e) {
-  console.info('Clipboard');
+  /*
   console.info('Action:', e.action);
   console.info('Text:', e.text);
   console.info('Trigger:', e.trigger);
+   */
 
   e.clearSelection();
 });
 
 clipboard.on('error', function(e) {
-  console.error('Clipboard');
   console.error('Action:', e.action);
   console.error('Trigger:', e.trigger);
 });
