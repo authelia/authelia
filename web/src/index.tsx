841e495dca23fd4f8e9eb04e8e0c6fcabc39dc4e
James Elliott <james-d-elliott@users.noreply.github.com>
2022-06-21 10:35:30 +1000

    build(deps): update mui monorepo to v5.8.4 (major) (#3215)

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -7,8 +7,9 @@ import App from "@root/App";
 import * as serviceWorker from "@root/serviceWorker";
 import "@i18n/index.ts";
 
+const nonce = document.head.querySelector("[property=csp-nonce][content]")?.getAttribute("content") || undefined;
 const root = createRoot(document.getElementById("root")!);
-root.render(<App />);
+root.render(<App nonce={nonce} />);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
