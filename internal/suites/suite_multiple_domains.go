fa2b48d0b394731bfbfd969aa07155d4fa643cda
10672208+mind-ar <10672208+mind-ar@users.noreply.github.com>
2022-06-25 21:18:26 +0000

    suite: configuring suite

---
+++ internal/suites/suite_multiple_domains.go
@@ -0,0 +1,70 @@
+package suites
+
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
+var multipleDomainSuiteName = "MultipleDomain"
+
+func init() {
+	_ = os.MkdirAll("/tmp/authelia/MultipleDomainSuite/", 0700)
+	_ = os.WriteFile("/tmp/authelia/MultipleDomainSuite/jwt", []byte("very_important_secret"), 0600)       //nolint:gosec
+	_ = os.WriteFile("/tmp/authelia/MultipleDomainSuite/session", []byte("unsecure_session_secret"), 0600) //nolint:gosec
+
+	dockerEnvironment := NewDockerEnvironment([]string{
+		"internal/suites/docker-compose.yml",
+		"internal/suites/MultipleDomain/docker-compose.yml",
+		"internal/suites/example/compose/authelia/docker-compose.backend.{}.yml",
+		"internal/suites/example/compose/authelia/docker-compose.frontend.{}.yml",
+		"internal/suites/example/compose/nginx/backend/docker-compose.yml",
+		"internal/suites/example/compose/nginx/portal/docker-compose.yml",
+		"internal/suites/example/compose/smtp/docker-compose.yml",
+	})
+
+	setup := func(suitePath string) error {
+		if err := dockerEnvironment.Up(); err != nil {
+			return err
+		}
+
+		return waitUntilAutheliaIsReady(dockerEnvironment, multipleDomainSuiteName)
+	}
+
+	displayAutheliaLogs := func() error {
+		backendLogs, err := dockerEnvironment.Logs("authelia-backend", nil)
+		if err != nil {
+			return err
+		}
+
+		fmt.Println(backendLogs)
+
+		frontendLogs, err := dockerEnvironment.Logs("authelia-frontend", nil)
+		if err != nil {
+			return err
+		}
+
+		fmt.Println(frontendLogs)
+
+		return nil
+	}
+
+	teardown := func(suitePath string) error {
+		err := dockerEnvironment.Down()
+		_ = os.Remove("/tmp/db.sqlite3")
+
+		return err
+	}
+
+	GlobalRegistry.Register(multipleDomainSuiteName, Suite{
+		SetUp:           setup,
+		SetUpTimeout:    5 * time.Minute,
+		OnError:         displayAutheliaLogs,
+		OnSetupTimeout:  displayAutheliaLogs,
+		TearDown:        teardown,
+		TestTimeout:     3 * time.Minute,
+		TearDownTimeout: 2 * time.Minute,
+		Description: `This suite is used to test Authelia in a MultipleDomain
+configuration with in-memory sessions and a local sqlite db stored on disk`,
+	})
+}
