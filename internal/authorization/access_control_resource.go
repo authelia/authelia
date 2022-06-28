ab1d0c51d31e423f3caf4da1e02f3cc863c2cbd9
James Elliott <james-d-elliott@users.noreply.github.com>
2022-06-28 12:51:05 +1000

    feat(authorization): acl resource regex named groups (#3597)
    This adds the named group functionality from domain_regex to the resource criteria.

--- internal/authorization/access_control_resource.go
+++ internal/authorization/access_control_resource.go
@@ -4,12 +4,32 @@ import (
 	"regexp"
 )
 
-// AccessControlResource represents an ACL resource.
+// NewAccessControlResource creates a AccessControlResource or AccessControlResourceGroup.
+func NewAccessControlResource(pattern regexp.Regexp) AccessControlResource {
+	var iuser, igroup = -1, -1
+
+	for i, group := range pattern.SubexpNames() {
+		switch group {
+		case subexpNameUser:
+			iuser = i
+		case subexpNameGroup:
+			igroup = i
+		}
+	}
+
+	if iuser != -1 || igroup != -1 {
+		return AccessControlResource{RegexpGroupStringSubjectMatcher{pattern, iuser, igroup}}
+	}
+
+	return AccessControlResource{RegexpStringSubjectMatcher{pattern}}
+}
+
+// AccessControlResource represents an ACL resource that matches without named groups.
 type AccessControlResource struct {
-	Pattern regexp.Regexp
+	Matcher StringSubjectMatcher
 }
 
 // IsMatch returns true if the ACL resource match the object path.
-func (acr AccessControlResource) IsMatch(object Object) (match bool) {
-	return acr.Pattern.MatchString(object.Path)
+func (acl AccessControlResource) IsMatch(subject Subject, object Object) (match bool) {
+	return acl.Matcher.IsMatch(object.Path, subject)
 }
---
+++ internal/authorization/regexp.go
@@ -0,0 +1,53 @@
+package authorization
+
+import (
+	"regexp"
+	"strings"
+
+	"github.com/authelia/authelia/v4/internal/utils"
+)
+
+// RegexpGroupStringSubjectMatcher matches the input string against the pattern taking into account Subexp groups.
+type RegexpGroupStringSubjectMatcher struct {
+	Pattern         regexp.Regexp
+	SubexpNameUser  int
+	SubexpNameGroup int
+}
+
+// IsMatch returns true if the underlying pattern matches the input given the subject.
+func (r RegexpGroupStringSubjectMatcher) IsMatch(input string, subject Subject) (match bool) {
+	matches := r.Pattern.FindAllStringSubmatch(input, -1)
+	if matches == nil {
+		return false
+	}
+
+	if r.SubexpNameUser != -1 && !strings.EqualFold(subject.Username, matches[0][r.SubexpNameUser]) {
+		return false
+	}
+
+	if r.SubexpNameGroup != -1 && !utils.IsStringInSliceFold(matches[0][r.SubexpNameGroup], subject.Groups) {
+		return false
+	}
+
+	return true
+}
+
+// String returns the pattern string.
+func (r RegexpGroupStringSubjectMatcher) String() string {
+	return r.Pattern.String()
+}
+
+// RegexpStringSubjectMatcher just matches the input string against the pattern.
+type RegexpStringSubjectMatcher struct {
+	Pattern regexp.Regexp
+}
+
+// IsMatch returns true if the underlying pattern matches the input.
+func (r RegexpStringSubjectMatcher) IsMatch(input string, _ Subject) (match bool) {
+	return r.Pattern.MatchString(input)
+}
+
+// String returns the pattern string.
+func (r RegexpStringSubjectMatcher) String() string {
+	return r.Pattern.String()
+}
