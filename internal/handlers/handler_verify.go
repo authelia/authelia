693be91934a71904220d1e1709a16ebf5b3208af
Manuel Nuñez <10672208+mind-ar@users.noreply.github.com>
2022-07-01 22:08:35 -0300

    Update internal/handlers/handler_verify.go
    Co-authored-by: Amir Zarrinkafsh <nightah@me.com>

--- internal/handlers/handler_verify.go
+++ internal/handlers/handler_verify.go
@@ -173,7 +173,6 @@ func verifySessionCookie(ctx *middlewares.AutheliaCtx, targetURL *url.URL, userS
 
 			ctx.Logger.Warnf("User %s has been inactive for too long", userSession.Username)
 
-			// Fixes #3587.
 			return "", "", nil, nil, authentication.NotAuthenticated, nil
 		}
 	}
