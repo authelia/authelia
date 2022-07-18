573d418cc4ba627b929f92dcd8b7f0cbdc6c4cfe
James Elliott <james-d-elliott@users.noreply.github.com>
2022-07-18 12:41:10 +1000

    fix(authorization): final slash in url matching not considered
    This fixes an issue with the URL matching machinery which ignores the final slash of a URL.

--- internal/authorization/authorizer_test.go
+++ internal/authorization/authorizer_test.go
@@ -669,11 +669,16 @@ func (s *AuthorizerSuite) TestShouldCheckResourceMatching() {
 			Policy:    twoFactor,
 			Resources: createSliceRegexRule(s.T(), []string{"^/a/longer/rule/.*$"}),
 		}).
+		WithRule(schema.ACLRule{
+			Domains:   []string{"resource.example.com"},
+			Policy:    twoFactor,
+			Resources: createSliceRegexRule(s.T(), []string{"^/an/exact/path/$"}),
+		}).
 		Build()
 
 	tester.CheckAuthorizations(s.T(), John, "https://resource.example.com/", "GET", Bypass)
 	tester.CheckAuthorizations(s.T(), John, "https://resource.example.com/bypass/abc", "GET", Bypass)
-	tester.CheckAuthorizations(s.T(), John, "https://resource.example.com/bypass/", "GET", Denied)
+	tester.CheckAuthorizations(s.T(), John, "https://resource.example.com/bypass/", "GET", Bypass)
 	tester.CheckAuthorizations(s.T(), John, "https://resource.example.com/one_factor/abc", "GET", OneFactor)
 	tester.CheckAuthorizations(s.T(), John, "https://resource.example.com/xyz/embedded/abc", "GET", Bypass)
 	tester.CheckAuthorizations(s.T(), John, "https://resource.example.com/a/longer/rule/abc", "GET", TwoFactor)
@@ -697,6 +702,7 @@ func (s *AuthorizerSuite) TestShouldCheckResourceMatching() {
 	tester.CheckAuthorizations(s.T(), John, "https://resource.example.com/bypass/%2E%2E%2fa/longer/rule/abc", "GET", TwoFactor)
 	tester.CheckAuthorizations(s.T(), John, "https://resource.example.com/bypass/%2E%2E%2F/a/longer/rule/abc", "GET", TwoFactor)
 	tester.CheckAuthorizations(s.T(), John, "https://resource.example.com/bypass/%2E%2E%2Fa/longer/rule/abc", "GET", TwoFactor)
+	tester.CheckAuthorizations(s.T(), John, "https://resource.example.com/bypass/%2E%2E%2Fan/exact/path/", "GET", TwoFactor)
 }
 
 // This test assures that rules without domains (not allowed by schema validator at this time) will pass validation correctly.
--- internal/utils/url.go
+++ internal/utils/url.go
@@ -3,15 +3,24 @@ package utils
 import (
 	"net/url"
 	"path"
+	"strings"
 )
 
 // URLPathFullClean returns a URL path with the query parameters appended (full path) with the path portion parsed
 // through path.Clean given a *url.URL.
-func URLPathFullClean(u *url.URL) (p string) {
-	switch len(u.RawQuery) {
-	case 0:
-		return path.Clean(u.Path)
-	default:
-		return path.Clean(u.Path) + "?" + u.RawQuery
+func URLPathFullClean(u *url.URL) string {
+	b := strings.Builder{}
+
+	b.WriteString(path.Clean(u.Path))
+
+	if len(u.Path) != 1 && u.Path[len(u.Path)-1] == '/' {
+		b.Write(slashForward)
 	}
+
+	if u.RawQuery != "" {
+		b.Write(questionMark)
+		b.WriteString(u.RawQuery)
+	}
+
+	return b.String()
 }
