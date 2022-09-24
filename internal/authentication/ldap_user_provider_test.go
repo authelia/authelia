04b8d9de1c1d84d30c81ffca71607176f8b5eb72
James Elliott <james-d-elliott@users.noreply.github.com>
2022-09-24 11:51:37 +1000

    feat(authentication): permit feature detection failures
    This adds a configuration option which permits the failure of feature detection (control type OIDs and extension OIDs).

--- internal/authentication/ldap_user_provider_test.go
+++ internal/authentication/ldap_user_provider_test.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/go-ldap/ldap/v3"
+	ldap "github.com/go-ldap/ldap/v3"
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -489,7 +489,7 @@ func TestShouldReturnCheckServerSearchError(t *testing.T) {
 	gomock.InOrder(dialURL, connBind, searchOIDs, connClose)
 
 	err := ldapClient.StartupCheck()
-	assert.EqualError(t, err, "could not perform the search")
+	assert.EqualError(t, err, "error occurred during RootDSE search: could not perform the search")
 
 	assert.False(t, ldapClient.features.Extensions.PwdModifyExOp)
 }
