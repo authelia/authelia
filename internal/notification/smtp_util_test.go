ff156c31038a3ab9ca32ab3427c4364d54fcacc1
James Elliott <james-d-elliott@users.noreply.github.com>
2022-09-27 10:18:43 +1000

    test: improve reliability

--- internal/notification/smtp_util_test.go
+++ internal/notification/smtp_util_test.go
@@ -10,8 +10,6 @@ import (
 )
 
 func TestNewMIMECharacteristics(t *testing.T) {
-	alphanumericPlus8Bit := utils.AlphaNumericCharacters + string([]byte{163})
-
 	testCases := []struct {
 		name     string
 		expected MIMECharacteristics
@@ -20,27 +18,42 @@ func TestNewMIMECharacteristics(t *testing.T) {
 		{
 			"ShouldDetectMessageCharacteristics7Bit",
 			MIMECharacteristics{},
-			createMIMEBytes(utils.AlphaNumericCharacters, true, 5, 150),
+			createMIMEBytes(false, true, 5, 150),
 		},
 		{
 			"ShouldDetectMessageCharacteristicsLongLine",
 			MIMECharacteristics{LongLines: true},
-			createMIMEBytes(utils.AlphaNumericCharacters, true, 3, 1200),
+			createMIMEBytes(false, true, 3, 1200),
 		},
 		{
-			"ShouldDetectMessageCharacteristicsCRLF",
+			"ShouldDetectMessageCharacteristicsLF",
 			MIMECharacteristics{LineFeeds: true},
-			createMIMEBytes(utils.AlphaNumericCharacters, false, 5, 150),
+			createMIMEBytes(false, false, 5, 150),
 		},
 		{
 			"ShouldDetectMessageCharacteristics8Bit",
 			MIMECharacteristics{Characters8BIT: true},
-			createMIMEBytes(alphanumericPlus8Bit, true, 3, 150),
+			createMIMEBytes(true, true, 3, 150),
+		},
+		{
+			"ShouldDetectMessageCharacteristicsLongLineAndLF",
+			MIMECharacteristics{true, true, false},
+			createMIMEBytes(false, false, 3, 1200),
+		},
+		{
+			"ShouldDetectMessageCharacteristicsLongLineAnd8Bit",
+			MIMECharacteristics{true, false, true},
+			createMIMEBytes(true, true, 3, 1200),
+		},
+		{
+			"ShouldDetectMessageCharacteristics8BitAndLF",
+			MIMECharacteristics{false, true, true},
+			createMIMEBytes(true, false, 3, 150),
 		},
 		{
 			"ShouldDetectMessageCharacteristicsAll",
 			MIMECharacteristics{true, true, true},
-			createMIMEBytes(alphanumericPlus8Bit, false, 3, 1200),
+			createMIMEBytes(true, false, 3, 1200),
 		},
 	}
 
@@ -53,16 +66,23 @@ func TestNewMIMECharacteristics(t *testing.T) {
 	}
 }
 
-func createMIMEBytes(charset string, crlf bool, lines, length int) []byte {
+func createMIMEBytes(include8bit, crlf bool, lines, length int) []byte {
 	buf := &bytes.Buffer{}
 
 	for i := 0; i < lines; i++ {
 		for j := 0; j < length/100; j++ {
-			buf.WriteString(utils.RandomString(100, charset, false))
+			switch {
+			case include8bit:
+				buf.Write(utils.RandomBytes(50, utils.AlphaNumericCharacters, false))
+				buf.Write([]byte{163})
+				buf.Write(utils.RandomBytes(49, utils.AlphaNumericCharacters, false))
+			default:
+				buf.Write(utils.RandomBytes(100, utils.AlphaNumericCharacters, false))
+			}
 		}
 
 		if n := length % 100; n != 0 {
-			buf.WriteString(utils.RandomString(n, charset, false))
+			buf.Write(utils.RandomBytes(n, utils.AlphaNumericCharacters, false))
 		}
 
 		switch {
