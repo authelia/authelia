f3e542348c0bb63252eaac700c309c8370e07b93
James Elliott <james-d-elliott@users.noreply.github.com>
2022-10-21 15:52:28 +1100

    refactor: remove dead code

--- internal/utils/crypto_test.go
+++ internal/utils/crypto_test.go
@@ -5,7 +5,6 @@ import (
 	"crypto/ed25519"
 	"crypto/elliptic"
 	"crypto/rsa"
-	"crypto/tls"
 	"crypto/x509"
 	"runtime"
 	"strings"
@@ -15,57 +14,6 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
-
-func TestShouldReturnCorrectTLSVersions(t *testing.T) {
-	tls13 := uint16(tls.VersionTLS13)
-	tls12 := uint16(tls.VersionTLS12)
-	tls11 := uint16(tls.VersionTLS11)
-	tls10 := uint16(tls.VersionTLS10)
-
-	version, err := TLSStringToTLSConfigVersion(TLS13)
-	assert.Equal(t, tls13, version)
-	assert.NoError(t, err)
-
-	version, err = TLSStringToTLSConfigVersion("TLS" + TLS13)
-	assert.Equal(t, tls13, version)
-	assert.NoError(t, err)
-
-	version, err = TLSStringToTLSConfigVersion(TLS12)
-	assert.Equal(t, tls12, version)
-	assert.NoError(t, err)
-
-	version, err = TLSStringToTLSConfigVersion("TLS" + TLS12)
-	assert.Equal(t, tls12, version)
-	assert.NoError(t, err)
-
-	version, err = TLSStringToTLSConfigVersion(TLS11)
-	assert.Equal(t, tls11, version)
-	assert.NoError(t, err)
-
-	version, err = TLSStringToTLSConfigVersion("TLS" + TLS11)
-	assert.Equal(t, tls11, version)
-	assert.NoError(t, err)
-
-	version, err = TLSStringToTLSConfigVersion(TLS10)
-	assert.Equal(t, tls10, version)
-	assert.NoError(t, err)
-
-	version, err = TLSStringToTLSConfigVersion("TLS" + TLS10)
-	assert.Equal(t, tls10, version)
-	assert.NoError(t, err)
-}
-
-func TestShouldReturnZeroAndErrorOnInvalidTLSVersions(t *testing.T) {
-	version, err := TLSStringToTLSConfigVersion("TLS1.4")
-	assert.Error(t, err)
-	assert.Equal(t, uint16(0), version)
-	assert.EqualError(t, err, "supplied tls version isn't supported")
-
-	version, err = TLSStringToTLSConfigVersion("SSL3.0")
-	assert.Error(t, err)
-	assert.Equal(t, uint16(0), version)
-	assert.EqualError(t, err, "supplied tls version isn't supported")
-}
 
 func TestShouldReturnErrWhenX509DirectoryNotExist(t *testing.T) {
 	pool, warnings, errors := NewX509CertPool("/tmp/asdfzyxabc123/not/a/real/dir")
