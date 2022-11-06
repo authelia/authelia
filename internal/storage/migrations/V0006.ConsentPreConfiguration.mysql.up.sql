bd16267a41c76c6dd180a69da6c855ef1622ba63
James Elliott <james-d-elliott@users.noreply.github.com>
2022-11-06 23:21:29 +1100

    fix: drop table order

--- internal/storage/migrations/V0006.ConsentPreConfiguration.mysql.up.sql
+++ internal/storage/migrations/V0006.ConsentPreConfiguration.mysql.up.sql
@@ -18,7 +18,12 @@ FROM oauth2_consent_session
 WHERE expires_at IS NOT NULL AND responded_at IS NOT NULL
 ORDER BY responded_at;
 
-DROP TABLE oauth2_consent_session;
+DROP TABLE IF EXISTS oauth2_access_token_session;
+DROP TABLE IF EXISTS oauth2_authorization_code_session;
+DROP TABLE IF EXISTS oauth2_openid_connect_session;
+DROP TABLE IF EXISTS oauth2_pkce_request_session;
+DROP TABLE IF EXISTS oauth2_refresh_token_session;
+DROP TABLE IF EXISTS oauth2_consent_session;
 
 CREATE TABLE oauth2_consent_session (
     id INTEGER AUTO_INCREMENT,
