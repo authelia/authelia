94d50191d9ddebe533d464393898413367bfc2d4
James Elliott <james-d-elliott@users.noreply.github.com>
2022-11-07 13:49:29 +1100

    fix: proc

--- internal/storage/migrations/V0007.ConsistencyFixes.mysql.up.sql
+++ internal/storage/migrations/V0007.ConsistencyFixes.mysql.up.sql
@@ -1,22 +1,20 @@
- DROP PROCEDURE IF EXISTS PROC_DROP_FOREIGN_KEY;
-    DELIMITER $$
-    CREATE PROCEDURE PROC_DROP_FOREIGN_KEY(IN tableName VARCHAR(64), IN constraintName VARCHAR(64))
-    BEGIN
-        IF EXISTS(
-            SELECT * FROM information_schema.table_constraints
-            WHERE
-                table_schema    = DATABASE()     AND
-                table_name      = tableName      AND
-                constraint_name = constraintName AND
-                constraint_type = 'FOREIGN KEY')
-        THEN
-            SET @query = CONCAT('ALTER TABLE ', tableName, ' DROP FOREIGN KEY ', constraintName, ';');
-            PREPARE stmt FROM @query;
-            EXECUTE stmt;
-            DEALLOCATE PREPARE stmt;
-        END IF;
-    END$$
-    DELIMITER ;
+DROP PROCEDURE IF EXISTS PROC_DROP_FOREIGN_KEY;
+CREATE PROCEDURE PROC_DROP_FOREIGN_KEY(IN tableName VARCHAR(64), IN constraintName VARCHAR(64))
+BEGIN
+	IF EXISTS(
+		SELECT * FROM information_schema.table_constraints
+		WHERE
+			table_schema    = DATABASE()     AND
+			table_name      = tableName      AND
+			constraint_name = constraintName AND
+			constraint_type = 'FOREIGN KEY')
+	THEN
+		SET @query = CONCAT('ALTER TABLE ', tableName, ' DROP FOREIGN KEY ', constraintName, ';');
+		PREPARE stmt FROM @query;
+		EXECUTE stmt;
+		DEALLOCATE PREPARE stmt;
+	END IF;
+END;
 
 DROP TABLE _bkp_UP_V0002_totp_configurations;
 DROP TABLE _bkp_UP_V0002_u2f_devices;
