8d75bf9ecb72956c5c1ac9b1c8f286cb5912db92
James Elliott <james-d-elliott@users.noreply.github.com>
2022-11-10 21:53:36 +1100

    fix: postgres indexes

--- internal/storage/migrations/V0007.ConsistencyFixes.postgres.up.sql
+++ internal/storage/migrations/V0007.ConsistencyFixes.postgres.up.sql
@@ -8,9 +8,42 @@ UPDATE webauthn_devices
 SET aaguid = NULL
 WHERE aaguid = '' OR aaguid = '00000000-00000000-00000000-00000000';
 
+ALTER TABLE duo_devices
+	DROP CONSTRAINT IF EXISTS duo_devices_username_key;
+
+DROP INDEX IF EXISTS duo_devices_username_key;
+
+CREATE UNIQUE INDEX duo_devices_username_key ON duo_devices (username);
+
+ALTER TABLE encryption
+    DROP CONSTRAINT IF EXISTS encryption_name_key;
+
+DROP INDEX IF EXISTS encryption_name_key;
+
+CREATE UNIQUE INDEX encryption_name_key ON encryption (name);
+
+ALTER TABLE identity_verification
+	DROP CONSTRAINT IF EXISTS identity_verification_jti_key;
+
+DROP INDEX IF EXISTS identity_verification_jti_key;
+
+CREATE UNIQUE INDEX identity_verification_jti_key ON identity_verification (jti);
+
 ALTER TABLE totp_configurations
 	DROP CONSTRAINT IF EXISTS totp_configurations_username_key1,
 	DROP CONSTRAINT IF EXISTS totp_configurations_username_key;
+
+DROP INDEX IF EXISTS totp_configurations_username_key1;
+DROP INDEX IF EXISTS totp_configurations_username_key;
+
+CREATE UNIQUE INDEX totp_configurations_username_key ON totp_configurations (username);
+
+ALTER TABLE user_preferences
+	DROP CONSTRAINT IF EXISTS user_preferences_username_key;
+
+DROP INDEX IF EXISTS user_preferences_username_key;
+
+CREATE UNIQUE INDEX user_preferences_username_key ON user_preferences (username);
 
 ALTER TABLE webauthn_devices
 	DROP CONSTRAINT IF EXISTS webauthn_devices_username_description_key1,
@@ -20,16 +53,13 @@ ALTER TABLE webauthn_devices
 	DROP CONSTRAINT IF EXISTS webauthn_devices_kid_key,
 	DROP CONSTRAINT IF EXISTS webauthn_devices_lookup_key;
 
-DROP INDEX IF EXISTS totp_configurations_username_key1;
 DROP INDEX IF EXISTS webauthn_devices_username_description_key1;
 DROP INDEX IF EXISTS webauthn_devices_kid_key1;
 DROP INDEX IF EXISTS webauthn_devices_lookup_key1;
-DROP INDEX IF EXISTS totp_configurations_username_key;
 DROP INDEX IF EXISTS webauthn_devices_username_description_key;
 DROP INDEX IF EXISTS webauthn_devices_kid_key;
 DROP INDEX IF EXISTS webauthn_devices_lookup_key;
 
-CREATE UNIQUE INDEX totp_configurations_username_key ON totp_configurations (username);
 CREATE UNIQUE INDEX webauthn_devices_kid_key ON webauthn_devices (kid);
 CREATE UNIQUE INDEX webauthn_devices_lookup_key ON webauthn_devices (username, description);
 
