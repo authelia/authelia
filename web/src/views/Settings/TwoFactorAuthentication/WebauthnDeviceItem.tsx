223d0dae5f7cedf1241d12621aac4f5443edf3e2
Stephen Kent <smkent@smkent.net>
2022-11-25 17:14:36 -0800

    feat: rework webauthn devices list ui

--- web/src/views/Settings/TwoFactorAuthentication/WebauthnDeviceItem.tsx
+++ web/src/views/Settings/TwoFactorAuthentication/WebauthnDeviceItem.tsx
@@ -2,22 +2,10 @@ import React from "react";
 
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
-import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
-import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
-import {
-    Box,
-    CircularProgress,
-    Collapse,
-    Divider,
-    Grid,
-    IconButton,
-    Stack,
-    Switch,
-    TableCell,
-    TableRow,
-    Tooltip,
-    Typography,
-} from "@mui/material";
+import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
+import KeyRoundedIcon from "@mui/icons-material/KeyRounded";
+import { Box, Button, CircularProgress, Stack, Typography } from "@mui/material";
+import { ButtonProps } from "@mui/material/Button";
 import { useTranslation } from "react-i18next";
 
 import { WebauthnDevice } from "@models/Webauthn";
@@ -28,6 +16,7 @@ interface Props {
     editing: boolean;
     webauthnShowDetails: boolean;
     handleWebAuthnDetailsChange: () => void;
+    handleDetails: () => void;
     handleDelete: () => void;
     handleEdit: () => void;
 }
@@ -36,127 +25,63 @@ export default function WebauthnDeviceItem(props: Props) {
     const { t: translate } = useTranslation("settings");
 
     return (
-        <React.Fragment>
-            <TableRow sx={{ "& > *": { borderBottom: "unset" } }} key={props.device.kid.toString()}>
-                <TableCell>
-                    <Tooltip title={translate("Show Details")} placement="right">
-                        <IconButton aria-label="expand row" size="small" onClick={props.handleWebAuthnDetailsChange}>
-                            {props.webauthnShowDetails ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-                        </IconButton>
-                    </Tooltip>
-                </TableCell>
-                <TableCell component="th" scope="row">
-                    {props.device.description}
-                </TableCell>
-                <TableCell>
-                    <Switch defaultChecked={false} size="small" />
-                </TableCell>
-                <TableCell align="center">
-                    <Stack direction="row" spacing={1} alignItems="center" justifyContent="center">
-                        {props.editing ? (
-                            <CircularProgress color="inherit" size={24} />
-                        ) : (
-                            <Tooltip title={translate("Edit")} placement="bottom">
-                                <IconButton aria-label="edit" onClick={props.handleEdit}>
-                                    <EditIcon />
-                                </IconButton>
-                            </Tooltip>
-                        )}
-                        {props.deleting ? (
-                            <CircularProgress color="inherit" size={24} />
-                        ) : (
-                            <Tooltip title={translate("Delete")} placement="bottom">
-                                <IconButton aria-label="delete" onClick={props.handleDelete}>
-                                    <DeleteIcon />
-                                </IconButton>
-                            </Tooltip>
-                        )}
-                    </Stack>
-                </TableCell>
-            </TableRow>
-            <TableRow>
-                <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={4}>
-                    <Collapse in={props.webauthnShowDetails} timeout="auto" unmountOnExit>
-                        <Grid container spacing={2} sx={{ mb: 3, margin: 1 }}>
-                            <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-                                <Box sx={{ margin: 1 }}>
-                                    <Typography variant="h6" gutterBottom component="div">
-                                        {translate("Details")}
-                                    </Typography>
-                                </Box>
-                            </Grid>
-                            <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-                                <Divider variant="middle" />
-                            </Grid>
-                            <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-                                <Typography>
-                                    {translate("Webauthn Credential Identifier", {
-                                        id: props.device.kid.toString(),
-                                    })}
-                                </Typography>
-                            </Grid>
-                            <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-                                <Typography>
-                                    Public Key: {props.device.public_key}
-                                    {translate("Webauthn Public Key", {
-                                        key: props.device.public_key.toString(),
-                                    })}
-                                </Typography>
-                            </Grid>
-                            <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-                                <Divider variant="middle" />
-                            </Grid>
-                            <Grid item xs={6} sm={6} md={4} lg={4} xl={3}>
-                                <Typography>{translate("Relying Party ID")}</Typography>
-                                <Typography>{props.device.rpid}</Typography>
-                            </Grid>
-                            <Grid item xs={6} sm={6} md={4} lg={4} xl={3}>
-                                <Typography>{translate("Authenticator Attestation GUID")}</Typography>
-                                <Typography>
-                                    {props.device.aaguid === undefined ? "N/A" : props.device.aaguid}
-                                </Typography>
-                            </Grid>
-                            <Grid item xs={6} sm={6} md={4} lg={4} xl={3}>
-                                <Typography>{translate("Attestation Type")}</Typography>
-                                <Typography>{props.device.attestation_type}</Typography>
-                            </Grid>
-                            <Grid item xs={6} sm={6} md={4} lg={4} xl={3}>
-                                <Typography>{translate("Transports")}</Typography>
-                                <Typography>
-                                    {props.device.transports.length === 0 ? "N/A" : props.device.transports.join(", ")}
-                                </Typography>
-                            </Grid>
-                            <Grid item xs={6} sm={6} md={4} lg={4} xl={3}>
-                                <Typography>{translate("Clone Warning")}</Typography>
-                                <Typography>
-                                    {props.device.clone_warning ? translate("Yes") : translate("No")}
-                                </Typography>
-                            </Grid>
-                            <Grid item xs={6} sm={6} md={4} lg={4} xl={3}>
-                                <Typography>{translate("Created")}</Typography>
-                                <Typography>{props.device.created_at.toString()}</Typography>
-                            </Grid>
-                            <Grid item xs={6} sm={6} md={4} lg={4} xl={3}>
-                                <Typography>{translate("Last Used")}</Typography>
-                                <Typography>
-                                    {props.device.last_used_at === undefined
-                                        ? translate("Never")
-                                        : props.device.last_used_at.toString()}
-                                </Typography>
-                            </Grid>
-                            <Grid item xs={6} sm={6} md={4} lg={4} xl={3}>
-                                <Typography>{translate("Usage Count")}</Typography>
-                                <Typography>
-                                    {props.device.sign_count === 0 ? translate("Never") : props.device.sign_count}
-                                </Typography>
-                            </Grid>
-                            <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-                                <Divider variant="middle" />
-                            </Grid>
-                        </Grid>
-                    </Collapse>
-                </TableCell>
-            </TableRow>
-        </React.Fragment>
+        <Stack direction="row" spacing={1} alignItems="center">
+            <KeyRoundedIcon fontSize="large" />
+            <Stack spacing={0} sx={{ minWidth: 400 }}>
+                <Box>
+                    <Typography display="inline" sx={{ fontWeight: "bold" }}>
+                        {props.device.description}
+                    </Typography>
+                    <Typography
+                        display="inline"
+                        variant="body2"
+                    >{` (${props.device.attestation_type.toUpperCase()})`}</Typography>
+                </Box>
+                <Typography>Added {props.device.created_at.toString()}</Typography>
+                <Typography>
+                    {props.device.last_used_at === undefined
+                        ? translate("Never used")
+                        : "Last used " + props.device.last_used_at.toString()}
+                </Typography>
+            </Stack>
+            <Button variant="outlined" color="primary" startIcon={<InfoOutlinedIcon />} onClick={props.handleDetails}>
+                {translate("Info")}
+            </Button>
+            <LoadingButton
+                loading={props.editing}
+                variant="outlined"
+                color="primary"
+                startIcon={<EditIcon />}
+                onClick={props.handleEdit}
+            >
+                {translate("Edit")}
+            </LoadingButton>
+            <LoadingButton
+                loading={props.deleting}
+                variant="outlined"
+                color="secondary"
+                startIcon={<DeleteIcon />}
+                onClick={props.handleDelete}
+            >
+                {translate("Remove")}
+            </LoadingButton>
+        </Stack>
     );
+}
+
+interface LoadingButtonProps extends ButtonProps {
+    loading: boolean;
+}
+
+function LoadingButton(props: LoadingButtonProps) {
+    let { loading, ...childProps } = props;
+    if (loading) {
+        childProps = {
+            ...childProps,
+            startIcon: <CircularProgress color="inherit" size={20} />,
+            color: "inherit",
+            onClick: undefined,
+        };
+    }
+    return <Button {...childProps}></Button>;
 }
