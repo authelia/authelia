0aa674505928f1d1d28f70db777710f5c7a4713a
James Elliott <james-d-elliott@users.noreply.github.com>
2022-12-05 09:37:08 +1100

    build(deps): update module github.com/go-crypt/crypt to v0.2.2 (#4452)

--- internal/oidc/hasher_test.go
+++ internal/oidc/hasher_test.go
@@ -5,32 +5,46 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestShouldNotRaiseErrorOnEqualPasswordsPlainText(t *testing.T) {
-	hasher := AdaptiveHasher{}
+	hasher, err := NewAdaptiveHasher()
+
+	require.NoError(t, err)
 
 	a := []byte("$plaintext$abc")
 	b := []byte("abc")
 
 	ctx := context.Background()
 
-	err := hasher.Compare(ctx, a, b)
+	assert.NoError(t, hasher.Compare(ctx, a, b))
+}
 
-	assert.NoError(t, err)
+func TestShouldNotRaiseErrorOnEqualPasswordsPlainTextWithSeparator(t *testing.T) {
+	hasher, err := NewAdaptiveHasher()
+
+	require.NoError(t, err)
+
+	a := []byte("$plaintext$abc$123")
+	b := []byte("abc$123")
+
+	ctx := context.Background()
+
+	assert.NoError(t, hasher.Compare(ctx, a, b))
 }
 
 func TestShouldRaiseErrorOnNonEqualPasswordsPlainText(t *testing.T) {
-	hasher := AdaptiveHasher{}
+	hasher, err := NewAdaptiveHasher()
+
+	require.NoError(t, err)
 
 	a := []byte("$plaintext$abc")
 	b := []byte("abcd")
 
 	ctx := context.Background()
 
-	err := hasher.Compare(ctx, a, b)
-
-	assert.Equal(t, errPasswordsDoNotMatch, err)
+	assert.Equal(t, errPasswordsDoNotMatch, hasher.Compare(ctx, a, b))
 }
 
 func TestShouldHashPassword(t *testing.T) {
