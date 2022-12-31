790419cb016cccadc8e2feb756c908dd3f0a4f33
Levko Burburas <levko.burburas@biptec.com>
2022-12-31 18:04:59 +0200

    fix: vite.config.ts

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,10 +1,11 @@
+import { resolve } from "path";
+
 import react from "@vitejs/plugin-react";
 import { defineConfig, loadEnv } from "vite";
 import eslintPlugin from "vite-plugin-eslint";
 import istanbul from "vite-plugin-istanbul";
 import svgr from "vite-plugin-svgr";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { resolve } from "path";
 
 // @ts-ignore
 export default defineConfig(({ mode }) => {
