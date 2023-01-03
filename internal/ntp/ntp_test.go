6c3d64a06c1a47030e97ffca42196317aad1f2e5
Manuel Nuñez <10672208+mind-ar@users.noreply.github.com>
2023-01-03 19:12:19 -0300

    test(ntp): add missing tests (#4693)

--- internal/ntp/ntp_test.go
+++ internal/ntp/ntp_test.go
@@ -10,11 +10,28 @@ import (
 	"github.com/authelia/authelia/v4/internal/configuration/validator"
 )
 
-func TestShouldCheckNTP(t *testing.T) {
+func TestShouldCheckNTPV4(t *testing.T) {
 	config := &schema.Configuration{
 		NTP: schema.NTPConfiguration{
 			Address:       "time.cloudflare.com:123",
 			Version:       4,
+			MaximumDesync: time.Second * 3,
+		},
+	}
+
+	sv := schema.NewStructValidator()
+	validator.ValidateNTP(config, sv)
+
+	ntp := NewProvider(&config.NTP)
+
+	assert.NoError(t, ntp.StartupCheck())
+}
+
+func TestShouldCheckNTPV3(t *testing.T) {
+	config := &schema.Configuration{
+		NTP: schema.NTPConfiguration{
+			Address:       "time.cloudflare.com:123",
+			Version:       3,
 			MaximumDesync: time.Second * 3,
 		},
 	}
