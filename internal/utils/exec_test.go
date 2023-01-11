8cd311002690b0e1f8c8b727fa8398b1b2256b93
10672208+mind-ar <10672208+mind-ar@users.noreply.github.com>
2023-01-11 22:55:29 +0000

    fix: lint

--- internal/utils/exec_test.go
+++ internal/utils/exec_test.go
@@ -10,7 +10,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-
 func TestShouldExecCommandOnAutheliaRootPath(t *testing.T) {
 	cmd := Command("pwd")
 	result, err := cmd.CombinedOutput()
@@ -32,27 +31,25 @@ func TestCommandShouldOutputResult(t *testing.T) {
 	assert.Equal(t, "hello", output)
 }
 
-
 func TestShouldWaitUntilCommandEnds(t *testing.T) {
 	cmd := Command("sleep", "2")
 
-	err := RunCommandWithTimeout(cmd, 3 * time.Second)
+	err := RunCommandWithTimeout(cmd, 3*time.Second)
 	assert.NoError(t, err, "")
 }
-
 
 func TestShouldTimeoutWaitingCommand(t *testing.T) {
 	cmd := Command("sleep", "3")
 
-	err := RunCommandWithTimeout(cmd, 2 * time.Second)
+	err := RunCommandWithTimeout(cmd, 2*time.Second)
 	assert.Error(t, err)
 }
 
 func TestShouldRunFuncUntilNoError(t *testing.T) {
 	counter := 0
 
-	err := RunFuncWithRetry(3, 500 * time.Millisecond, func() error {
-		counter ++
+	err := RunFuncWithRetry(3, 500*time.Millisecond, func() error {
+		counter++
 		if counter < 3 {
 			return errors.New("not ready")
 		}
@@ -64,8 +61,8 @@ func TestShouldRunFuncUntilNoError(t *testing.T) {
 func TestShouldFailAfterMaxAttemps(t *testing.T) {
 	counter := 0
 
-	err := RunFuncWithRetry(3, 500 * time.Millisecond, func() error {
-		counter ++
+	err := RunFuncWithRetry(3, 500*time.Millisecond, func() error {
+		counter++
 		if counter < 4 {
 			return errors.New("not ready")
 		}
