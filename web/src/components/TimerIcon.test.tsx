a6cb041ca5758deb9144e87128469929d4b8b000
Amir Zarrinkafsh <nightah@me.com>
2023-02-20 20:56:36 +1100

    test(web): additional frontend component tests and fix typo

--- web/src/components/TimerIcon.test.tsx
+++ web/src/components/TimerIcon.test.tsx
@@ -1,9 +1,35 @@
 import React from "react";
 
-import { render } from "@testing-library/react";
+import { act, render } from "@testing-library/react";
 
 import TimerIcon from "@components/TimerIcon";
 
+beforeEach(() => {
+    vi.useFakeTimers();
+});
+
+afterEach(() => {
+    // restoring date after each test run
+    vi.useRealTimers();
+});
+
 it("renders without crashing", () => {
     render(<TimerIcon width={32} height={32} period={30} />);
+});
+
+it("renders a timer icon with updating progress for a given period", () => {
+    const { container } = render(<TimerIcon width={32} height={32} period={30} />);
+    const initialProgress = container
+        .firstElementChild!.firstElementChild!.nextElementSibling!.nextElementSibling!.getAttribute("stroke-dasharray")!
+        .split(/\s(.+)/)[0];
+
+    act(() => {
+        vi.advanceTimersByTime(3000);
+    });
+
+    const updatedProgress = container
+        .firstElementChild!.firstElementChild!.nextElementSibling!.nextElementSibling!.getAttribute("stroke-dasharray")!
+        .split(/\s(.+)/)[0];
+
+    expect(Number(updatedProgress)).toBeGreaterThan(Number(initialProgress));
 });
--- web/src/components/TypographyWithTooltip.test.tsx
+++ web/src/components/TypographyWithTooltip.test.tsx
@@ -2,12 +2,43 @@ import React from "react";
 
 import { render } from "@testing-library/react";
 
-import TypographyWithTooltip from "@components/TypographyWithTootip";
+import TypographyWithTooltip, { Props } from "@components/TypographyWithTooltip";
 
-it("renders without crashing", () => {
-    render(<TypographyWithTooltip value={"Example"} variant={"h5"} />);
-});
+describe("TypographyWithTooltip", () => {
+    const defaultProps: Props = {
+        variant: "h5",
+        value: "Example",
+    };
 
-it("renders with tooltip without crashing", () => {
-    render(<TypographyWithTooltip value={"Example"} tooltip={"A tooltip"} variant={"h5"} />);
+    it("renders without crashing", () => {
+        render(<TypographyWithTooltip {...defaultProps} />);
+    });
+
+    it("renders with tooltip without crashing", () => {
+        const props: Props = {
+            ...defaultProps,
+            tooltip: "A tooltip",
+        };
+        render(<TypographyWithTooltip {...props} />);
+    });
+
+    it("renders the text correctly", () => {
+        const props: Props = {
+            ...defaultProps,
+            value: "Test text",
+        };
+        const { getByText } = render(<TypographyWithTooltip {...props} />);
+        const element = getByText(props.value!);
+        expect(element).toBeInTheDocument();
+    });
+
+    it("renders the tooltip correctly", () => {
+        const props: Props = {
+            ...defaultProps,
+            tooltip: "Test tooltip",
+        };
+        const { getByText } = render(<TypographyWithTooltip {...props} />);
+        const element = getByText(props.value!);
+        expect(element).toHaveAttribute("aria-label", props.tooltip);
+    });
 });
