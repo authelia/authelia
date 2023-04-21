7a7a2587512c442c736e4683356cf99a7cb3b7a3
James Elliott <james-d-elliott@users.noreply.github.com>
2023-04-21 23:25:09 +1000

    fix: remove unused elements
    Signed-off-by: James Elliott <james-d-elliott@users.noreply.github.com>

--- web/src/services/RegisterDevice.ts
+++ web/src/services/RegisterDevice.ts
@@ -1,17 +1,9 @@
-import { CompleteTOTPRegistrationPath, InitiateTOTPRegistrationPath, TOTPRegistrationPath } from "@services/Api";
-import { Post, PostWithOptionalResponse, Put } from "@services/Client";
-
-export async function initiateTOTPRegistrationProcess() {
-    await PostWithOptionalResponse(InitiateTOTPRegistrationPath);
-}
+import { TOTPRegistrationPath } from "@services/Api";
+import { Put } from "@services/Client";
 
 interface CompleteTOTPRegistrationResponse {
     base32_secret: string;
     otpauth_url: string;
-}
-
-export async function completeTOTPRegistrationProcess(processToken: string) {
-    return Post<CompleteTOTPRegistrationResponse>(CompleteTOTPRegistrationPath, { token: processToken });
 }
 
 export async function getTOTPSecret(algorithm: string, length: number, period: number) {
