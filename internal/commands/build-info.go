a7eafda81f9e55c1bc542a19033329e46d19387e
James Elliott <james-d-elliott@users.noreply.github.com>
2023-07-26 11:00:05 +1000

    feat(commands): go build info
    Include go build info in the authelia build-info command.
    
    Signed-off-by: James Elliott <james-d-elliott@users.noreply.github.com>

--- internal/commands/build-info.go
+++ internal/commands/build-info.go
@@ -2,9 +2,10 @@ package commands
 
 import (
 	"fmt"
-	"runtime"
-
 	"github.com/spf13/cobra"
+	"runtime"
+	"runtime/debug"
+	"strings"
 
 	"github.com/authelia/authelia/v4/internal/utils"
 )
@@ -26,8 +27,35 @@ func newBuildInfoCmd(ctx *CmdCtx) (cmd *cobra.Command) {
 
 // BuildInfoRunE is the RunE for the authelia build-info command.
 func (ctx *CmdCtx) BuildInfoRunE(_ *cobra.Command, _ []string) (err error) {
+	var (
+		info *debug.BuildInfo
+		ok   bool
+	)
+
+	if info, ok = debug.ReadBuildInfo(); !ok {
+		return fmt.Errorf("failed to read build info")
+	}
+
+	b := &strings.Builder{}
+
+	b.WriteString(fmt.Sprintf(fmtAutheliaBuildGo, info.GoVersion, info.Main.Path, info.Path))
+
+	if len(info.Settings) != 0 {
+		b.WriteString("    Settings:\n")
+		for _, setting := range info.Settings {
+			b.WriteString(fmt.Sprintf("        %s: %s\n", setting.Key, setting.Value))
+		}
+	}
+
+	if len(info.Deps) != 0 {
+		b.WriteString("    Dependencies:\n")
+		for _, dep := range info.Deps {
+			b.WriteString(fmt.Sprintf("        %s@%s (%s)\n", dep.Path, dep.Version, dep.Sum))
+		}
+	}
+
 	_, err = fmt.Printf(fmtAutheliaBuild, utils.BuildTag, utils.BuildState, utils.BuildBranch, utils.BuildCommit,
-		utils.BuildNumber, runtime.GOOS, runtime.GOARCH, utils.BuildDate, utils.BuildExtra)
+		utils.BuildNumber, runtime.GOOS, runtime.GOARCH, utils.BuildDate, utils.BuildExtra, b.String())
 
 	return err
 }
