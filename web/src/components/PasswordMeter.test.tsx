b0746dc4d43eddb60d34644811749d2c3a948252
Manuel Nuñez <10672208+mind-ar@users.noreply.github.com>
2023-08-02 21:38:21 -0300

    feat(web): add password policy visible feedback on password change (#5714)
    This makes user feedback regarding the password policy more prominent.
    
    Closes #5048
    
    Signed-off-by: mind-ar

--- web/src/components/PasswordMeter.test.tsx
+++ web/src/components/PasswordMeter.test.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+// import React from "react";
 
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 
 import PasswordMeter from "@components/PasswordMeter";
 import "@i18n/index.ts";
@@ -40,4 +40,68 @@ it("renders adjusted height without crashing", () => {
             }}
         />,
     );
+});
+
+it("displays warning message on password too large", async () => {
+    const maxLenght = 5;
+    render(
+        <PasswordMeter
+            value={"password"}
+            policy={{
+                max_length: maxLenght,
+                min_length: 4,
+                min_score: 0,
+                require_lowercase: true,
+                require_number: true,
+                require_special: true,
+                require_uppercase: true,
+                mode: PasswordPolicyMode.Standard,
+            }}
+        />,
+    );
+
+    const text = `Must not be more than ${maxLenght} characters in length`;
+    expect(screen.queryByText(text)).toBeInTheDocument();
+});
+
+it("displays warning message on password too short", async () => {
+    const minLenght = 5;
+    render(
+        <PasswordMeter
+            value={"abc"}
+            policy={{
+                max_length: 0,
+                min_length: minLenght,
+                min_score: 0,
+                require_lowercase: true,
+                require_number: true,
+                require_special: true,
+                require_uppercase: true,
+                mode: PasswordPolicyMode.Standard,
+            }}
+        />,
+    );
+
+    const text = `Must be at least ${minLenght} characters in length`;
+    expect(screen.queryByText(text)).toBeInTheDocument();
+});
+
+it("displays warning message on password policy fail", async () => {
+    render(
+        <PasswordMeter
+            value={""}
+            policy={{
+                max_length: 0,
+                min_length: 0,
+                min_score: 0,
+                require_lowercase: true,
+                require_number: true,
+                require_special: true,
+                require_uppercase: true,
+                mode: PasswordPolicyMode.Standard,
+            }}
+        />,
+    );
+
+    expect(screen.queryByText("The password does not meet the password policy")).toBeInTheDocument();
 });
