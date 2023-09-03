fb4be19d03de95c45ea06b3cc9586738b3579d31
James Elliott <james-d-elliott@users.noreply.github.com>
2023-09-04 09:20:23 +1000

    test: refactor linting (#5956)
    Signed-off-by: James Elliott <james-d-elliott@users.noreply.github.com>

--- internal/suites/suite_pathprefix.go
+++ internal/suites/suite_pathprefix.go
@@ -1,7 +1,6 @@
 package suites
 
 import (
-	"fmt"
 	"os"
 	"time"
 )
@@ -45,23 +44,7 @@ func init() {
 	}
 
 	displayAutheliaLogs := func() error {
-		backendLogs, err := dockerEnvironment.Logs("authelia-backend", nil)
-		if err != nil {
-			return err
-		}
-
-		fmt.Println(backendLogs)
-
-		if os.Getenv("CI") != t {
-			frontendLogs, err := dockerEnvironment.Logs("authelia-frontend", nil)
-			if err != nil {
-				return err
-			}
-
-			fmt.Println(frontendLogs)
-		}
-
-		return nil
+		return dockerEnvironment.PrintLogs("authelia-backend", "authelia-frontend")
 	}
 
 	teardown := func(suitePath string) error {
