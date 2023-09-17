9325e448d5b557b345264aab5b66c4a94737afaf
James Elliott <james-d-elliott@users.noreply.github.com>
2023-09-17 14:04:41 +1000

    test: misc lint updates (#6004)
    Signed-off-by: James Elliott <james-d-elliott@users.noreply.github.com>

--- internal/templates/funcs_test.go
+++ internal/templates/funcs_test.go
@@ -274,18 +274,22 @@ func TestFuncElemsJoin(t *testing.T) {
 }
 
 func TestFuncIterate(t *testing.T) {
+	uintptr := func(in uint) *uint {
+		return &in
+	}
+
 	testCases := []struct {
 		name     string
-		have     uint
+		have     *uint
 		expected []uint
 	}{
-		{"ShouldGiveZeroResults", 0, nil},
-		{"ShouldGive10Results", 10, []uint{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}},
+		{"ShouldGiveZeroResults", uintptr(0), nil},
+		{"ShouldGive10Results", uintptr(10), []uint{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			assert.Equal(t, tc.expected, FuncIterate(&tc.have))
+			assert.Equal(t, tc.expected, FuncIterate(tc.have))
 		})
 	}
 }
