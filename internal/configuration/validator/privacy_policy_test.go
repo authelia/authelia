be81c375351848e9a0292b06962046d4b6f07944
James Elliott <james-d-elliott@users.noreply.github.com>
2023-09-17 13:37:25 +1000

    test: misc lint updates
    Signed-off-by: James Elliott <james-d-elliott@users.noreply.github.com>

--- internal/configuration/validator/privacy_policy_test.go
+++ internal/configuration/validator/privacy_policy_test.go
@@ -11,14 +11,14 @@ import (
 func TestValidatePrivacyPolicy(t *testing.T) {
 	testCases := []struct {
 		name     string
-		have     schema.PrivacyPolicy
+		have     *schema.PrivacyPolicy
 		expected string
 	}{
-		{"ShouldValidateDefaultConfig", schema.PrivacyPolicy{}, ""},
-		{"ShouldValidateValidEnabledPolicy", schema.PrivacyPolicy{Enabled: true, PolicyURL: MustParseURL("https://example.com/privacy")}, ""},
-		{"ShouldValidateValidEnabledPolicyWithUserAcceptance", schema.PrivacyPolicy{Enabled: true, RequireUserAcceptance: true, PolicyURL: MustParseURL("https://example.com/privacy")}, ""},
-		{"ShouldNotValidateOnInvalidScheme", schema.PrivacyPolicy{Enabled: true, PolicyURL: MustParseURL("http://example.com/privacy")}, "privacy_policy: option 'policy_url' must have the 'https' scheme but it's configured as 'http'"},
-		{"ShouldNotValidateOnMissingURL", schema.PrivacyPolicy{Enabled: true}, "privacy_policy: option 'policy_url' must be provided when the option 'enabled' is true"},
+		{"ShouldValidateDefaultConfig", &schema.PrivacyPolicy{}, ""},
+		{"ShouldValidateValidEnabledPolicy", &schema.PrivacyPolicy{Enabled: true, PolicyURL: MustParseURL("https://example.com/privacy")}, ""},
+		{"ShouldValidateValidEnabledPolicyWithUserAcceptance", &schema.PrivacyPolicy{Enabled: true, RequireUserAcceptance: true, PolicyURL: MustParseURL("https://example.com/privacy")}, ""},
+		{"ShouldNotValidateOnInvalidScheme", &schema.PrivacyPolicy{Enabled: true, PolicyURL: MustParseURL("http://example.com/privacy")}, "privacy_policy: option 'policy_url' must have the 'https' scheme but it's configured as 'http'"},
+		{"ShouldNotValidateOnMissingURL", &schema.PrivacyPolicy{Enabled: true}, "privacy_policy: option 'policy_url' must be provided when the option 'enabled' is true"},
 	}
 
 	validator := schema.NewStructValidator()
@@ -27,7 +27,7 @@ func TestValidatePrivacyPolicy(t *testing.T) {
 		validator.Clear()
 
 		t.Run(tc.name, func(t *testing.T) {
-			ValidatePrivacyPolicy(&tc.have, validator)
+			ValidatePrivacyPolicy(tc.have, validator)
 
 			assert.Len(t, validator.Warnings(), 0)
 
