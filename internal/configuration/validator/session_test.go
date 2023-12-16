authelia/authelia

commit 5edd5fcf69d8f17a6731e0f1b5cf8f8591823e56

fix(configuration): optional value not treated as optional (#5853)
Fixes a configuration validation process for redis where the redis port was meant to be optional but was not treated as optional.

Signed-off-by: Paul Calabro <585059+paulcalabro@users.noreply.github.com>

internal/configuration/validator/session.go
@@ -218,7 +218,9 @@ func validateRedis(config *schema.SessionConfiguration, validator *schema.Struct
 
 	validateRedisCommon(config, validator)
 
-	if !path.IsAbs(config.Redis.Host) && (config.Redis.Port < 1 || config.Redis.Port > 65535) {
+	if config.Redis.Port == 0 {
+		config.Redis.Port = schema.DefaultRedisConfiguration.Port
+	} else if !path.IsAbs(config.Redis.Host) && (config.Redis.Port < 1 || config.Redis.Port > 65535) {
 		validator.Push(fmt.Errorf(errFmtSessionRedisPortRange, config.Redis.Port))
 	}
 
@@ -234,7 +236,7 @@ func validateRedisSentinel(config *schema.SessionConfiguration, validator *schem
 
 	if config.Redis.Port == 0 {
 		config.Redis.Port = 26379
-	} else if config.Redis.Port < 0 || config.Redis.Port > 65535 {
+	} else if config.Redis.Port < 1 || config.Redis.Port > 65535 {
 		validator.Push(fmt.Errorf(errFmtSessionRedisPortRange, config.Redis.Port))
 	}
 

internal/configuration/validator/session_test.go
@@ -299,7 +299,7 @@ func TestShouldRaiseErrorWhenRedisIsUsedAndSecretNotSet(t *testing.T) {
 	assert.EqualError(t, validator.Errors()[0], fmt.Sprintf(errFmtSessionSecretRequired, "redis"))
 }
 
-func TestShouldRaiseErrorWhenRedisHasHostnameButNoPort(t *testing.T) {
+func TestShouldNotRaiseErrorsAndSetDefaultPortWhenRedisPortBlank(t *testing.T) {
 	validator := schema.NewStructValidator()
 	config := newDefaultSessionConfig()
 
@@ -319,8 +319,33 @@ func TestShouldRaiseErrorWhenRedisHasHostnameButNoPort(t *testing.T) {
 	ValidateSession(&config, validator)
 
 	assert.False(t, validator.HasWarnings())
+	assert.False(t, validator.HasErrors())
+
+	assert.Equal(t, 6379, config.Redis.Port)
+}
+
+func TestShouldRaiseErrorWhenRedisPortInvalid(t *testing.T) {
+	validator := schema.NewStructValidator()
+	config := newDefaultSessionConfig()
+
+	ValidateSession(&config, validator)
+
+	assert.Len(t, validator.Errors(), 0)
+	validator.Clear()
+
+	config = newDefaultSessionConfig()
+
+	// Set redis config because password must be set only when redis is used.
+	config.Redis = &schema.RedisSessionConfiguration{
+		Host: "redis.localhost",
+		Port: -1,
+	}
+
+	ValidateSession(&config, validator)
+
+	assert.False(t, validator.HasWarnings())
 	assert.Len(t, validator.Errors(), 1)
-	assert.EqualError(t, validator.Errors()[0], "session: redis: option 'port' must be between 1 and 65535 but it's configured as '0'")
+	assert.EqualError(t, validator.Errors()[0], "session: redis: option 'port' must be between 1 and 65535 but it's configured as '-1'")
 }
 
 func TestShouldRaiseOneErrorWhenRedisHighAvailabilityHasNodesWithNoHost(t *testing.T) {

internal/configuration/schema/session.go
@@ -77,6 +77,7 @@ var DefaultSessionConfiguration = SessionConfiguration{
 
 // DefaultRedisConfiguration is the default redis configuration.
 var DefaultRedisConfiguration = RedisSessionConfiguration{
+	Port: 6379,
 	TLS: &TLSConfig{
 		MinimumVersion: TLSVersion{Value: tls.VersionTLS12},
 	},

commit 41faab35875086acab63e068e866f927146a58d4

fix(configuration): regression in redis default port
THis fixes an issue introduced in 5edd5fcf69d8f17a6731e0f1b5cf8f8591823e56 which prevented a redis socket from being used.

Signed-off-by: James Elliott <james-d-elliott@users.noreply.github.com>

internal/configuration/validator/session.go
@@ -250,9 +250,11 @@ func validateRedis(config *schema.Session, validator *schema.StructValidator) {
 
 	validateRedisCommon(config, validator)
 
-	if config.Redis.Port == 0 {
+	abs := path.IsAbs(config.Redis.Host)
+
+	if !abs && config.Redis.Port == 0 {
 		config.Redis.Port = schema.DefaultRedisConfiguration.Port
-	} else if !path.IsAbs(config.Redis.Host) && (config.Redis.Port < 1 || config.Redis.Port > 65535) {
+	} else if !abs && (config.Redis.Port < 1 || config.Redis.Port > 65535) {
 		validator.Push(fmt.Errorf(errFmtSessionRedisPortRange, config.Redis.Port))
 	}
 

internal/configuration/validator/session_test.go
@@ -298,6 +298,27 @@ func TestShouldHandleRedisConfigSuccessfully(t *testing.T) {
 	assert.Equal(t, 8, config.Session.Redis.MaximumActiveConnections)
 }
 
+func TestShouldHandleRedisSocketConfigSuccessfully(t *testing.T) {
+	validator := schema.NewStructValidator()
+	config := newDefaultSessionConfig()
+
+	// Set redis config because password must be set only when redis is used.
+	config.Session.Redis = &schema.SessionRedis{
+		Host:     "/path/to/socket.sock",
+		Port:     0,
+		Password: "password",
+	}
+
+	ValidateSession(&config, validator)
+
+	assert.Len(t, validator.Warnings(), 0)
+	assert.Len(t, validator.Errors(), 0)
+
+	assert.Equal(t, 8, config.Session.Redis.MaximumActiveConnections)
+	assert.Equal(t, 0, config.Session.Redis.Port)
+	assert.Equal(t, "/path/to/socket.sock", config.Session.Redis.Host)
+}
+
 func TestShouldRaiseErrorWithInvalidRedisPortLow(t *testing.T) {
 	validator := schema.NewStructValidator()
 	config := newDefaultSessionConfig()