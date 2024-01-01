2c6a8e18ba1496222452ada2f1f64d1af901d631
James Elliott <james-d-elliott@users.noreply.github.com>
2024-01-01 12:17:43 +1100

    feat(server): recover panics
    This adds a general panic recovery middleware to the server so any request can theoretically not cause unreoovered panics.
    
    Signed-off-by: James Elliott <james-d-elliott@users.noreply.github.com>

--- internal/middlewares/errors.go
+++ internal/middlewares/errors.go
@@ -1,6 +1,11 @@
 package middlewares
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+
+	"github.com/valyala/fasthttp"
+)
 
 var (
 	// ErrMissingXForwardedProto is returned on methods which require an X-Forwarded-Proto header.
@@ -15,3 +20,34 @@ var (
 	// ErrMissingXOriginalURL is returned on methods which require an X-Original-URL header.
 	ErrMissingXOriginalURL = errors.New("missing required X-Original-URL header")
 )
+
+// RecoverPanic recovers from panics and logs the error.
+func RecoverPanic(next fasthttp.RequestHandler) fasthttp.RequestHandler {
+	return func(ctx *fasthttp.RequestCtx) {
+		defer func() {
+			if r := recover(); r != nil {
+				NewRequestLogger(ctx).WithError(recoverErr(r)).Error("Panic (recovered) occurred while handling requests, please report this error")
+
+				ctx.Response.Reset()
+				ctx.SetStatusCode(fasthttp.StatusInternalServerError)
+				ctx.SetContentTypeBytes(contentTypeTextPlain)
+				ctx.SetBodyString(fmt.Sprintf("%d %s", fasthttp.StatusInternalServerError, fasthttp.StatusMessage(fasthttp.StatusInternalServerError)))
+			}
+		}()
+
+		next(ctx)
+	}
+}
+
+func recoverErr(i any) error {
+	switch v := i.(type) {
+	case nil:
+		return nil
+	case string:
+		return fmt.Errorf("recovered panic: %s", v)
+	case error:
+		return fmt.Errorf("recovered panic: %w", v)
+	default:
+		return fmt.Errorf("recovered panic with unknown type: %v", v)
+	}
+}
