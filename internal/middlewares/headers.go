authelia/authelia

commit 427ed6c98aa6c24606166eb45306866bc679f912

fix(server): oidc flow failure due to corb (#6921)
This fixes an issue where the CORP/CORB/COEP headers cause issues with OpenID Connect 1.0 dialog windows. We will reevaluate these headers at a future date.

Signed-off-by: James Elliott <james-d-elliott@users.noreply.github.com>

internal/server/handlers.go
@@ -125,8 +125,7 @@ func handleRouter(config *schema.Configuration, providers middlewares.Providers)
 	handlerPublicHTML := newPublicHTMLEmbeddedHandler()
 	handlerLocales := newLocalesEmbeddedHandler()
 
-	bridge := middlewares.NewBridgeBuilder(*config, providers).
-		WithPreMiddlewares(middlewares.SecurityHeaders).Build()
+	bridge := middlewares.NewBridgeBuilder(*config, providers).Build()
 
 	bridgeSwagger := middlewares.NewBridgeBuilder(*config, providers).
 		WithPreMiddlewares(middlewares.SecurityHeadersRelaxed).Build()
@@ -182,16 +181,16 @@ func handleRouter(config *schema.Configuration, providers middlewares.Providers)
 	}
 
 	middlewareAPI := middlewares.NewBridgeBuilder(*config, providers).
-		WithPreMiddlewares(middlewares.SecurityHeaders, middlewares.SecurityHeadersNoStore, middlewares.SecurityHeadersCSPNone).
+		WithPreMiddlewares(middlewares.SecurityHeadersNoStore, middlewares.SecurityHeadersCSPNone).
 		Build()
 
 	middleware1FA := middlewares.NewBridgeBuilder(*config, providers).
-		WithPreMiddlewares(middlewares.SecurityHeaders, middlewares.SecurityHeadersNoStore, middlewares.SecurityHeadersCSPNone).
+		WithPreMiddlewares(middlewares.SecurityHeadersNoStore, middlewares.SecurityHeadersCSPNone).
 		WithPostMiddlewares(middlewares.Require1FA).
 		Build()
 
 	middlewareElevated1FA := middlewares.NewBridgeBuilder(*config, providers).
-		WithPreMiddlewares(middlewares.SecurityHeaders, middlewares.SecurityHeadersNoStore, middlewares.SecurityHeadersCSPNone).
+		WithPreMiddlewares(middlewares.SecurityHeadersNoStore, middlewares.SecurityHeadersCSPNone).
 		WithPostMiddlewares(middlewares.RequireElevated).
 		Build()
 
@@ -336,7 +335,7 @@ func handleRouter(config *schema.Configuration, providers middlewares.Providers)
 
 	if providers.OpenIDConnect != nil {
 		bridgeOIDC := middlewares.NewBridgeBuilder(*config, providers).WithPreMiddlewares(
-			middlewares.SecurityHeaders, middlewares.SecurityHeadersCSPNoneOpenIDConnect, middlewares.SecurityHeadersNoStore,
+			middlewares.SecurityHeadersCSPNoneOpenIDConnect, middlewares.SecurityHeadersNoStore,
 		).Build()
 
 		r.GET("/api/oidc/consent", bridgeOIDC(handlers.OpenIDConnectConsentGET))

internal/suites/example/compose/nginx/portal/nginx.conf
@@ -115,9 +115,6 @@ http {
             add_header X-Frame-Options "DENY";
             add_header Permissions-Policy "accelerometer=(), autoplay=(), camera=(), display-capture=(), geolocation=(), gyroscope=(), keyboard-map=(), magnetometer=(), microphone=(), midi=(), payment=(), picture-in-picture=(), screen-wake-lock=(), sync-xhr=(), xr-spatial-tracking=(), interest-cohort=()";
             add_header X-DNS-Prefetch-Control "off";
-            add_header Cross-Origin-Opener-Policy "same-origin";
-            add_header Cross-Origin-Embedder-Policy "require-corp";
-            add_header Cross-Origin-Resource-Policy "same-origin";
 
             proxy_pass        $frontend_endpoint;
         }
@@ -168,9 +165,6 @@ http {
         add_header X-Frame-Options "DENY";
         add_header Permissions-Policy "accelerometer=(), autoplay=(), camera=(), display-capture=(), geolocation=(), gyroscope=(), keyboard-map=(), magnetometer=(), microphone=(), midi=(), payment=(), picture-in-picture=(), screen-wake-lock=(), sync-xhr=(), xr-spatial-tracking=(), interest-cohort=()";
         add_header X-DNS-Prefetch-Control "off";
-        add_header Cross-Origin-Opener-Policy "same-origin";
-        add_header Cross-Origin-Embedder-Policy "require-corp";
-        add_header Cross-Origin-Resource-Policy "same-origin";
 
         error_page 497 301 =307 https://$host:$server_port$request_uri;
 
@@ -342,9 +336,6 @@ http {
         add_header X-Frame-Options "DENY";
         add_header Permissions-Policy "accelerometer=(), autoplay=(), camera=(), display-capture=(), geolocation=(), gyroscope=(), keyboard-map=(), magnetometer=(), microphone=(), midi=(), payment=(), picture-in-picture=(), screen-wake-lock=(), sync-xhr=(), xr-spatial-tracking=(), interest-cohort=()";
         add_header X-DNS-Prefetch-Control "off";
-        add_header Cross-Origin-Opener-Policy "same-origin";
-        add_header Cross-Origin-Embedder-Policy "require-corp";
-        add_header Cross-Origin-Resource-Policy "same-origin";
 
         error_page 497 301 =307 https://$host:$server_port$request_uri;
 

internal/handlers/handler_authz_test.go
@@ -80,7 +80,7 @@ func (s *AuthzSuite) Builder() (builder *AuthzBuilder) {
 	return
 }
 
-func (s *AuthzSuite) BuilderWithBearerScheme() (buildier *AuthzBuilder) {
+func (s *AuthzSuite) BuilderWithBearerScheme() (builder *AuthzBuilder) {
 	switch s.implementation {
 	case AuthzImplExtAuthz:
 		return NewAuthzBuilder().WithImplementationExtAuthz().WithStrategies(NewHeaderProxyAuthorizationAuthnStrategy(model.AuthorizationSchemeBasic.String(), model.AuthorizationSchemeBearer.String()), NewCookieSessionAuthnStrategy(schema.NewRefreshIntervalDurationAlways()))

commit 2f1b4f8d0763b5152ad25d9a70253778abde3be8

fix(server): header inconsistencies
There were some header inconsistencies introduced by 427ed6c98aa6c24606166eb45306866bc679f912. This fixes that issue in a partial reversion of the changes.

internal/middlewares/headers.go
@@ -22,6 +22,15 @@ func SecurityHeadersRelaxed(next fasthttp.RequestHandler) fasthttp.RequestHandle
 	}
 }
 
+// The SecurityHeadersBase middleware adds several modern recommended security headers with relaxed values.
+func SecurityHeadersBase(next fasthttp.RequestHandler) fasthttp.RequestHandler {
+	return func(ctx *fasthttp.RequestCtx) {
+		SetBaseSecurityHeaders(ctx)
+
+		next(ctx)
+	}
+}
+
 // The SetStandardSecurityHeaders function adds several modern recommended security headers with safe values.
 func SetStandardSecurityHeaders(ctx *fasthttp.RequestCtx) {
 	SetBaseSecurityHeaders(ctx)

internal/server/asset.go
@@ -59,7 +59,7 @@ func newPublicHTMLEmbeddedHandler() fasthttp.RequestHandler {
 			return
 		}
 
-		middlewares.SetStandardSecurityHeaders(ctx)
+		middlewares.SetBaseSecurityHeaders(ctx)
 
 		contentType := mime.TypeByExtension(path.Ext(p))
 		if len(contentType) == 0 {
@@ -191,7 +191,7 @@ func newLocalesEmbeddedHandler() (handler fasthttp.RequestHandler) {
 			data = []byte("{}")
 		}
 
-		middlewares.SetStandardSecurityHeaders(ctx)
+		middlewares.SetBaseSecurityHeaders(ctx)
 		middlewares.SetContentTypeApplicationJSON(ctx)
 
 		switch {

internal/server/handlers.go
@@ -125,7 +125,8 @@ func handleRouter(config *schema.Configuration, providers middlewares.Providers)
 	handlerPublicHTML := newPublicHTMLEmbeddedHandler()
 	handlerLocales := newLocalesEmbeddedHandler()
 
-	bridge := middlewares.NewBridgeBuilder(*config, providers).Build()
+	bridge := middlewares.NewBridgeBuilder(*config, providers).
+		WithPreMiddlewares(middlewares.SecurityHeadersBase).Build()
 
 	bridgeSwagger := middlewares.NewBridgeBuilder(*config, providers).
 		WithPreMiddlewares(middlewares.SecurityHeadersRelaxed).Build()
@@ -181,16 +182,16 @@ func handleRouter(config *schema.Configuration, providers middlewares.Providers)
 	}
 
 	middlewareAPI := middlewares.NewBridgeBuilder(*config, providers).
-		WithPreMiddlewares(middlewares.SecurityHeadersNoStore, middlewares.SecurityHeadersCSPNone).
+		WithPreMiddlewares(middlewares.SecurityHeadersBase, middlewares.SecurityHeadersNoStore, middlewares.SecurityHeadersCSPNone).
 		Build()
 
 	middleware1FA := middlewares.NewBridgeBuilder(*config, providers).
-		WithPreMiddlewares(middlewares.SecurityHeadersNoStore, middlewares.SecurityHeadersCSPNone).
+		WithPreMiddlewares(middlewares.SecurityHeadersBase, middlewares.SecurityHeadersNoStore, middlewares.SecurityHeadersCSPNone).
 		WithPostMiddlewares(middlewares.Require1FA).
 		Build()
 
 	middlewareElevated1FA := middlewares.NewBridgeBuilder(*config, providers).
-		WithPreMiddlewares(middlewares.SecurityHeadersNoStore, middlewares.SecurityHeadersCSPNone).
+		WithPreMiddlewares(middlewares.SecurityHeadersBase, middlewares.SecurityHeadersNoStore, middlewares.SecurityHeadersCSPNone).
 		WithPostMiddlewares(middlewares.RequireElevated).
 		Build()
 
@@ -335,7 +336,7 @@ func handleRouter(config *schema.Configuration, providers middlewares.Providers)
 
 	if providers.OpenIDConnect != nil {
 		bridgeOIDC := middlewares.NewBridgeBuilder(*config, providers).WithPreMiddlewares(
-			middlewares.SecurityHeadersCSPNoneOpenIDConnect, middlewares.SecurityHeadersNoStore,
+			middlewares.SecurityHeadersBase, middlewares.SecurityHeadersCSPNoneOpenIDConnect, middlewares.SecurityHeadersNoStore,
 		).Build()
 
 		r.GET("/api/oidc/consent", bridgeOIDC(handlers.OpenIDConnectConsentGET))

internal/server/template.go
@@ -39,7 +39,7 @@ func ServeTemplatedFile(t templates.Template, opts *TemplatedFileOptions) middle
 			}
 		}
 
-		middlewares.SetStandardSecurityHeaders(ctx.RequestCtx)
+		middlewares.SetBaseSecurityHeaders(ctx.RequestCtx)
 
 		switch ext {
 		case extHTML:

internal/suites/example/compose/nginx/portal/nginx.conf
@@ -115,6 +115,9 @@ http {
             add_header X-Frame-Options "DENY";
             add_header Permissions-Policy "accelerometer=(), autoplay=(), camera=(), display-capture=(), geolocation=(), gyroscope=(), keyboard-map=(), magnetometer=(), microphone=(), midi=(), payment=(), picture-in-picture=(), screen-wake-lock=(), sync-xhr=(), xr-spatial-tracking=(), interest-cohort=()";
             add_header X-DNS-Prefetch-Control "off";
+            add_header Cross-Origin-Opener-Policy "same-origin";
+            add_header Cross-Origin-Embedder-Policy "require-corp";
+            add_header Cross-Origin-Resource-Policy "same-origin";
 
             proxy_pass        $frontend_endpoint;
         }
@@ -165,6 +168,9 @@ http {
         add_header X-Frame-Options "DENY";
         add_header Permissions-Policy "accelerometer=(), autoplay=(), camera=(), display-capture=(), geolocation=(), gyroscope=(), keyboard-map=(), magnetometer=(), microphone=(), midi=(), payment=(), picture-in-picture=(), screen-wake-lock=(), sync-xhr=(), xr-spatial-tracking=(), interest-cohort=()";
         add_header X-DNS-Prefetch-Control "off";
+        add_header Cross-Origin-Opener-Policy "same-origin";
+        add_header Cross-Origin-Embedder-Policy "require-corp";
+        add_header Cross-Origin-Resource-Policy "same-origin";
 
         error_page 497 301 =307 https://$host:$server_port$request_uri;
 
@@ -336,6 +342,9 @@ http {
         add_header X-Frame-Options "DENY";
         add_header Permissions-Policy "accelerometer=(), autoplay=(), camera=(), display-capture=(), geolocation=(), gyroscope=(), keyboard-map=(), magnetometer=(), microphone=(), midi=(), payment=(), picture-in-picture=(), screen-wake-lock=(), sync-xhr=(), xr-spatial-tracking=(), interest-cohort=()";
         add_header X-DNS-Prefetch-Control "off";
+        add_header Cross-Origin-Opener-Policy "same-origin";
+        add_header Cross-Origin-Embedder-Policy "require-corp";
+        add_header Cross-Origin-Resource-Policy "same-origin";
 
         error_page 497 301 =307 https://$host:$server_port$request_uri;
 