e83571d8fca7c724526c69af5e15c669647e3f50
Brynn Crowley <littlehill723@gmail.com>
2024-04-01 23:37:28 -0700

    feat: added editing of oidc client values, including arrays

---
+++ web/src/views/AdminUI/Common/EditListItem.tsx
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+
+import CheckIcon from "@mui/icons-material/Check";
+import CloseIcon from "@mui/icons-material/Close";
+import { IconButton, List, ListItem, TextField } from "@mui/material";
+
+interface Props {
+    values: string[];
+    onValuesUpdate: (updatedValues: string[]) => void;
+}
+
+const EditListItem = (props: Props) => {
+    //const [editedValues, setEditedValues] = useState<string[]>(props.values);
+    const [newFormValues, setNewFormValues] = useState<string[]>(props.values);
+    const [newFieldValue, setNewFieldValue] = useState<string>("");
+    console.log(newFormValues);
+
+    const handleInputChange = (index: number, newValue: string) => {
+        const updatedValues = [...newFormValues];
+        updatedValues[index] = newValue;
+        setNewFormValues(updatedValues);
+        props.onValuesUpdate(updatedValues);
+    };
+
+    const handleDelete = (index: number) => {
+        const updatedValues = [...newFormValues];
+        const filteredValues = updatedValues.filter((_: any, i: any) => i !== index);
+        setNewFormValues(filteredValues);
+        props.onValuesUpdate(filteredValues);
+    };
+
+    const handleAddField = () => {
+        if (newFieldValue.trim() !== "") {
+            const updatedValues = [...newFormValues, newFieldValue];
+            setNewFormValues(updatedValues);
+            props.onValuesUpdate(updatedValues);
+            setNewFieldValue("");
+        }
+    };
+
+    return (
+        <div>
+            <List>
+                {newFormValues.map((value, index) => (
+                    <ListItem key={`edit-${index}`}>
+                        <TextField
+                            fullWidth
+                            size="small"
+                            value={value}
+                            onChange={(e) => handleInputChange(index, e.target.value)}
+                        />
+                        <IconButton onClick={() => handleDelete(index)}>
+                            <CloseIcon color={"error"} />
+                        </IconButton>
+                    </ListItem>
+                ))}
+                <ListItem key={`add-value`}>
+                    <TextField
+                        size="small"
+                        onChange={(event) => setNewFieldValue(event.target.value)}
+                        value={newFieldValue}
+                        placeholder="New Value"
+                    />
+                    <IconButton onClick={handleAddField}>
+                        <CheckIcon color={"success"} />
+                    </IconButton>
+                </ListItem>
+            </List>
+        </div>
+    );
+};
+
+export default EditListItem;
