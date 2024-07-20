dc701f15bbde1c7f7b5029fc9e5d77586161619c
James Elliott <james-d-elliott@users.noreply.github.com>
2024-07-21 09:23:27 +1000

    build(deps): update lockfiles
    Signed-off-by: James Elliott <james-d-elliott@users.noreply.github.com>

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,5 +1,5 @@
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { Plugin, defineConfig } from "vite";
 import checkerPlugin from "vite-plugin-checker";
 import istanbul from "vite-plugin-istanbul";
 import svgr from "vite-plugin-svgr";
@@ -109,6 +109,51 @@ export default defineConfig(({ mode }) => {
             react(),
             svgr(),
             tsconfigPaths(),
+            muteWarningsPlugin(warningsToIgnore),
         ],
     };
 });
+
+const warningsToIgnore = [
+    ["SOURCEMAP_ERROR", "Can't resolve original location of error"],
+    ["INVALID_ANNOTATION", "contains an annotation that Rollup cannot interpret"],
+];
+
+const muteWarningsPlugin = (warningsToIgnore: string[][]): Plugin => {
+    const mutedMessages = new Set();
+    return {
+        name: "mute-warnings",
+        enforce: "pre",
+        config: (userConfig) => ({
+            build: {
+                rollupOptions: {
+                    onwarn(warning, defaultHandler) {
+                        if (warning.code) {
+                            const muted = warningsToIgnore.find(
+                                ([code, message]) => code == warning.code && warning.message.includes(message),
+                            );
+
+                            if (muted) {
+                                mutedMessages.add(muted.join());
+                                return;
+                            }
+                        }
+
+                        if (userConfig.build?.rollupOptions?.onwarn) {
+                            userConfig.build.rollupOptions.onwarn(warning, defaultHandler);
+                        } else {
+                            defaultHandler(warning);
+                        }
+                    },
+                },
+            },
+        }),
+        closeBundle() {
+            const diff = warningsToIgnore.filter((x) => !mutedMessages.has(x.join()));
+            if (diff.length > 0) {
+                this.warn("Some of your muted warnings never appeared during the build process:");
+                diff.forEach((m) => this.warn(`- ${m.join(": ")}`));
+            }
+        },
+    };
+};
