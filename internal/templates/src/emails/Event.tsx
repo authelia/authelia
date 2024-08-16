e375cfdba09060b2cd744b421741102752e070ad
Brynn Crowley <littlehill723@gmail.com>
2024-08-16 16:39:10 -0700

    refactor(templates): reword/reformat new email templates (#7686)

--- internal/templates/src/emails/Event.tsx
+++ internal/templates/src/emails/Event.tsx
@@ -40,17 +40,12 @@ export const Event = ({
 			<Tailwind>
 				<Body className="bg-white my-auto mx-auto font-sans px-2">
 					<Container className="border border-solid border-[#eaeaea] rounded my-[40px] mx-auto p-[20px] max-w-[465px]">
-						<Heading className="text-black text-[24px] font-normal text-center p-0 my-[30px] mx-0">
-							An important event has occurred with your account
-						</Heading>
 						{title ? <Text className="text-black text-[24px] font-normal text-center p-0 my-[30px] mx-0">{title}</Text> : null}
 						<Text className="text-black text-[14px] leading-[24px]">
 							Hi {displayName},
 						</Text>
 						<Text className="text-black text-[14px] leading-[24px]">
-							This notification has been sent to you in order to notify you of an important event that has
-							occurred with your account. Specifically the <strong><i>{title}</i></strong> event has been
-							triggered.
+							This notification has been sent to you in order to notify you that a new <strong><i>{title}</i></strong> 
 						</Text>
 						<Hr className="border border-solid border-[#eaeaea] my-[26px] mx-0 w-full" />
 						<Text>Event Details:</Text>
