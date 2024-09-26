ffa674b6c8b6c8e30afb7d02d539a111c0f08420
Amir Zarrinkafsh <nightah@me.com>
2024-09-26 10:17:37 +1000

    ci(cmd): build with freebsd14 toolchain (#7920)
    Update to freebsd14 build toolchain via crossbuild container.
    
    Signed-off-by: Amir Zarrinkafsh <nightah@me.com>

--- cmd/authelia-scripts/cmd/build.go
+++ cmd/authelia-scripts/cmd/build.go
@@ -97,7 +97,7 @@ func buildAutheliaBinaryGOX(xflags []string) {
 	go func() {
 		defer wg.Done()
 
-		cmd := utils.CommandWithStdout("bash", "-c", "docker run --rm -e GOX_LINUX_ARM_CC=arm-linux-gnueabihf-gcc -e GOX_LINUX_ARM64_CC=aarch64-linux-gnu-gcc -e GOX_FREEBSD_AMD64_CC=x86_64-pc-freebsd13-gcc -v ${PWD}:/workdir -v /buildkite/.go:/root/go authelia/crossbuild "+
+		cmd := utils.CommandWithStdout("bash", "-c", "docker run --rm -e GOX_LINUX_ARM_CC=arm-linux-gnueabihf-gcc -e GOX_LINUX_ARM64_CC=aarch64-linux-gnu-gcc -e GOX_FREEBSD_AMD64_CC=x86_64-pc-freebsd14-gcc -v ${PWD}:/workdir -v /buildkite/.go:/root/go authelia/crossbuild "+
 			"gox -output={{.Dir}}-{{.OS}}-{{.Arch}} -buildmode=pie -trimpath -cgo -ldflags=\"-linkmode=external -s -w "+strings.Join(xflags, " ")+"\" -osarch=\"linux/amd64 linux/arm linux/arm64 freebsd/amd64\" ./cmd/authelia/")
 
 		err := cmd.Run()
