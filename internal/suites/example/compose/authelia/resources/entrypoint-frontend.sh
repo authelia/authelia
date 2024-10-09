58f6017aab6ebfe3d20d20000b74cd3ee725f8d8
Amir Zarrinkafsh <nightah@me.com>
2024-10-09 18:56:21 +1100

    fix(suites): consistent configuration for oidc suites
    Signed-off-by: Amir Zarrinkafsh <nightah@me.com>

--- internal/suites/example/compose/authelia/resources/entrypoint-frontend.sh
+++ internal/suites/example/compose/authelia/resources/entrypoint-frontend.sh
@@ -2,8 +2,8 @@
 
 PNPM_MODULE="./node_modules/.modules.yaml"
 
-if [[ -f ${PNPM_MODULE} ]]; then
-  rm ${PNPM_MODULE}
+if [[ -f "${PNPM_MODULE}" ]]; then
+  rm "${PNPM_MODULE}"
 fi
 
 pnpm install --frozen-lockfile && pnpm start
