2b19f19adada4e729a7e2b499401f853bd1c4093
James Elliott <james-d-elliott@users.noreply.github.com>
2024-10-27 09:18:24 +1100

    fix(web): encoding
    Signed-off-by: James Elliott <james-d-elliott@users.noreply.github.com>

--- internal/suites/suite_two_factor_test.go
+++ internal/suites/suite_two_factor_test.go
@@ -26,6 +26,10 @@ func (s *TwoFactorSuite) TestNewTwoFactorWebAuthnScenario() {
 	suite.Run(s.T(), NewTwoFactorWebAuthnScenario())
 }
 
+func (s *TwoFactorSuite) TestPasskeyScenario() {
+	suite.Run(s.T(), NewPasskeyScenario())
+}
+
 func TestTwoFactorSuite(t *testing.T) {
 	if testing.Short() {
 		t.Skip("skipping suite test in short mode")
