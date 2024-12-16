ebeb14964c004f9566665a0abf6969d414096dd2
10672208+mind-ar <10672208+mind-ar@users.noreply.github.com>
2024-12-16 23:41:17 +0000

    refactor: remove comented code

--- cmd/authelia-gen/types.go
+++ cmd/authelia-gen/types.go
@@ -95,33 +95,6 @@ type ConfigurationKey struct {
 	Env    string `json:"env"`
 }
 
-/*
-// Languages is the docs json model for the Authelia languages configuration.
-
-	type Languages struct {
-		Defaults   DefaultsLanguages `json:"defaults"`
-		Namespaces []string          `json:"namespaces"`
-		Languages  []Language        `json:"languages"`
-	}
-
-	type DefaultsLanguages struct {
-		Language  Language `json:"language"`
-		Namespace string   `json:"namespace"`
-	}
-
-// Language is the docs json model for a language.
-
-	type Language struct {
-		Display    string   `json:"display"`
-		Locale     string   `json:"locale"`
-		Namespaces []string `json:"namespaces,omitempty"`
-		Fallbacks  []string `json:"fallbacks,omitempty"`
-		Parent     string   `json:"-"`
-
-		Tag language.Tag `json:"-"`
-	}
-*/
-
 type Compose struct {
 	Services map[string]ComposeService `json:"services"`
 }
