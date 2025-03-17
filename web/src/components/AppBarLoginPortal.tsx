c661154d46144b9febf923964efe6bca45f2799a
James Elliott <james-d-elliott@users.noreply.github.com>
2025-03-17 12:58:12 +1100

    fix(web): appbar layout (#8987)
    Fix an issue where the AppBar doesn't properly center the items vertically.

--- web/src/components/AppBarLoginPortal.tsx
+++ web/src/components/AppBarLoginPortal.tsx
@@ -19,6 +19,7 @@ const StyledToolbar = styled(Toolbar)(({ theme }) => ({
     alignItems: "flex-start",
     paddingTop: theme.spacing(1),
     paddingBottom: theme.spacing(2),
+    margin: "auto",
 }));
 
 const AppBarLoginPortal = function (props: Props) {
