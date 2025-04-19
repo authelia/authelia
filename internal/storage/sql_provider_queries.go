e1b9ff0e362ab3ff4caf6a8d5de0aee366e06690
James Elliott <james-d-elliott@users.noreply.github.com>
2025-04-19 17:18:10 +1000

    fix(migrations): update mssql migrations
    Signed-off-by: James Elliott <james-d-elliott@users.noreply.github.com>

--- internal/storage/sql_provider_queries.go
+++ internal/storage/sql_provider_queries.go
@@ -24,8 +24,8 @@ const (
 
 	queryMSSQLSelectExistingTables = `
 		SELECT TABLE_NAME
-		FROM @p1.INFORMATION_SCHEMA.TABLES
-		WHERE TABLE_TYPE = 'BASE TABLE';`
+		FROM INFORMATION_SCHEMA.TABLES
+		WHERE TABLE_TYPE = 'BASE TABLE' AND TABLE_SCHEMA = @p1;`
 
 	queryPostgreSelectExistingTables = `
 		SELECT table_name
