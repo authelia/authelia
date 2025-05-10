f54c34b4ffff735a381a7a083e038897e7d4fba0
James Elliott <james-d-elliott@users.noreply.github.com>
2025-05-10 10:28:26 +1000

    test: update dlv and bootstrap (#9460)
    Signed-off-by: James Elliott <james-d-elliott@users.noreply.github.com>

--- internal/suites/example/compose/authelia/resources/run-backend-dev.sh
+++ internal/suites/example/compose/authelia/resources/run-backend-dev.sh
@@ -4,6 +4,6 @@ set -e
 
 while true;
 do
-    AUTHELIA_SERVER_DISABLE_HEALTHCHECK=true CGO_ENABLED=1 dlv --listen 0.0.0.0:2345 --headless=true --output=./authelia --continue --accept-multiclient debug cmd/authelia/*.go
+    AUTHELIA_SERVER_DISABLE_HEALTHCHECK=true CGO_ENABLED=1 dlv debug --headless --listen=:2345 --api-version=2 --output=./authelia --continue --accept-multiclient cmd/authelia/*.go
     sleep 3
 done
