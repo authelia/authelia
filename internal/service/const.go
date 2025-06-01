00c6e57c28b6bbc82213b445d98cd9e861a080ad
James Elliott <james-d-elliott@users.noreply.github.com>
2025-06-01 13:53:32 +1000

    feat(configuration): reload
    This allows users to enable reloading Authelia on configuration changes.
    
    Signed-off-by: James Elliott <james-d-elliott@users.noreply.github.com>

--- internal/service/const.go
+++ internal/service/const.go
@@ -1,5 +1,7 @@
 package service
 
+import "errors"
+
 const (
 	fmtLogServerListening = "Listening for %s connections on '%s' path '%s'"
 )
@@ -12,4 +14,10 @@ const (
 	serviceTypeServer  = "server"
 	serviceTypeWatcher = "watcher"
 	serviceTypeSignal  = "signal"
+)
+
+var (
+	// ErrConfigReload is emitted when the config is being reloaded. This effectively starts Authelia again instead
+	// of doing a full exit.
+	ErrConfigReload = errors.New("config reload")
 )
