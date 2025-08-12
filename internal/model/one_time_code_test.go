4cf76fbb3775d72218545362f27bb8d1e153972b
James Elliott <james-d-elliott@users.noreply.github.com>
2025-08-12 16:23:04 +1000

    test: add test coverage
    Signed-off-by: James Elliott <james-d-elliott@users.noreply.github.com>

--- internal/model/one_time_code_test.go
+++ internal/model/one_time_code_test.go
@@ -1,1 +1,42 @@
 package model
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewOneTimeCode(t *testing.T) {
+	testCases := []struct {
+		name       string
+		username   string
+		characters int
+		duration   time.Duration
+		expected   *OneTimeCode
+		err        string
+	}{
+		{
+			"Success",
+			"username",
+			1,
+			time.Hour,
+			&OneTimeCode{},
+			"",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			actual, err := NewOneTimeCode(nil, tc.username, tc.characters, tc.duration)
+
+			if tc.err != "" {
+				assert.EqualError(t, err, tc.err)
+				assert.Nil(t, actual)
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, tc.expected, actual)
+			}
+		})
+	}
+}
