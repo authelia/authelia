402a4be6b0379282ebc72c02a53d7c6a202af460
Jens Thiel <jens@thiel.de>
2025-09-26 13:18:19 +0100

    feat(utils): add Clamp function for value clamping between min and max

---
+++ internal/utils/numerics.go
@@ -0,0 +1,14 @@
+package utils
+
+import "cmp"
+
+// Clamp returns value clamped between min and max (inclusive).
+func Clamp[T cmp.Ordered](value, min, max T) T {
+    if value < min {
+        return min
+    }
+    if value > max {
+        return max
+    }
+    return value
+}
