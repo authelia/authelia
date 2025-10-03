b9e8820722295806f037a3b8736c07d464bf33f0
Amir Zarrinkafsh <nightah@me.com>
2025-10-03 10:12:03 +1000

    refactor: pin go development deps (#10458)
    This change pins dependencies utilised in the development environment for hot-reloading and debugging.
    
    Signed-off-by: Amir Zarrinkafsh <nightah@me.com>

--- internal/suites/example/compose/authelia/resources/entrypoint-backend.sh
+++ internal/suites/example/compose/authelia/resources/entrypoint-backend.sh
@@ -2,12 +2,11 @@
 
 set -x
 
-# We move out of the workspace to not include the modules as dependencies of the project.
-cd /
+cd /resources
 
 echo "Use hot reloaded version of Authelia backend"
-go install github.com/cespare/reflex@latest
-go install github.com/go-delve/delve/cmd/dlv@latest
+go install github.com/cespare/reflex
+go install github.com/go-delve/delve/cmd/dlv
 
 cd /app
 
