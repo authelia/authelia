a946cb779f20138b47f66adbf0e1220038ad54b8
Amir Zarrinkafsh <3339418+nightah@users.noreply.github.com>
2025-11-12 10:59:32 +1100

    fix(web): remote call
    Signed-off-by: Amir Zarrinkafsh <3339418+nightah@users.noreply.github.com>

--- web/src/hooks/Configuration.ts
+++ web/src/hooks/Configuration.ts
@@ -2,5 +2,5 @@ import { useRemoteCall } from "@hooks/RemoteCall";
 import { getConfiguration } from "@services/Configuration";
 
 export function useConfiguration() {
-    return useRemoteCall(getConfiguration, []);
+    return useRemoteCall(getConfiguration);
 }
