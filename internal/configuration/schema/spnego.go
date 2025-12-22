1e3e0373e2d54a20ecc7e711d05404877410e800
Matthieu Pignolet <matthieu@puffer.fish>
2025-12-22 15:13:37 +0100

    fix: typo in the SPNEGO configuration description
    Signed-off-by: Matthieu Pignolet <matthieu@puffer.fish>

--- internal/configuration/schema/spnego.go
+++ internal/configuration/schema/spnego.go
@@ -1,7 +1,7 @@
 package schema
 
 type SPNEGO struct {
-	Disable   bool   `koanf:"disable" json:"disable" jsonschema:"default=false,title=Disable" jsonschema_description:"Disables the WebAuthn 2FA functionality."`
+	Disable   bool   `koanf:"disable" json:"disable" jsonschema:"default=false,title=Disable" jsonschema_description:"Disables the SPNEGO authentication functionality."`
 	Keytab    string `koanf:"keytab" json:"keytab" jsonschema:"title=File Path" jsonschema_description:"The filepath to the kerberos keytab."`
 	Principal string `koanf:"principal" json:"principal" jsonschema:"title=Principal" jsonschema_description:"The service principal name."`
 }
