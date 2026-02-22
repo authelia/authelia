0f80a0f5b6fc3611874a31e3465157bec4bd3f62
James Elliott <james-d-elliott@users.noreply.github.com>
2026-02-22 16:31:38 +1100

    refactor: encapsulated session manager

--- internal/session/session.go
+++ internal/session/session.go
@@ -112,3 +112,63 @@ func (p *Session) GetExpiration(ctx *fasthttp.RequestCtx) (time.Duration, error)
 
 	return store.GetExpiration(), nil
 }
+
+func NewEncapsulatedSession(base *Session, ctx *fasthttp.RequestCtx) *EncapsulatedSession {
+	return &EncapsulatedSession{base: base, ctx: ctx}
+}
+
+type EncapsulatedSession struct {
+	base *Session
+
+	ctx *fasthttp.RequestCtx
+}
+
+// NewDefaultUserSession returns a new default UserSession for this session provider.
+func (p *EncapsulatedSession) NewDefaultUserSession() (userSession UserSession) {
+	return p.base.NewDefaultUserSession()
+}
+
+// GetSession return the user session from a request.
+func (p *EncapsulatedSession) GetSession() (userSession UserSession, err error) {
+	return p.base.GetSession(p.ctx)
+}
+
+// SaveSession save the user session.
+func (p *EncapsulatedSession) SaveSession(userSession UserSession) (err error) {
+	return p.base.SaveSession(p.ctx, userSession)
+}
+
+// RegenerateSession regenerate a session ID.
+func (p *EncapsulatedSession) RegenerateSession() (err error) {
+	return p.base.RegenerateSession(p.ctx)
+}
+
+// DestroySession destroy a session ID and delete the cookie.
+func (p *EncapsulatedSession) DestroySession() (err error) {
+	return p.base.DestroySession(p.ctx)
+}
+
+// UpdateSessionExpiration update the expiration of the cookie and session.
+func (p *EncapsulatedSession) UpdateSessionExpiration(expiration time.Duration) (err error) {
+	return p.base.UpdateExpiration(p.ctx, expiration)
+}
+
+// GetSessionExpiration get the expiration of the current session.
+func (p *EncapsulatedSession) GetSessionExpiration() (expiration time.Duration, err error) {
+	return p.base.GetExpiration(p.ctx)
+}
+
+func (p *EncapsulatedSession) GetSessionConfig() (config schema.SessionCookie) {
+	return p.base.Config
+}
+
+type Manager interface {
+	NewDefaultUserSession() (userSession UserSession)
+	GetSession() (userSession UserSession, err error)
+	SaveSession(userSession UserSession) (err error)
+	RegenerateSession() (err error)
+	DestroySession() (err error)
+	UpdateSessionExpiration(expiration time.Duration) (err error)
+	GetSessionExpiration() (expiration time.Duration, err error)
+	GetSessionConfig() (config schema.SessionCookie)
+}
