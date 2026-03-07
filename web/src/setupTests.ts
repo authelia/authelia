8e699eea59e87484bc1da04d7505a77db89a22b6
Amir Zarrinkafsh <3339418+nightah@users.noreply.github.com>
2026-03-07 12:57:32 +1100

    test(web): clean up noisy logs during frontend unit testing
    Signed-off-by: Amir Zarrinkafsh <3339418+nightah@users.noreply.github.com>

--- web/src/setupTests.ts
+++ web/src/setupTests.ts
@@ -4,6 +4,7 @@ import { initReactI18next } from "react-i18next";
 
 await i18n.use(initReactI18next).init({
     resources: { en: { testNS: {} } },
+    showSupportNotice: false,
 });
 
 interface LocalStorageMock {
