858921f6e10c8ff9aa4ad8293e76e4629751aa5e
Amir Zarrinkafsh <3339418+nightah@users.noreply.github.com>
2026-03-16 19:07:04 +1100

    fix(web): oidc visual alignment and fix disappearing claims
    Signed-off-by: Amir Zarrinkafsh <3339418+nightah@users.noreply.github.com>

--- web/src/views/ConsentPortal/OpenIDConnect/DecisionFormScopes.tsx
+++ web/src/views/ConsentPortal/OpenIDConnect/DecisionFormScopes.tsx
@@ -14,25 +14,21 @@ const DecisionFormScopes: FC<Props> = (props: Props) => {
     const { t: translate } = useTranslation(["consent"]);
 
     return (
-        <div className="w-full">
-            <div className="text-center">
-                <ul className="my-4 inline-block list-none bg-card">
-                    {props.scopes.map((scope: string) => (
-                        <TooltipProvider key={scope}>
-                            <Tooltip>
-                                <TooltipTrigger asChild>
-                                    <li id={"scope-" + scope} className="flex items-center gap-3 px-2 py-1">
-                                        <span className="flex-shrink-0">{ScopeAvatar(scope)}</span>
-                                        <span>{formatScope(translate(`scopes.${scope}`), scope)}</span>
-                                    </li>
-                                </TooltipTrigger>
-                                <TooltipContent>{translate("Scope", { name: scope })}</TooltipContent>
-                            </Tooltip>
-                        </TooltipProvider>
-                    ))}
-                </ul>
-            </div>
-        </div>
+        <ul className="mt-4 mb-1 list-none rounded-md bg-card p-2">
+            {props.scopes.map((scope: string) => (
+                <TooltipProvider key={scope}>
+                    <Tooltip>
+                        <TooltipTrigger asChild>
+                            <li id={"scope-" + scope} className="flex items-center gap-3 px-2 py-1">
+                                <span className="flex-shrink-0">{ScopeAvatar(scope)}</span>
+                                <span>{formatScope(translate(`scopes.${scope}`), scope)}</span>
+                            </li>
+                        </TooltipTrigger>
+                        <TooltipContent>{translate("Scope", { name: scope })}</TooltipContent>
+                    </Tooltip>
+                </TooltipProvider>
+            ))}
+        </ul>
     );
 };
 
