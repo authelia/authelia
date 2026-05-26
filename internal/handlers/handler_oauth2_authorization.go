ab5dca7329f9cb0567bfa10895c703cc1e2458e8
James Elliott <james-d-elliott@users.noreply.github.com>
2026-05-26 15:44:30 +1000

    fix(handlers): hoist issuer checks (#12160)
    This moves the issuer check up higher at every endpoint where
    applicable. While this probably doesn't make for a meaningful fix it
    will ensure the most relevant and meaningful error is shown first. In
    addition it changes the way these messages are communicated. It also add
    some additional simple hardening.

--- internal/handlers/handler_oauth2_authorization.go
+++ internal/handlers/handler_oauth2_authorization.go
@@ -29,6 +29,16 @@ func OAuth2AuthorizationGET(ctx *middlewares.AutheliaCtx, rw http.ResponseWriter
 		err       error
 	)
 
+	if issuer, err = ctx.IssuerURL(); err != nil {
+		rfc := oidc.ErrEffectiveIssuer.WithWrap(err)
+
+		ctx.GetLogger().WithError(err).Errorf("Authorization Request could not be processed: %s", oauthelia2.ErrorToDebugRFC6749Error(rfc))
+
+		ctx.Providers.OpenIDConnect.WriteAuthorizeError(ctx, rw, requester, rfc)
+
+		return
+	}
+
 	if requester, err = ctx.Providers.OpenIDConnect.NewAuthorizeRequest(ctx, r); requester == nil {
 		err = oauthelia2.ErrServerError.WithDebug("The requester was nil.")
 
@@ -54,14 +64,6 @@ func OAuth2AuthorizationGET(ctx *middlewares.AutheliaCtx, rw http.ResponseWriter
 	clientID := requester.GetClient().GetID()
 
 	ctx.GetLogger().Debugf("Authorization Request with id '%s' on client with id '%s' is being processed", requester.GetID(), clientID)
-
-	if issuer, err = ctx.IssuerURL(); err != nil {
-		ctx.GetLogger().WithError(err).Errorf("Authorization Request with id '%s' on client with id '%s' could not be processed: %s", requester.GetID(), clientID, oidc.ErrTextEffectiveIssuer)
-
-		ctx.Providers.OpenIDConnect.WriteAuthorizeError(ctx, rw, requester, oidc.ErrEffectiveIssuer)
-
-		return
-	}
 
 	if client, err = ctx.Providers.OpenIDConnect.GetRegisteredClient(ctx, clientID); err != nil {
 		if errors.Is(err, oauthelia2.ErrNotFound) || errors.Is(err, oauthelia2.ErrInvalidClient) {
@@ -179,7 +181,20 @@ func OAuth2AuthorizationGET(ctx *middlewares.AutheliaCtx, rw http.ResponseWriter
 func OAuth2AuthorizationPOST(ctx *middlewares.AutheliaCtx, rw http.ResponseWriter, r *http.Request) {
 	requester := oauthelia2.NewAuthorizeRequest()
 
-	var err error
+	var (
+		redirectURL *url.URL
+		err         error
+	)
+
+	if redirectURL, err = ctx.IssuerURL(); err != nil {
+		rfc := oidc.ErrEffectiveIssuer.WithWrap(err)
+
+		ctx.GetLogger().WithError(err).Errorf("Authorization Request with id '%s' could not be processed: %s", requester.GetID(), oauthelia2.ErrorToDebugRFC6749Error(rfc))
+
+		ctx.Providers.OpenIDConnect.WriteAuthorizeError(ctx, rw, requester, rfc)
+
+		return
+	}
 
 	r.Body = http.MaxBytesReader(rw, r.Body, 10<<20)
 
@@ -193,16 +208,6 @@ func OAuth2AuthorizationPOST(ctx *middlewares.AutheliaCtx, rw http.ResponseWrite
 	}
 
 	query := r.Form
-
-	var redirectURL *url.URL
-
-	if redirectURL, err = ctx.IssuerURL(); err != nil {
-		ctx.GetLogger().WithError(err).Errorf("Authorization Request with id '%s' could not be processed: %s", requester.GetID(), oidc.ErrTextEffectiveIssuer)
-
-		ctx.Providers.OpenIDConnect.WriteAuthorizeError(ctx, rw, requester, oidc.ErrEffectiveIssuer)
-
-		return
-	}
 
 	redirectURL = redirectURL.JoinPath(oidc.EndpointPathAuthorization)
 
