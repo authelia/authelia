989a7640924938b46cb04ff602dc71b37bb3104d
Brynn Crowley <littlehill723@gmail.com>
2026-08-01 16:57:19 -0700

    feat: user management frontend
    Signed-off-by: Brynn Crowley <littlehill723@gmail.com>

--- web/src/models/UserInfo.ts
+++ web/src/models/UserInfo.ts
@@ -1,8 +1,14 @@
 import { SecondFactorMethod } from "@models/Methods";
 
 export interface UserInfo {
+    username: string;
     display_name: string;
     emails: string[];
+    groups: string[];
+    disabled?: boolean;
+    last_logged_in?: Date;
+    last_password_change?: Date;
+    user_created_at?: Date;
     method: SecondFactorMethod;
     has_webauthn: boolean;
     has_totp: boolean;
