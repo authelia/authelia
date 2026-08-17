e55fa3fdabf8aa59b53d8dff19ac1a5b40f3dbf9
James Elliott <james-d-elliott@users.noreply.github.com>
2026-08-17 10:27:31 +1000

    refactor: regenerate session eagerly on reset

--- internal/handlers/handler_reset_password.go
+++ internal/handlers/handler_reset_password.go
@@ -47,6 +47,14 @@ func ResetPasswordDELETE(ctx *middlewares.AutheliaCtx) {
 		return
 	}
 
+	if err = ctx.RegenerateSession(); err != nil {
+		ctx.GetLogger().WithError(err).Error("Error occurred regenerating user session")
+
+		ctx.Error(err, messageOperationFailed)
+
+		return
+	}
+
 	token, err = jwt.ParseWithClaims(body.Token, &model.IdentityVerificationClaim{},
 		func(token *jwt.Token) (any, error) {
 			return []byte(ctx.Configuration.IdentityValidation.ResetPassword.JWTSecret), nil
@@ -144,6 +152,14 @@ func ResetPasswordPOST(ctx *middlewares.AutheliaCtx) {
 	if userSession, err = ctx.GetSession(); err != nil {
 		ctx.GetLogger().WithError(err).Error(errStrUserSessionData)
 		ctx.SetJSONError(messageUnableToResetPassword)
+
+		return
+	}
+
+	if err = ctx.RegenerateSession(); err != nil {
+		ctx.GetLogger().WithError(err).Error("Error occurred regenerating user session")
+
+		ctx.Error(err, messageUnableToResetPassword)
 
 		return
 	}
