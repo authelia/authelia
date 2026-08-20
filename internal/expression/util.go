7eb40b592a070c177f9d54651afaa02d36ded996
James Elliott <james-d-elliott@users.noreply.github.com>
2026-08-20 15:31:01 +1000

    build(deps): update module cel.dev/cel-go to v0.32.0

--- internal/expression/util.go
+++ internal/expression/util.go
@@ -1,10 +1,10 @@
 package expression
 
 import (
-	"github.com/google/cel-go/cel"
-	"github.com/google/cel-go/common/types"
-	"github.com/google/cel-go/common/types/ref"
-	"github.com/google/cel-go/ext"
+	"cel.dev/cel-go/cel"
+	"cel.dev/cel-go/common/types"
+	"cel.dev/cel-go/common/types/ref"
+	"cel.dev/cel-go/ext"
 )
 
 func optExtra(name string, attribute ExtraAttribute) (opt cel.EnvOption) {
