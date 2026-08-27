afaf4c5ebbd6cfc2a0f56c2454d9df1cc40697fc
James Elliott <james-d-elliott@users.noreply.github.com>
2026-08-27 23:11:02 +1000

    build(deps): update module github.com/go-webauthn/webauthn to v0.18.0

--- internal/webauthn/util.go
+++ internal/webauthn/util.go
@@ -14,34 +14,25 @@ import (
 // IsCredentialCreationDiscoverable returns true if the *protocol.ParsedCredentialCreationData indicates a discoverable
 // credential was generated.
 func IsCredentialCreationDiscoverable(logger *logrus.Entry, response *protocol.ParsedCredentialCreationData) (discoverable bool) {
-	if value, ok := response.ClientExtensionResults[ExtensionCredProps]; ok {
-		switch credentialProperties := value.(type) {
-		case map[string]any:
-			var v any
+	credentialProperties := response.ClientExtensionResults.CredProps
 
-			if v, ok = credentialProperties[ExtensionCredPropsResidentKey]; ok {
-				if discoverable, ok = v.(bool); ok {
-					logger.WithFields(map[string]any{LogFieldDiscoverable: discoverable}).Trace("Determined Credential Discoverability via Client Extension Results")
+	if credentialProperties == nil {
+		logger.WithFields(map[string]any{LogFieldDiscoverable: false}).Trace("Assuming Credential Discoverability is false as the 'credProps' extension is missing from the Client Extension Results")
 
-					return discoverable
-				} else {
-					logger.WithFields(map[string]any{LogFieldDiscoverable: false}).Trace("Assuming Credential Discoverability is false as the 'rk' field for the 'credProps' extension in the Client Extension Results was not a boolean")
-				}
-			} else {
-				logger.WithFields(map[string]any{LogFieldDiscoverable: false}).Trace("Assuming Credential Discoverability is false as the 'rk' field for the 'credProps' extension was missing from the Client Extension Results")
-			}
-
-			return false
-		default:
-			logger.WithFields(map[string]any{LogFieldDiscoverable: false}).Trace("Assuming Credential Discoverability is false as the 'credProps' extension in the Client Extension Results does not appear to be a dictionary")
-
-			return false
-		}
+		return false
 	}
 
-	logger.WithFields(map[string]any{LogFieldDiscoverable: false}).Trace("Assuming Credential Discoverability is false as the 'credProps' extension is missing from the Client Extension Results")
+	if credentialProperties.RK == nil {
+		logger.WithFields(map[string]any{LogFieldDiscoverable: false}).Trace("Assuming Credential Discoverability is false as the 'rk' field for the 'credProps' extension was missing from the Client Extension Results")
 
-	return false
+		return false
+	}
+
+	discoverable = *credentialProperties.RK
+
+	logger.WithFields(map[string]any{LogFieldDiscoverable: discoverable}).Trace("Determined Credential Discoverability via Client Extension Results")
+
+	return discoverable
 }
 
 // ValidateCredentialAllowed returns an error if the given credential is prohibited by the configured filters.
