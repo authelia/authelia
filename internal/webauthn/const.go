2d5325d9a7d1814d515a77e56d2582051c21ac87
James Elliott <james-d-elliott@users.noreply.github.com>
2026-08-29 02:11:32 +0000

    build(deps): update module github.com/go-webauthn/webauthn to v0.18.0 (#12900)
    Closes #12901.

--- internal/webauthn/const.go
+++ internal/webauthn/const.go
@@ -1,11 +1,9 @@
 package webauthn
 
-// WebAuthn extension, log field, and cache name strings.
+// WebAuthn log field and cache name strings.
 const (
-	ExtensionCredProps            = "credProps"
-	ExtensionCredPropsResidentKey = "rk"
-	LogFieldDiscoverable          = "discoverable"
-	cacheMDS3                     = "mds3"
+	LogFieldDiscoverable = "discoverable"
+	cacheMDS3            = "mds3"
 )
 
 // Metadata cache policy strings.
