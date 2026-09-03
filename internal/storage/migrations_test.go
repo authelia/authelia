6dd4cfcf76295a6f4a5c568511eab43ffbaa3214
James Elliott <james-d-elliott@users.noreply.github.com>
2026-09-03 17:23:58 +1000

    fix(storage): access token jwt upgrade
    This fixes an issue where the Access Token JWT sessions that need to be linked to Refresh Token sessions prevented an upgrade due to their signature lengths.

--- internal/storage/migrations_test.go
+++ internal/storage/migrations_test.go
@@ -17,7 +17,7 @@ import (
 
 const (
 	// This is the latest schema version for the purpose of tests.
-	LatestVersion = 27
+	LatestVersion = 28
 )
 
 func TestShouldObtainCorrectMigrations(t *testing.T) {
