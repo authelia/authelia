e38a7cb4c59c24b841855c56aa65a10d848349d6
James Elliott <james-d-elliott@users.noreply.github.com>
2026-09-07 10:08:49 +1000

    refactor: suggestions

--- web/types/vitest.d.ts
+++
@@ -1,8 +0,0 @@
-import type { TestingLibraryMatchers } from "@testing-library/jest-dom/matchers";
-
-declare module "vitest" {
-    interface Matchers<
-        R extends Promise<void> | void = Promise<void> | void,
-        T = unknown,
-    > extends TestingLibraryMatchers<T, R> {}
-}
