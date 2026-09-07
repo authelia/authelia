1ccbfab1d6f12730cdbec2a97d2cff4635e42b6e
Brynn Crowley <littlehill723@gmail.com>
2026-09-07 12:57:07 -0700

    ci: add traceability labels to integration image builds

--- .buildkite/integration.sh
+++ .buildkite/integration.sh
@@ -5,6 +5,8 @@ DIRECTORY="unset"
 GROUP="unset"
 PREFIX="authelia/"
 TAG="unset"
+CREATED=$(date -u +%Y-%m-%dT%H:%M:%SZ)
+SOURCE="https://github.com/authelia/authelia"
 
 if [[ "${BUILDKITE_BRANCH}" =~ ^renovate- ]]; then
   TAG="renovate"
@@ -32,7 +34,17 @@ steps:
   - label: ":docker: Build and Deploy"
     commands:
       - "cd ${DIRECTORY}"
-      - "docker build --tag ${PREFIX}${BUILDKITE_PIPELINE_NAME}:${TAG} --platform linux/amd64,linux/arm64 --provenance mode=max,reproducible=true --sbom true --builder buildx --pull --push ."
+      - "docker build \
+          --tag ${PREFIX}${BUILDKITE_PIPELINE_NAME}:${TAG} \
+          --label org.opencontainers.image.revision=${BUILDKITE_COMMIT} \
+          --label org.opencontainers.image.created=${CREATED} \
+          --label org.opencontainers.image.source=${SOURCE} \
+          --label com.authelia.ci.build-url=${BUILDKITE_BUILD_URL} \
+          --platform linux/amd64,linux/arm64 \
+          --provenance mode=max,reproducible=true \
+          --sbom true \
+          --builder buildx \
+          --pull --push ."
     concurrency: 1
     concurrency_group: "${GROUP}"
     agents:
