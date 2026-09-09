9efedc0e6acd533e2c74d75aa5860ebd9e78220c
James Elliott <james-d-elliott@users.noreply.github.com>
2026-09-09 15:22:17 +1000

    docs: add license headers to internal (3/3)

--- internal/service/doc.go
+++ internal/service/doc.go
@@ -1,2 +1,6 @@
+// SPDX-FileCopyrightText: 2026 Authelia
+//
+// SPDX-License-Identifier: Apache-2.0
+
 // Package service implements the long running services which make up a running Authelia process.
 package service
