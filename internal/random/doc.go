ea9c8d8e51e98017d2824734de7358d5a6f644fd
James Elliott <james-d-elliott@users.noreply.github.com>
2026-09-09 15:22:16 +1000

    docs: add license headers to internal (2/3)

--- internal/random/doc.go
+++ internal/random/doc.go
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2026 Authelia
+//
+// SPDX-License-Identifier: Apache-2.0
+
 // Package random provides the random providers used to generate cryptographically secure and mathematical random
 // values.
 package random
