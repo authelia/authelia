7eb91d540a3eb6b7898191cafebf6c4cb327f1d2
James Elliott <james-d-elliott@users.noreply.github.com>
2026-09-09 18:40:21 +1000

    refactor(suites): fix env parser

--- cmd/authelia-suites/main.go
+++ cmd/authelia-suites/main.go
@@ -7,6 +7,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"path/filepath"
 	"strings"
@@ -82,6 +83,48 @@ func removeRunningSuiteFile() error {
 	return os.Remove(runningSuiteFile)
 }
 
+func loadSuiteEnvironment(path string) (err error) {
+	if _, err = os.Stat(path); err != nil {
+		if os.IsNotExist(err) {
+			return nil
+		}
+
+		return fmt.Errorf("error checking environment file '%s': %w", path, err)
+	}
+
+	file, err := os.Open(path)
+	if err != nil {
+		return fmt.Errorf("error opening environment file '%s': %w", path, err)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+
+		if line == "" || strings.HasPrefix(line, "#") {
+			continue
+		}
+
+		v := strings.SplitN(line, "=", 2)
+		if len(v) != 2 {
+			return fmt.Errorf("error parsing environment file '%s': line '%s' is not a comment and does not contain a '=' character", path, line)
+		}
+
+		if err = os.Setenv(v[0], v[1]); err != nil {
+			return fmt.Errorf("error setting environment variable '%s' from environment file '%s': %w", v[0], path, err)
+		}
+	}
+
+	if err = scanner.Err(); err != nil {
+		return fmt.Errorf("error reading environment file '%s': %w", path, err)
+	}
+
+	return nil
+}
+
 func setupSuite(cmd *cobra.Command, args []string) {
 	suiteName := args[0]
 	s := suites.GlobalRegistry.Get(suiteName)
@@ -98,25 +141,8 @@ func setupSuite(cmd *cobra.Command, args []string) {
 		log.Fatal(err)
 	}
 
-	suiteEnv := suiteResourcePath + "/.env"
-
-	_, err = os.Stat(suiteEnv)
-	if err == nil {
-		file, err := os.Open(suiteEnv)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		env := bufio.NewScanner(file)
-
-		for env.Scan() {
-			v := strings.Split(env.Text(), "=")
-
-			err := os.Setenv(v[0], v[1])
-			if err != nil {
-				log.Fatal(err)
-			}
-		}
+	if err = loadSuiteEnvironment(suiteResourcePath + "/.env"); err != nil {
+		log.Fatal(err)
 	}
 
 	suiteTmpDirectory := suites.SuiteTmpPath("authelia", "suites", suiteName)
