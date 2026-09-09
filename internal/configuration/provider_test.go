531ab68e2d973876a45464149860bd7e22bc6b07
James Elliott <james-d-elliott@users.noreply.github.com>
2026-09-09 15:54:52 +1000

    ci: fix config

--- internal/configuration/provider_test.go
+++ internal/configuration/provider_test.go
@@ -1,6 +1,6 @@
 // SPDX-FileCopyrightText: 2026 Authelia
 //
-// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: Apache-2.0.
 
 package configuration
 
@@ -1457,7 +1457,7 @@ func TestConfigurationTemplate(t *testing.T) {
 
 		defer f.Close()
 
-		lints := regexp.MustCompile(`^(\s+)?# yamllint`)
+		lints := regexp.MustCompile(`^(\s+)?# (yamllint|SPDX-FileCopyrightText|SPDX-License-Identifier)`)
 		doc := regexp.MustCompile(`^\s+?## `)
 		commented := regexp.MustCompile(`^(\s+)?# (.*)$`)
 		uncommented := regexp.MustCompile(`^(\s+)?\w+`)
