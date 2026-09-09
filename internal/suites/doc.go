25bf94c912cfdfbaa68b798c19b2b16b3496e463
James Elliott <james-d-elliott@users.noreply.github.com>
2026-09-09 08:58:57 +0000

    docs(suites): add license headers (1/2) (#13056)

--- internal/suites/doc.go
+++ internal/suites/doc.go
@@ -1,2 +1,6 @@
+// SPDX-FileCopyrightText: 2026 Authelia
+//
+// SPDX-License-Identifier: Apache-2.0
+
 // Package suites implements the integration test suites and the utilities they share.
 package suites
